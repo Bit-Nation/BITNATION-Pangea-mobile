fecb026104bcf11af6fcfb1af56c6e10a5b7b321
Andrii Selivanov <seland2008@gmail.com>
2017-12-28 23:52:04 +0200

    [project] Fix to passing tests.

--- App.js
+++ App.js
@@ -6,20 +6,24 @@ import configureStore from './src/config/configureStore';
 import { registerScreens } from './src/screens/screens';
 import Screens, { tabsStyle } from './src/global/Screens';
 
-const App = () => {
-  const store = configureStore();
-  registerScreens(store, Provider);
+const store = configureStore();
+registerScreens(store, Provider);
 
-  Navigation.startTabBasedApp({
-    tabs: [
-      Screens.DASHBOARD_SCREEN,
-      Screens.CHAT_SCREEN,
-      Screens.NATIONS_SCREEN,
-      Screens.WALLET_SCREEN,
-      Screens.PROFILE_SCREEN,
-    ],
-    tabsStyle,
-  });
+Navigation.startTabBasedApp({
+  tabs: [
+    Screens.DASHBOARD_SCREEN,
+    Screens.CHAT_SCREEN,
+    Screens.NATIONS_SCREEN,
+    Screens.WALLET_SCREEN,
+    Screens.PROFILE_SCREEN,
+  ],
+  tabsStyle,
+});
 
-};
-export default App();
+class App extends Component {
+  render() {
+    return null;
+  }
+}
+
+export default App;
