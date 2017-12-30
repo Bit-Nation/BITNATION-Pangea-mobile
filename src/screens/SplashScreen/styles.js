915ed430345e971241ab091dddafaacb0255442d
Kent <kent.dahlgren@gmail.com>
2017-12-29 23:30:36 -0800

    Adding folders and files in alignment with Andrew's work

--- src/screens/SplashScreen/styles.js
+++ src/screens/SplashScreen/styles.js
@@ -2,15 +2,15 @@ import { MediaQueryStyleSheet } from 'react-native-responsive';
 import Colors from '../../global/Colors';
 
 const styles = MediaQueryStyleSheet.create({
-        container: {
-          flex: 1,
-          justifyContent: 'center',
-          alignItems:'center',
-          backgroundColor: Colors.Blue,
-        },
-        logo: {
-          alignSelf:'center',
-        },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.Blue,
+  },
+  logo: {
+    alignSelf: 'center',
+  },
 });
 
 export default styles;
