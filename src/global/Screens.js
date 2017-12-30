f785b6e5940e8591d08ed77b74b2c909fecfffd5
Kent <kent.dahlgren@gmail.com>
2017-12-29 23:22:25 -0800

    adding screens.js because pull wasn't working when I checked out feature/profile

---
+++ src/global/Screens.js
@@ -0,0 +1,60 @@
+import AssetsImages from './AssetsImages';
+import Colors from './Colors';
+
+export const tabsStyle = {
+  tabBarButtonColor: Colors.white,
+  tabBarLabelColor: Colors.BitNationLightBlue,
+  tabBarSelectedLabelColor: Colors.white,
+  tabBarSelectedButtonColor: Colors.white,
+  tabBarBackgroundColor: 'rgba(17,39,110,0.9)',
+};
+
+export const appStyle = {
+  ...tabsStyle
+};
+
+export const navigatorStyle = {
+  statusBarTextColorScheme: 'light',
+  statusBarColor: 'transparent',
+  navBarTransparent: true,
+  navBarTranslucent: true,
+  navBarNoBorder: true,
+  drawUnderNavBar: true,
+  drawUnderStatusBar: true,
+  navBarTitleTextCentered: true,
+  navBarTextColor: Colors.white,
+};
+
+export default {
+  SPLASH_SCREEN: {
+    screen: 'Pangea.SplashScreen',
+    title: 'Splash',
+  },
+  DASHBOARD_SCREEN: {
+    screen: 'Pangea.DashboardScreen',
+    label: 'Dashboard',
+    icon: AssetsImages.TabIcons.dashboard,
+  },
+  CHAT_SCREEN: {
+    screen: 'Pangea.ChatScreen',
+    label: 'Chat',
+    icon: AssetsImages.TabIcons.chat,
+  },
+  NATIONS_SCREEN: {
+    screen: 'Pangea.NationsScreen',
+    label: 'Nations',
+    icon: AssetsImages.TabIcons.nations,
+  },
+  WALLET_SCREEN: {
+    screen: 'Pangea.WalletScreen',
+    label: 'Wallet',
+    icon: AssetsImages.TabIcons.wallet,
+  },
+  PROFILE_SCREEN: {
+    screen: 'Pangea.ProfileScreen',
+    label: 'Profile',
+    icon: AssetsImages.TabIcons.profile,
+    title: 'Profile',
+    navigatorStyle
+  },
+};
