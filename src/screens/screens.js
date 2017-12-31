eb83b1e66d49d410b81bb00bf5bcf1006f27a332
Kent <kent.dahlgren@gmail.com>
2017-12-30 21:39:13 -0800

    adding changes pulled from Andrew's work

--- src/screens/screens.js
+++ src/screens/screens.js
@@ -3,8 +3,19 @@ import { Navigation } from 'react-native-navigation';
 //import Drawer from './modules/_global/Drawer';
 import SplashScreen from './SplashScreen';
 import MainScreen from './MainScreen';
+import DashboardScreen from './DashboardScreen';
+import ChatScreen from './ChatScreen';
+import NationsScreen from './NationsScreen';
+import WalletScreen from './WalletScreen';
+import ProfileScreen from './ProfileScreen';
+import Screens from '../global/Screens';
+
 
 export function registerScreens(store, Provider) {
-    Navigation.registerComponent('Pangea.SplashScreen', () => SplashScreen, store, Provider);
-    Navigation.registerComponent('Pangea.MainScreen', () => MainScreen, store, Provider);
+  Navigation.registerComponent(Screens.SPLASH_SCREEN.screen, () => SplashScreen, store, Provider);
+  Navigation.registerComponent(Screens.DASHBOARD_SCREEN.screen, () => DashboardScreen, store, Provider);
+  Navigation.registerComponent(Screens.CHAT_SCREEN.screen, () => ChatScreen, store, Provider);
+  Navigation.registerComponent(Screens.NATIONS_SCREEN.screen, () => NationsScreen, store, Provider);
+  Navigation.registerComponent(Screens.WALLET_SCREEN.screen, () => WalletScreen, store, Provider);
+  Navigation.registerComponent(Screens.PROFILE_SCREEN.screen, () => ProfileScreen, store, Provider);
 }
