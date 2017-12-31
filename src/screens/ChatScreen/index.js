3e05a4820b8d2e243a0aeadf8806d2419e88895d
Kent <kent.dahlgren@gmail.com>
2017-12-31 12:56:21 -0800

    added a more dynamic chat capability using the react-native-gifted-chat library from Git. It handles changes in aspect ratio far better than the static image, but I need to modify the look and feel to bring it into alignment with the overall asthetic.

--- src/screens/ChatScreen/index.js
+++ src/screens/ChatScreen/index.js
@@ -1,25 +1,101 @@
 import React, { Component } from 'react';
-
 import {
   View,
-  Image
+  Text,
 } from 'react-native';
-
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-export default class Chat extends Component {
-  render() {
-    return (
+import { GiftedChat } from 'react-native-gifted-chat';
+import AssetsImages from "../../global/AssetsImages";
 
-      <Image
-      source={require("../../assets/images/staticChat.png")}
-      resizeMode="contain"
-      style={{flex:1, height: undefined, width: undefined}}
-      />
+class ChatScreen extends React.Component {
 
-    );
-  }
+    state = {
+        messages: [],
+    };
+
+    componentWillMount() {
+        this.setState({
+            messages: [
+                {
+                    _id: 1,
+                    text: 'Hello, David what\'s cracking?',
+                    createdAt: new Date(),
+                    user: {
+                        _id: 2,
+                        name: 'Girl',
+                       avatar: 'https://image.flaticon.com/icons/png/128/220/220126.png',
+                    },
+                    showUserAvatar : true,
+                },
+                {
+                    _id: 2,
+                    text: 'What\'s the status of our agreement? Can we schedule a call for later this week?',
+                    createdAt: new Date(Date.UTC(2016, 5, 11, 17, 20, 0)),
+                    user: {
+                        _id: 3,
+                        name: 'Piggie',
+                        avatar: 'https://image.flaticon.com/icons/png/128/220/220127.png',
+                    },
+                    showUserAvatar : true,
+                    // Any additional custom parameters are passed through
+                },
+                {
+                    _id: 3,
+                    text: 'My message ',
+                    createdAt: new Date(Date.UTC(2016, 5, 11, 17, 20, 0)),
+                    user: {
+                        _id: 1,
+                        name: 'David',
+                    },
+                    // Any additional custom parameters are passed through
+                },
+            ],
+        });
+    }
+
+    onSend(messages = []) {
+        this.setState((previousState) => ({
+            messages: GiftedChat.append(previousState.messages, messages),
+        }));
+    }
+
+    render() {
+        return (
+
+            <GiftedChat
+                messages={this.state.messages}
+                onSend={(messages) => this.onSend(messages)}
+                user={{
+                    _id: 1,
+                }}
+            />
+
+        );
+        return (
+          <View
+            style={{
+              flexDirection: 'row',
+              height: 100,
+              padding: 20,
+            }}>
+              <View style={{backgroundColor: 'blue', flex: 0.3}} />
+          </View>
+        );
+    }
+
 }
 
-//const styles = {flex:1, height: undefined, width: undefined};
+
+
+
+
+const mapStateToProps = state => ({
+  ...state,
+});
+
+const mapDispatchToProps = dispatch => ({
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ChatScreen);
