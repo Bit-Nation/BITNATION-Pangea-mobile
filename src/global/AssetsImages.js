eee74d83c89b28ffcfc7aec0f80ebf5888f1e532
Kent <kent.dahlgren@gmail.com>
2017-12-30 21:38:03 -0800

    fixing some path errors on my end and playing with Andrew's code

--- src/global/AssetsImages.js
+++ src/global/AssetsImages.js
@@ -1,7 +1,29 @@
 const logo = require('../assets/images/logo.png');
+const background = require('../assets/images/background.jpg');
+
+const dashboardTabIcon = require('../assets/images/dashboardTabIcon.png');
+const chatTabIcon = require('../assets/images/chatTabIcon.png');
+const nationsTabIcon = require('../assets/images/nationsTabIcon.png');
+const walletTabIcon = require('../assets/images/walletTabIcon.png');
+const profileTabIcon = require('../assets/images/profileTabIcon.png');
+
+// const holonsPlaceholder = require('../assets/images/holonsPlaceholder.png');
+// const achievementsPlaceholder = require('../assets/images/achievementsPlaceholder.png');
 
 const Images = {
-  logo
+  logo,
+  background,
+  TabIcons: {
+    dashboard: dashboardTabIcon,
+    chat: chatTabIcon,
+    nations: nationsTabIcon,
+    wallet: walletTabIcon,
+    profile: profileTabIcon,
+  },
+//  Placeholder: {
+//    holons: holonsPlaceholder,
+//    achievements: achievementsPlaceholder,
+//  }
 };
 
 export default Images;
