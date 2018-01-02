fd7eadb9a83e71ce3ad5ef424721b497f730dac2
Andrii Selivanov <seland2008@gmail.com>
2018-01-02 18:15:09 +0200

    [project] Fix merge.

--- index.js
+++ index.js
@@ -1,28 +1,6 @@
-
-import React, { Component } from 'react';
-import {
-    View,
-    Text,
-    StyleSheet,
-    Image,
-    AppRegistry
-} from 'react-native';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-
 import './shim';
 import { AppRegistry } from 'react-native';
 
 import App from './App';
 
-
-const mapStateToProps = state => ({
-                                  ...state,
-                                  });
-
-const mapDispatchToProps = dispatch => ({
-});
-
 AppRegistry.registerComponent('Pangea', () => App);
-
-export default connect(mapStateToProps, mapDispatchToProps);
