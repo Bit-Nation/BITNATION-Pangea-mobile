0c384b716db99b49d44aec96078a1e219d74c312
Andrii Selivanov <seland2008@gmail.com>
2018-01-02 18:16:54 +0200

    [project] Apply styling to chat screen.

--- src/screens/ChatScreen/index.js
+++ src/screens/ChatScreen/index.js
@@ -2,99 +2,103 @@ import React, { Component } from 'react';
 import {
   View,
   Text,
-  Image
+  Image,
+  Platform,
 } from 'react-native';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import styles from './styles';
-import { GiftedChat } from 'react-native-gifted-chat';
-import AssetsImages from "../../global/AssetsImages";
+import { GiftedChat, Composer, InputToolbar } from 'react-native-gifted-chat';
+import AssetsImages from '../../global/AssetsImages';
 
 import BackgroundImage from '../../components/common/BackgroundImage';
-
+import FakeNavigationBar from '../../components/common/FakeNavigationBar';
 
 
 class ChatScreen extends React.Component {
 
-    state = {
-        messages: [],
-    };
+  state = {
+    messages: [],
+  };
 
 
-    componentWillMount() {
-        this.setState({
-            messages: [
-                {
-                    _id: 1,
-                    text: 'Hello, David what\'s cracking?',
-                    createdAt: new Date(),
-                    user: {
-                        _id: 2,
-                        name: 'Girl',
-                       avatar: 'https://image.flaticon.com/icons/png/128/220/220126.png',
-                    },
-                    showUserAvatar : true,
-                },
-                {
-                    _id: 2,
-                    text: 'What\'s the status of our agreement? Can we schedule a call for later this week?',
-                    createdAt: new Date(Date.UTC(2016, 5, 11, 17, 20, 0)),
-                    user: {
-                        _id: 3,
-                        name: 'Piggie',
-                        avatar: 'https://image.flaticon.com/icons/png/128/220/220127.png',
-                    },
-                    showUserAvatar : true,
-                    // Any additional custom parameters are passed through
-                },
-                {
-                    _id: 3,
-                    text: 'My message ',
-                    createdAt: new Date(Date.UTC(2016, 5, 11, 17, 20, 0)),
-                    user: {
-                        _id: 1,
-                        name: 'David',
-                    },
-                    // Any additional custom parameters are passed through
-                },
-            ],
-        });
-    }
+  componentWillMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello, David what\'s cracking?',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'Girl',
+            avatar: 'https://image.flaticon.com/icons/png/128/220/220126.png',
+          },
+          showUserAvatar: true,
+        },
+        {
+          _id: 2,
+          text: 'What\'s the status of our agreement? Can we schedule a call for later this week?',
+          createdAt: new Date(Date.UTC(2016, 5, 11, 17, 20, 0)),
+          user: {
+            _id: 3,
+            name: 'Piggie',
+            avatar: 'https://image.flaticon.com/icons/png/128/220/220127.png',
+          },
+          showUserAvatar: true,
+          // Any additional custom parameters are passed through
+        },
+        {
+          _id: 3,
+          text: 'My message ',
+          createdAt: new Date(Date.UTC(2016, 5, 11, 17, 20, 0)),
+          user: {
+            _id: 1,
+            name: 'David',
+          },
+          // Any additional custom parameters are passed through
+        },
+      ],
+    });
+  }
 
-    onSend(messages = []) {
-        this.setState((previousState) => ({
-            messages: GiftedChat.append(previousState.messages, messages),
-        }));
-    }
+  onSend(messages = []) {
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+  }
 
-    render() {
-        return (
-          <View style={{ flex: 1 }}>
-            <BackgroundImage/>
+  render() {
+    return (
+      <View style={styles.container}>
+        <BackgroundImage/>
+        <FakeNavigationBar navBarHidden/>
 
-
-            <GiftedChat
-                messages={this.state.messages}
-                onSend={(messages) => this.onSend(messages)}
-                user={{
-                    _id: 1,
-                }}
-            />
-          </View>
-        );
-    }
+        <GiftedChat
+          messages={this.state.messages}
+          onSend={(messages) => this.onSend(messages)}
+          user={{
+            _id: 1,
+          }}
+          bottomOffset={Platform.OS == 'ios' ? 48.5 : 0}
+          renderComposer={(props) =>
+            <Composer {...props} textInputStyle={styles.composer}/>
+          }
+          renderInputToolbar={(props) =>
+            <InputToolbar {...props} containerStyle={styles.inputToolbar}/>
+          }
+        />
+      </View>
+    );
+  }
 
 }
-
-
-
 
 
 const mapStateToProps = state => ({
   ...state,
 });
 
-const mapDispatchToProps = dispatch => ({
-});
+const mapDispatchToProps = dispatch => ({});
 
 export default connect(mapStateToProps, mapDispatchToProps)(ChatScreen);
--- src/screens/ChatScreen/styles.js
+++ src/screens/ChatScreen/styles.js
@@ -4,64 +4,18 @@ import Colors from '../../global/Colors';
 const styles = MediaQueryStyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'stretch',
   },
-  infoContainer: {
-    paddingBottom: 20,
-    paddingTop: 10,
-    marginLeft: 8,
-    marginRight: 8,
-    borderRadius: 8,
-    backgroundColor: Colors.getBitNationBlue(0.2),
-    alignItems: 'center',
+  composer: {
+    backgroundColor: Colors.getBitNationLightBlue(0.2),
+    borderWidth: 1,
+    borderColor: Colors.borderColor,
+    color: Colors.white,
+    paddingLeft: 4,
   },
-  header: {
+  inputToolbar: {
     backgroundColor: 'transparent',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'space-around',
-    marginBottom: 20,
+    borderTopWidth: 0,
   },
-  achievementsContainer: {
-    marginTop: 8,
-    marginBottom: 10,
-  },
-  placeholder: {
-    resizeMode: 'contain',
-  },
-  avatar: {
-    height: 100,
-    width: 100,
-    borderRadius: 50,
-  },
-  nameText: {
-    fontSize: 28,
-    color: Colors.titleColor,
-    letterSpacing: -1.13,
-    textAlign: 'center',
-    backgroundColor: 'transparent'
-  },
-  infoText: {
-    fontSize: 16,
-    color: Colors.BitNationLightBlue,
-    letterSpacing: -0.65,
-    lineHeight: 20.8,
-    textAlign: 'center',
-    backgroundColor: 'transparent'
-  },
-  ethAddress: {
-    fontSize: 14,
-    color: Colors.BitNationLightBlue,
-    letterSpacing: -0.65,
-    lineHeight: 20.8,
-    textAlign: 'center',
-    backgroundColor: 'transparent'
-  },
-  fakeNavigationBar: {
-    height: 64,
-    backgroundColor: 'transparent',
-  }
 });
 
 export default styles;
