ff7fe15cdaa81a8723a91cee9d8980746c353560
Florian Lenz <flenz81@googlemail.com>
2018-01-02 02:23:46 +0100

    [app] fixed formatting in App.js

--- App.js
+++ App.js
@@ -11,11 +11,11 @@ registerScreens(store, Provider);
 // const navigatorStyle = { navBarHidden: true }
 Navigation.startTabBasedApp({
   tabs: [
-     Screens.DASHBOARD_SCREEN,
-    Screens.CHAT_SCREEN,
-    Screens.NATIONS_SCREEN,
-   Screens.WALLET_SCREEN,
-   Screens.PROFILE_SCREEN,
+      Screens.DASHBOARD_SCREEN,
+      Screens.CHAT_SCREEN,
+      Screens.NATIONS_SCREEN,
+      Screens.WALLET_SCREEN,
+      Screens.PROFILE_SCREEN,
   ],
   tabsStyle,
   appStyle,
