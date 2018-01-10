03b076892b3f1d19b13e47db7a6e434d256478c9
Andrii Selivanov <seland2008@gmail.com>
2018-01-11 01:09:30 +0200

    [project] Adjust create key success screen.

--- src/screens/Key/Create/CreateKeySuccessScreen/index.js
+++ src/screens/Key/Create/CreateKeySuccessScreen/index.js
@@ -1,46 +1,41 @@
 import React, { Component } from 'react';
 import {
   View,
-  Text,
-  Image,
-  StatusBar,
-  TouchableOpacity
 } from 'react-native';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Navigation } from 'react-native-navigation';
-import Images from '../../../../global/AssetsImages';
-import styles from './styles';
-import Colors from '../../../../global/Colors';
-import Header from '../../../../components/Header';
-import { screen } from '../../../../global/Screens';
-import Button from '../../../../components/common/Button';
 
-class CreateKeySuccessScreen extends Component {
+import styles from './styles';
+import Button from '../../../../components/common/Button';
+import BackgroundImage from '../../../../components/common/BackgroundImage';
+import Text from '../../../../components/common/Text';
+import FakeNavigationBar from '../../../../components/common/FakeNavigationBar';
+import CreateKeyBaseScreen from '../CreateKeyBaseScreen/index';
+
+class CreateKeySuccessScreen extends CreateKeyBaseScreen {
 
   onNextButtonPressed() {
-    this.props.navigator.popToRoot();
-  }
-
-  onCancelButtonPressed() {
-    alert('cancel');
+    this.props.navigator.dismissModal();
   }
 
   render() {
     return (
 
       <View style={styles.container}>
-        <Image
-          style={styles.backgroundImage}
-          source={Images.background}
-        />
+        <BackgroundImage/>
+        <FakeNavigationBar/>
 
         <View style={styles.wholeText}>
-
-          <Text style={styles.description}>Congratulations, you now have a private key.</Text>
-          <Text style={styles.description}>Remember, your private key is the only way to restore your wallet. If your phone is lost, stolen, broken, or upgraded, you must have a copy of private key, or you will lose all your money!</Text>
-          <Text style={[styles.description, styles.applyBold]}>Put your paper with your private key in a safe place.</Text>
-
+          <Text messageText style={styles.description}>
+            Congratulations, you now have a private key.
+          </Text>
+          <Text messageText style={styles.description}>
+            Remember, your private key is the only way to restore your wallet. If your phone is lost, stolen, broken, or
+            upgraded, you must have a copy of private key, or you will lose all your money!
+          </Text>
+          <Text messageText style={[styles.description, styles.applyBold]}>
+            Put your paper with your private key in a safe place.
+          </Text>
         </View>
 
         <View style={styles.buttonContainer}>
