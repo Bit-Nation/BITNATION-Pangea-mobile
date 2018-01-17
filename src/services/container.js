97e24d45e9cfafc021ea4758d26cf4ee776f1496
Florian Lenz <flenz81@googlemail.com>
2018-01-17 14:58:04 +0100

    [services] implemented network change

--- src/services/container.js
+++ src/services/container.js
@@ -4,17 +4,42 @@ import pangeaLibsFactory from 'BITNATION-Pangea-libs'
 import secureStorage from './panthalassa/secureStorage';
 import osDeps from './panthalassa/osDependencies';
 import ethDaemon from './panthalassa/ethDaemon';
+import {NetInfo} from 'react-native';
+import {APP_ONLINE, APP_OFFLINE} from 'BITNATION-Pangea-libs/src/events'
 const EventEmitter = require('eventemitter3');
 
 const DB_PATH = 'pangea';
 
-const ee = new EventEmitter();
+export default new Promise((res, rej) => {
 
-export default pangeaLibsFactory(
-    secureStorage,
-    DB_PATH,
-    ethDaemon,
-    osDeps,
-    ee,
-    false
-);
+    const ee = new EventEmitter();
+
+    /**
+     * @desc Inform pangea utils about connectivity change
+     */
+    NetInfo.isConnected.addEventListener('connectionChange', isConnected => {
+
+        if(false === isConnected){
+            ee.emit(APP_OFFLINE);
+            return;
+        }
+
+        ee.emit(APP_ONLINE);
+
+    });
+
+    NetInfo
+        .isConnected
+        .fetch()
+        .then(isConnected => pangeaLibsFactory(
+            secureStorage,
+            DB_PATH,
+            ethDaemon,
+            osDeps,
+            ee,
+            isConnected
+        ))
+        .then(res)
+        .catch(rej);
+
+})
