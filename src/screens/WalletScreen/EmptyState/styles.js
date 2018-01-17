555b32338b7302493529d1611f3e3297d59cbcf9
Andrii Selivanov <seland2008@gmail.com>
2018-01-17 23:29:09 +0200

    [project] Adjust styles on wallet empty state screen.

--- src/screens/WalletScreen/EmptyState/styles.js
+++ src/screens/WalletScreen/EmptyState/styles.js
@@ -1,78 +1,20 @@
-import { StyleSheet } from 'react-native';
-import { Dimensions, } from 'react-native';
+import { MediaQueryStyleSheet } from 'react-native-responsive';
 import Colors from '../../../global/Colors';
 
-var { height, width } = Dimensions.get('window');
-
-export default styles = StyleSheet.create({
-  backgroundScreen: {
-    position: 'absolute',
-    height: height,
-    width: width,
-    zIndex: -1,
-  },
-  header: {
-    fontSize: 30,
-    fontWeight: 'bold',
-    color: Colors.white,
-    marginTop: '5%',
-    marginLeft: '5%',
-    opacity: 1,
-    backgroundColor: Colors.Transparent,
-    paddingBottom: '10%',
-  },
-  cardMain: {
-    backgroundColor: '#3d75ad',
-    opacity: 0.3,
-    width: '90%',
-    marginLeft: '5%',
-    borderRadius: 5,
-    marginTop: height * 0.03,
+export default styles = MediaQueryStyleSheet.create({
+  container: {
+    flex: 1,
   },
   card: {
-    height: '30%',
-    marginLeft: '2.4%',
-    marginRight: '2.4%',
-  },
-  firstCard: {  // strange issue when use %
-    marginTop: 40,
-    marginBottom: 10,
+    marginLeft: 8,
+    marginRight: 8,
   },
   cardContainer: {
-    justifyContent: 'space-around',
-    position: 'absolute'
+    flex: 1,
+    justifyContent: 'flex-start',
   },
-  discpHeading: {
-    fontSize: 24,
-    color: Colors.white,
-    textAlign: 'center',
-    opacity: 1,
-  },
-
-  discpText: {
-    color: Colors.white,
-    fontSize: 16,
-    textAlign: 'center',
-    marginLeft: '3%',
-    marginRight: '3%',
-    opacity: 1,
-  },
-  discpButton: {
-    backgroundColor: Colors.BitnationBlue,
-    opacity: 1,
-    height: height * .05,
-    marginBottom: height * .02,
-    marginTop: height * 0.01,
-    borderRadius: 15,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  discpButtonText: {
-    color: Colors.white,
-    fontSize: 16,
-    opacity: 1,
-    paddingLeft: width * .05,
-    paddingRight: width * .05,
+  firstCard: {
+    marginBottom: 8,
   },
 });
   
