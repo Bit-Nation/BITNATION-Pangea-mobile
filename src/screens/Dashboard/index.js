fd936b46a545479ba9f4daf1f5faf258fececcd0
David Gross <davidigross@me.com>
2018-01-19 15:10:39 -0800

    Update Dashboard mockup

--- src/screens/Dashboard/index.js
+++ src/screens/Dashboard/index.js
@@ -1,204 +1,222 @@
 /*eslint-disable */
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
-  View,
-  Text,
-  Image,
-  ListView,
-  TouchableOpacity,
-} from 'react-native';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import Images from '../../global/AssetsImages';
-import styles from './styles';
-import TextBox from '../../components/TextBox';
-import ListModal from '../../components/ListModal';
-import BackgroundScreen from '../../components/BackgroundScreen/index';
-import FakeNavigationBar from '../../components/common/FakeNavigationBar';
+	View,
+	Text,
+	Image,
+	ListView,
+	TouchableOpacity,
+	ScrollView,
+} from 'react-native'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import Images from '../../global/AssetsImages'
+import styles from './styles'
+import TextBox from '../../components/TextBox'
+import ListModal from '../../components/ListModal'
+import BackgroundScreen from '../../components/BackgroundScreen/index'
+import FakeNavigationBar from '../../components/common/FakeNavigationBar'
+import PanelView from '../../components/common/PanelView'
 
 class Dashboard extends Component {
-  constructor(props) {
-    super(props);
-
-    const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
-    this.state = {
-      dataSource: ds.cloneWithRows([{
-        name: 'Label',
-        avatar_url: Images.rightArrow,
-        subtitle: 'Vice President'
-      },
-        {
-          name: 'Label',
-          avatar_url: Images.rightArrow,
-          subtitle: 'Vice Chairman'
-        },
-        {
-          name: 'Label',
-          avatar_url: Images.rightArrow,
-          subtitle: 'Vice Chairman'
-        },
-        {
-          name: 'Label',
-          avatar_url: Images.rightArrow,
-          subtitle: 'Vice Chairman'
-        },
-        {
-          name: 'Label',
-          avatar_url: Images.rightArrow,
-          subtitle: 'Vice Chairman'
-        }, {
-          name: 'Label',
-          avatar_url: Images.rightArrow,
-          subtitle: 'Vice Chairman'
-        }, {
-          name: 'Label',
-          avatar_url: Images.rightArrow,
-          subtitle: 'Vice Chairman'
-        }, {
-          name: 'Label',
-          avatar_url: Images.rightArrow,
-          subtitle: 'Vice Chairman'
-        },
-
-      ]),
-    };
-  }
-
-  listDemo() {
-    return (
-      <View style={styles.cFlex}>
-
-        <View style={styles.chatTextContainer}>
-          <Text style={styles.chatsText}>Chats</Text>
-
-        </View>
-        <View style={styles.listContainer}>
-          <ListView
-            dataSource={this.state.dataSource}
-            renderRow={(data) =>
-              <TouchableOpacity style={styles.containerList}>
-                <View style={styles.listName}>
-                  <Text style={styles.text}>
-                    {data.name}
-                  </Text>
-                </View>
-                <View style={styles.listIcon}>
-                  <Image source={data.avatar_url} style={styles.photo2}/>
-                </View>
-
-              </TouchableOpacity>}
-
-            // renderSeparator={(sectionId, rowId) => <View key={rowId} style={styles.separator}/>}
-          />
-        </View>
-
-      </View>
-    );
-  }
-
-  render() {
-    return (
-
-      <View style={styles.container}>
-        <FakeNavigationBar navBarHidden/>
-        <BackgroundScreen/>
-        <View style={styles.firstContainer}>
-
-          <View style={styles.NWContainer}>
-
-
-            <ListModal
-              title="Nations"
-              flexNumber={2}
-              marginBottom={3}
-              marginLeft={2.5}
-              data={this.state.dataSource}/>
-
-            <ListModal
-              title="Wallet"
-              flexNumber={1}
-              marginLeft={2.5}
-              data={this.state.dataSource}/>
-
-
-          </View>
-
-
-          <ListModal
-            title="Activity"
-            flexNumber={1.5}
-            marginRight={2.6}
-            marginLeft={0}
-            marginBottom={0}
-            data={this.state.dataSource}/>
-        </View>
-
-
-        <View style={styles.ChatNationContainer}>
-          <View style={styles.chatContainer}>
-
-            <Image style={styles.demo}
-                   source={Images.demo}
-                   resizeMode="contain"
-            />
-            {this.listDemo()}
-
-          </View>
-          <View style={styles.contractsContainer}>
-            <Image style={styles.demoContracts}
-                   source={Images.demo}
-                   resizeMode="contain"
-            />
-            <ListModal
-              title="Contracts"
-              flexNumber={1}
-              marginRight={0}
-              marginLeft={0}
-              marginBottom={0}
-              data={this.state.dataSource}/>
-          </View>
-
-        </View>
-
-        <View style={styles.secondContainer}>
-
-
-          <Image style={styles.demoWorld}
-                 source={Images.demo}
-                 resizeMode="contain"
-          />
-
-
-          <Text style={styles.secondText}>Around the World
-          </Text>
-
-          <View style={styles.sImage}>
-            <Image
-              style={styles.secondImageContainer}
-              source={Images.logo}
-              resizeMode="contain"
-            />
-          </View>
-
-
-        </View>
-
-
-      </View>
-
-    );
-  }
-
+	constructor (props) {
+		super(props)
+		
+		const ds = new ListView.DataSource(
+			{rowHasChanged: (r1, r2) => r1 !== r2})
+		this.state = {
+			dataSource: ds.cloneWithRows([
+				{
+					name: 'Label',
+					avatar_url: Images.rightArrow,
+					subtitle: 'Vice President',
+				},
+				{
+					name: 'Label',
+					avatar_url: Images.rightArrow,
+					subtitle: 'Vice Chairman',
+				},
+				{
+					name: 'Label',
+					avatar_url: Images.rightArrow,
+					subtitle: 'Vice Chairman',
+				},
+				{
+					name: 'Label',
+					avatar_url: Images.rightArrow,
+					subtitle: 'Vice Chairman',
+				},
+				{
+					name: 'Label',
+					avatar_url: Images.rightArrow,
+					subtitle: 'Vice Chairman',
+				}, {
+					name: 'Label',
+					avatar_url: Images.rightArrow,
+					subtitle: 'Vice Chairman',
+				}, {
+					name: 'Label',
+					avatar_url: Images.rightArrow,
+					subtitle: 'Vice Chairman',
+				}, {
+					name: 'Label',
+					avatar_url: Images.rightArrow,
+					subtitle: 'Vice Chairman',
+				},
+			
+			]),
+		}
+	}
+	
+	listDemo () {
+		return (
+			<View style={styles.cFlex}>
+				
+				<View style={styles.chatTextContainer}>
+					<Text style={styles.chatsText}>Chats</Text>
+				
+				</View>
+				<View style={styles.listContainer}>
+					<ListView
+						dataSource={this.state.dataSource}
+						renderRow={(data) =>
+							<TouchableOpacity style={styles.containerList}>
+								<View style={styles.listName}>
+									<Text style={styles.text}>
+										{data.name}
+									</Text>
+								</View>
+								<View style={styles.listIcon}>
+									<Image source={data.avatar_url}
+									       style={styles.photo2}/>
+								</View>
+							
+							</TouchableOpacity>}
+						
+						// renderSeparator={(sectionId, rowId) => <View key={rowId} style={styles.separator}/>}
+					/>
+				</View>
+			
+			</View>
+		)
+	}
+	
+	render () {
+		return (
+			<View style={styles.screenContainer}>
+				<BackgroundScreen/>
+				<FakeNavigationBar navBarHidden/>
+				<View style={styles.bodyContainer}>
+					<ScrollView style={styles.scrollView}>
+						
+						{this._panel()}
+						
+						<View style={styles.ChatNationContainer}>
+							<View style={styles.chatContainer}>
+								
+								<Image style={styles.demo}
+								       source={Images.demo}
+								       resizeMode="contain"
+								/>
+								{this.listDemo()}
+							
+							</View>
+							<View style={styles.contractsContainer}>
+								<Image style={styles.demoContracts}
+								       source={Images.demo}
+								       resizeMode="contain"
+								/>
+								<ListModal
+									title="Contracts"
+									flexNumber={1}
+									marginRight={0}
+									marginLeft={0}
+									marginBottom={0}
+									data={this.state.dataSource}/>
+							</View>
+						
+						</View>
+						
+						<View style={styles.secondContainer}>
+							
+							
+							<Image style={styles.demoWorld}
+							       source={Images.demo}
+							       resizeMode="contain"
+							/>
+							
+							
+							<Text style={styles.secondText}>Around the World
+							</Text>
+							
+							<View style={styles.sImage}>
+								<Image
+									style={styles.secondImageContainer}
+									source={Images.logo}
+									resizeMode="contain"
+								/>
+							</View>
+						
+						</View>
+					</ScrollView>
+				
+				</View>
+			
+			</View>
+		
+		)
+	}
+	
+	/* ------------------------- */
+	/* FUNCTIONS */
+	/* ------------------------- */
+	
+	/* ------------------------- */
+// Useful Notes:
+// PanelView Props: title = text, messageText = text, style, renderBottom = method, renderAdditionalInfo = method, children = main text of the display
+// DemoImage overlays a message telling user this is a demonstration
+	
+	/* ------------------------- */
+	
+	_panel () {
+		return (
+			<PanelView style={styles.panelView} title={`Panel Title`} icon={'?'}
+			           body={'XXXLorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.'}>
+{/*
+			<Text style={styles.body}>
+					Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+					sed diam nonummy nibh euismod tincidunt ut laoreet dolore
+					magna aliquam erat volutpat. Ut wisi enim ad minim veniam,
+					quis nostrud exerci tation ullamcorper suscipit lobortis
+					nisl ut aliquip ex ea commodo consequat.
+				</Text>
+*/}
+				
+				{/*<View style={styles.NWContainer}>
+					<ListModal
+						title='Nations'
+						flexNumber={2}
+						marginBottom={3}
+						marginLeft={2.5}
+						data={this.state.dataSource}/>
+				</View>*/}
+			</PanelView>
+		)
+	}
+	
 }
 
-Dashboard.propTypes = {};
+/* ------------------------- */
+/* PROPS */
 
-Dashboard.defaultProps = {};
+Dashboard.propTypes = {}
+
+Dashboard.defaultProps = {}
 
 const mapStateToProps = state => ({
-  ...state,
-});
+	...state,
+})
 
-const mapDispatchToProps = dispatch => ({});
+const mapDispatchToProps = dispatch => ({})
 
-export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)
--- src/screens/Dashboard/styles.js
+++ src/screens/Dashboard/styles.js
@@ -1,231 +1,222 @@
 /*eslint-disable*/
-import { MediaQueryStyleSheet } from 'react-native-responsive';
-import Colors from '../../global/Colors';
-import {Dimensions} from 'react-native';
+import { MediaQueryStyleSheet } from 'react-native-responsive'
+import { Dimensions } from 'react-native'
+import Colors from '../../global/Colors'
+import GlobalStyles from '../../global/Styles'
 
-var {height, width} = Dimensions.get('window');
+var {height, width} = Dimensions.get('window')
+
 const styles = MediaQueryStyleSheet.create(
-  {
-
-    containerList: {
-        flex: 1,
-        flexDirection: 'row',
-        //alignItems: 'center',
-
-      },
-      listName: {
-          marginRight:'10%',
-          width:'80%',
-          justifyContent:'center',
- 
-
-      },
-      text: {
-        marginLeft: '3%',
-        fontSize: 16,
-        color:'white',
-        marginTop:'3%',
-        marginBottom:'4%',
-      },
-
-      listIcon: {
-          
-        justifyContent:'flex-start',
-
-      },
-      photo2: {
-        height: 10,
-        width: 10,
-        borderRadius: 20,
-      },
-      line2: {
-          backgroundColor:'black',
-          width:'100%',
-          marginLeft:'2%',
-          height:1
-
-      },
-      separator: {
-        width:'100%',
-        marginLeft:'3%',
-        height: 1,
-        backgroundColor:Colors.GraySilver,
-      },
-
-    container: {
-      flexDirection:'column',
-      flex:1,
-
-    },
-    backImage : {
-        position: 'absolute',
-        zIndex:-1,
-
-    },
-    firstContainer:
-    {
-        flex:2,
-        flexDirection:'row',
-        marginBottom:'1.6%',
-        opacity:0.6,
-    },
-
-
-    //Nation, Wallet
-    NWContainer:
-    {
-        flex:1,
-        flexDirection:'column',
-        marginRight:'1.7%',
-
-    },
-    NationsContainer: {
-        flex:2,
-        backgroundColor:Colors.BitnationBlue,
-        marginBottom: '5%',
-        marginLeft:'5%',
-        borderRadius:8,
-
-    },
-
-
-  
-    WalletContainer: {
-        flex:1,
-        backgroundColor:Colors.BitnationBlue,
-        marginBottom: '1%',
-        marginLeft:'5%',
-        borderRadius:6
-
-    },
-
-    cFlex: {
-
-        flex:1,
-    },
-    chatTextContainer :
-    {
-        flex:1,
-    },
-    chatsText: {
-        color:'white',
-        fontWeight:'bold',
-        textAlign:'center',
-        marginTop:'1%'
-    },
-
-    listContainer: {
-        flex:3
-    },
-    //Activity
-    ActivityContainer:
-    {
-        flex:1.5,
-        flexDirection:'row',
-        backgroundColor:Colors.BitnationBlue,
-        marginRight:'2%',
-        borderRadius:8
-
-    },
-
-
-    
-    ChatNationContainer: {
-        flex:1,
-        flexDirection:'row',
-        opacity:0.6,
-    },
-
-    chatContainer: {
-        flex:1.5,
-        backgroundColor:Colors.BitnationBlue,
-        borderRadius:6,
-        marginLeft:'2%',
-        marginRight:'2%',
-        marginBottom:'2%'
-      
-
-    },
-    demo: {
-        position:'absolute',
-        marginLeft:'20%',
-        marginTop:'10%',
-        width:80,
-        height:60,
-        zIndex:10
-
-    },
-    demoContracts: {
-        position:'absolute',
-        marginLeft:'20%',
-        marginTop:'10%',
-        width:100,
-        height:80,
-        zIndex:10,
-
-    },
-
-    logoW: {
-        backgroundColor:'red',
-        width:'100%',
-        height:'90%',
-        zIndex:20,
-        position:'absolute'
-
-    },
-
-    contractsContainer:{
-        flex:1,
-        backgroundColor:Colors.BitnationBlue,
-        borderRadius:6,
-        marginRight:'2%',
-        marginBottom:'2%'
-
-    },
-
-
-    secondContainer: {
-        flex:1,
-        backgroundColor:Colors.BitnationBlue,
-        borderRadius:8,
-        paddingRight:'4%',
-        paddingLeft:'4%',
-        paddingTop:'3%',
-        flexDirection:'column',
-        opacity:0.6,
-        marginLeft:'2%',
-        marginRight:'2%',
-    },  
-
-    secondText: {
-        textAlign:'center',
-        fontWeight:'bold',
-        color:'white'
-    },
-    demoWorld: {
-        position:'absolute',
-        marginLeft:'30%',
-        marginTop:'10%',
-        width:100,
-        height:80,
-        zIndex:10
-    
-    },
-    sImage: {
-       
-        //backgroundColor: 'black'
-         //backgroundColor:'red'
-         //position:'absolute'
-    },
-    secondImageContainer: {
-        //backgroundColor: Colors.Transparent,
-        marginTop:'5%',
-        marginLeft:'5%',
-        marginRight:'5%',
-       
-       // position:'absolute',
-
-     },
-
-});
-export default styles;
+	{
+		...GlobalStyles,
+		
+		containerList: {
+			flex: 1,
+			flexDirection: 'row',
+			//alignItems: 'center',
+			
+		},
+		listName: {
+			marginRight: '10%',
+			width: '80%',
+			justifyContent: 'center',
+			
+		},
+		text: {
+			...GlobalStyles.body,
+		},
+		
+		listIcon: {
+			
+			justifyContent: 'flex-start',
+			
+		},
+		photo2: {
+			height: 10,
+			width: 10,
+			borderRadius: 20,
+		},
+		line2: {
+			backgroundColor: 'black',
+			width: '100%',
+			marginLeft: '2%',
+			height: 1,
+			
+		},
+		separator: {
+			width: '100%',
+			marginLeft: '3%',
+			height: 1,
+			backgroundColor: Colors.GraySilver,
+		},
+		
+		container: {
+			flexDirection: 'column',
+			flex: 1,
+			
+		},
+		backImage: {
+			position: 'absolute',
+			zIndex: -1,
+			
+		},
+		firstContainer:
+			{
+				flex: 2,
+				flexDirection: 'row',
+				marginBottom: '1.6%',
+				opacity: 0.6,
+			},
+		
+		//Nation, Wallet
+		NWContainer:
+			{
+				flex: 1,
+				flexDirection: 'column',
+				marginRight: '1.7%',
+				
+			},
+		NationsContainer: {
+			flex: 2,
+			backgroundColor: Colors.BitnationBlue,
+			marginBottom: '5%',
+			marginLeft: '5%',
+			borderRadius: 8,
+			
+		},
+		
+		WalletContainer: {
+			flex: 1,
+			backgroundColor: Colors.BitnationBlue,
+			marginBottom: '1%',
+			marginLeft: '5%',
+			borderRadius: 6,
+			
+		},
+		
+		cFlex: {
+			
+			flex: 1,
+		},
+		chatTextContainer:
+			{
+				flex: 1,
+			},
+		chatsText: {
+			color: 'white',
+			fontWeight: 'bold',
+			textAlign: 'center',
+			marginTop: '1%',
+		},
+		
+		listContainer: {
+			flex: 3,
+		},
+		//Activity
+		ActivityContainer:
+			{
+				flex: 1.5,
+				flexDirection: 'row',
+				backgroundColor: Colors.BitnationBlue,
+				marginRight: '2%',
+				borderRadius: 8,
+				
+			},
+		
+		ChatNationContainer: {
+			flex: 1,
+			flexDirection: 'row',
+			opacity: 0.6,
+		},
+		
+		chatContainer: {
+			flex: 1.5,
+			backgroundColor: Colors.BitnationBlue,
+			borderRadius: 6,
+			marginLeft: '2%',
+			marginRight: '2%',
+			marginBottom: '2%',
+			
+		},
+		demo: {
+			position: 'absolute',
+			marginLeft: '20%',
+			marginTop: '10%',
+			width: 80,
+			height: 60,
+			zIndex: 10,
+			
+		},
+		demoContracts: {
+			position: 'absolute',
+			marginLeft: '20%',
+			marginTop: '10%',
+			width: 100,
+			height: 80,
+			zIndex: 10,
+			
+		},
+		
+		logoW: {
+			backgroundColor: 'red',
+			width: '100%',
+			height: '90%',
+			zIndex: 20,
+			position: 'absolute',
+			
+		},
+		
+		contractsContainer: {
+			flex: 1,
+			backgroundColor: Colors.BitnationBlue,
+			borderRadius: 6,
+			marginRight: '2%',
+			marginBottom: '2%',
+			
+		},
+		
+		secondContainer: {
+			flex: 1,
+			backgroundColor: Colors.BitnationBlue,
+			borderRadius: 8,
+			paddingRight: '4%',
+			paddingLeft: '4%',
+			paddingTop: '3%',
+			flexDirection: 'column',
+			opacity: 0.6,
+			marginLeft: '2%',
+			marginRight: '2%',
+		},
+		
+		secondText: {
+			textAlign: 'center',
+			fontWeight: 'bold',
+			color: 'white',
+		},
+		demoWorld: {
+			position: 'absolute',
+			marginLeft: '30%',
+			marginTop: '10%',
+			width: 100,
+			height: 80,
+			zIndex: 10,
+			
+		},
+		sImage: {
+			
+			//backgroundColor: 'black'
+			//backgroundColor:'red'
+			//position:'absolute'
+		},
+		secondImageContainer: {
+			//backgroundColor: Colors.Transparent,
+			marginTop: '5%',
+			marginLeft: '5%',
+			marginRight: '5%',
+			
+			// position:'absolute',
+			
+		},
+		
+	})
+export default styles
