c551496797fee626a8c7f84ccc75691eac10abe7
Alberto Estarrona <alberto@estarrona.me>
2018-01-25 18:32:35 -0600

    [project] Comments added to function

--- src/utils/normalizer.js
+++ src/utils/normalizer.js
@@ -5,6 +5,11 @@ var {
 
 var deviceHeight = Dimensions.get('window').height;
 
+/**
+ * Funtion to normalize Font sizes depending on screen size
+ * @param size The Font Size
+ * @returns {integer} The new font size depending the current screen
+ */
 export function normalizer (size) {
   if(deviceHeight === 568) {
     return size / 0.65
