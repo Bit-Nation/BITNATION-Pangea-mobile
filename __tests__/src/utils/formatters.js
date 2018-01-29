40b7f8b26c6fbcd3142b5413f87b7bac51b4c3af
Florian Lenz <flenz81@googlemail.com>
2018-01-29 08:30:45 +0100

    [tests] added formatters test

--- __tests__/src/utils/formatters.js
+++ __tests__/src/utils/formatters.js
@@ -1,7 +1,13 @@
-import {roundEth} from '../../../src/utils/formatters';
+import {roundEth, shortEthAddress} from '../../../src/utils/formatters';
 
 test('roundEth', () => {
 
     expect(roundEth('0.03452345234')).toBe('0.03452');
+
+});
+
+test('shortEthAddress', () => {
+
+    expect(shortEthAddress('0x6e5d44060fac77c126a75fa1f7bfaf224ab52c1e')).toBe('0x6e5...52c1e');
 
 });
