1d787ed3dc7210db44ea1ef3503d2bd5d7d9a77a
Andrii Selivanov <seland2008@gmail.com>
2018-01-30 18:34:44 +0200

    [tests] Loading component.

---
+++ __tests__/src/components/common/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Loading from '../../../../src/components/common/Loading';
+
+test('Loading renders correctly', () => {
+  const tree = renderer
+    .create(<Loading/>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
