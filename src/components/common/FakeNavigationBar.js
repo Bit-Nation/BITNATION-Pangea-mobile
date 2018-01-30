8495e66868249a438c556ec3156a2497f3665191
Andrii Selivanov <seland2008@gmail.com>
2018-01-30 18:09:14 +0200

    [docs] FakeNavigationBar component.

--- src/components/common/FakeNavigationBar.js
+++ src/components/common/FakeNavigationBar.js
@@ -4,6 +4,11 @@ import { MediaQueryStyleSheet } from 'react-native-responsive';
 
 import GlobalStyles from '../../global/Styles';
 
+/**
+ * @desc Component is used to add margin on top of the screen.
+ * @discussion This component is created to be used together with background image for the whole screen (including status/navigation bar), but content rendered below navigation bar.
+ * @todo Create component for global screen margins handling and deprecate this one.
+ */
 export default class FakeNavigationBar extends Component {
 
   render() {
