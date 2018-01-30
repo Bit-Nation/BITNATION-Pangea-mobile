0ccd671b086d6f213b883d4b9e02dbf8a3d82c18
Andrii Selivanov <seland2008@gmail.com>
2018-01-30 16:19:10 +0200

    [tests] Add tests for BackgroundImage component.

---
+++ __tests__/src/components/common/BackgroundImage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import BackgroundImage from '../../../../src/components/common/BackgroundImage';
+import renderer from 'react-test-renderer';
+
+test('BackgroundImage renders correctly', () => {
+  const tree = renderer
+    .create(<BackgroundImage/>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
