40f33db7a2224aa2c4e214af8ae31f39368a3de8
Andrii Selivanov <seland2008@gmail.com>
2018-01-30 16:26:28 +0200

    [refactor] BackgroundImage tests.

--- __tests__/src/components/common/BackgroundImage.js
+++ __tests__/src/components/common/BackgroundImage.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import BackgroundImage from '../../../../src/components/common/BackgroundImage';
 import renderer from 'react-test-renderer';
+
+import BackgroundImage from '../../../../src/components/common/BackgroundImage';
 
 test('BackgroundImage renders correctly', () => {
   const tree = renderer
