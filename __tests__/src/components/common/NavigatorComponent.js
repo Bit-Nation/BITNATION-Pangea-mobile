115944739ef6c1742c810a4537d2468e0380ba21
Andrii Selivanov <seland2008@gmail.com>
2018-01-31 16:21:19 +0200

    [tests] Improve tests for NavigatorComponent.

--- __tests__/src/components/common/NavigatorComponent.js
+++ __tests__/src/components/common/NavigatorComponent.js
@@ -2,32 +2,30 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import { Navigator } from 'react-native-navigation';
 
-jest.mock('react-native-navigation');
-
 import NavigatorComponent from '../../../../src/components/common/NavigatorComponent';
 
 describe('NavigatorComponent tests', () => {
 
   test('NavigatorComponent passes navigation button press event', () => {
-    const navigatorMock = new Navigator();
-    const component = new NavigatorComponent({ navigator: navigatorMock });
+    const navigator = new Navigator();
+    const component = new NavigatorComponent({ navigator });
     const mockFunc = jest.fn();
     const testButtonID = 'TEST_BUTTON_ID';
     const buttonPressMockEvent = { type: 'NavBarButtonPress', id: testButtonID };
 
     component.onNavBarButtonPress = mockFunc;
-    component.onNavigatorEvent(buttonPressMockEvent);
+    navigator.onNavigatorEvent(buttonPressMockEvent);
 
     expect(mockFunc).toHaveBeenCalledTimes(1);
     expect(mockFunc.mock.calls[0][0]).toBe(testButtonID);
   });
 
   function componentPassesEvent(callbackName, id) {
-    const navigatorMock = new Navigator();
-    const component = new NavigatorComponent({ navigator: navigatorMock });
+    const navigator = new Navigator();
+    const component = new NavigatorComponent({ navigator });
     const mockFunc = jest.fn();
     component[callbackName] = mockFunc;
-    component.onNavigatorEvent({ id });
+    navigator.onNavigatorEvent({ id });
     expect(mockFunc).toHaveBeenCalledTimes(1);
   }
 
