bed1b84ffeb5f27cf2f38cad1a355df700e4917e
Andrii Selivanov <seland2008@gmail.com>
2018-01-31 15:33:57 +0200

    [tests] NationListItem component.

---
+++ __tests__/src/components/common/NationListItem.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import NationListItem from '../../../../src/components/common/NationListItem';
+
+describe('NationListItem tests', () => {
+
+  test('NationListItem renders correctly', () => {
+    const tree = renderer
+      .create(<NationListItem text='Test list item'/>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+});
