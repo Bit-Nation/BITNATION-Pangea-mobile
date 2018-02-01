dfad4d951c5f6c2156532b98685c1789fc883308
Alberto Estarrona <alberto@estarrona.me>
2018-02-01 17:36:22 -0600

    [fix] Added test in different states and fix for NationActionButton

--- __tests__/src/components/common/NationActionButton.js
+++ __tests__/src/components/common/NationActionButton.js
@@ -2,11 +2,55 @@ import NationActionButton from '../../../../src/components/common/NationActionBu
 import React from 'react';
 import renderer from 'react-test-renderer'
 import AssetsImage from "../../../../src/global/AssetsImages";
+import {shallow} from "enzyme/build/index";
+import {TouchableOpacity} from "react-native";
 
-test('NationActionButton renders correctly', () => {
-  const tree = renderer.create(<NationActionButton
-                                  iconSource={AssetsImage.Actions.chat}
-                                  title='Chat' disable={true}/>
-                              ).toJSON();
-  expect(tree).toMatchSnapshot();
+describe('NationActionButton component tests', () => {
+
+  test('NationActionButton renders correctly', () => {
+    const tree = renderer.create(<NationActionButton
+      iconSource={AssetsImage.Actions.chat}
+      title='Chat' disable={true}/>
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('NationActionButton press works', () => {
+    const onPress = jest.fn();
+    const wrapper = shallow(
+      <NationActionButton onPress={onPress} enabled={true}/>
+    );
+    expect(wrapper).toMatchSnapshot();
+    const render = wrapper.dive();
+    render.find(TouchableOpacity).simulate('press');
+    expect(onPress).toHaveBeenCalledTimes(1);
+  });
+
+  test('NationActionButton press disbled', () => {
+    const onPress = jest.fn();
+    const wrapper = shallow(
+      <NationActionButton onPress={onPress} disable={true}/>
+    );
+    expect(wrapper).toMatchSnapshot();
+    const render = wrapper.dive();
+    render.find(TouchableOpacity).simulate('press');
+    expect(render.find(TouchableOpacity).props()).toEqual(expect.objectContaining({ "activeOpacity": 1 }));
+  });
+
+  test('NationActionButton press enabled', () => {
+    const onPress = jest.fn();
+    const wrapper = shallow(
+      <NationActionButton
+        onPress={onPress}
+        disable={false}
+        iconSource={AssetsImage.Actions.chat}
+        title='TEST'
+      />
+    );
+    expect(wrapper).toMatchSnapshot();
+    const render = wrapper.dive();
+    render.find(TouchableOpacity).simulate('press');
+    expect(render.find(TouchableOpacity).props()).toEqual(expect.objectContaining({ "activeOpacity": 0.4 }));
+  });
+
 });
