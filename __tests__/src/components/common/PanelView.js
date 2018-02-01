dee4b4c02e9fb636a39794ac7e99295109d6c5c4
Andrii Selivanov <seland2008@gmail.com>
2018-02-01 22:26:04 +0200

    [tests] PanelView component.

--- __tests__/src/components/common/PanelView.js
+++ __tests__/src/components/common/PanelView.js
@@ -1,33 +1,47 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 
 import PanelView from '../../../../src/components/common/PanelView';
+import Button from '../../../../src/components/common/Button';
 
-describe('PanelView rendering', () => {
+describe('PanelView tests', () => {
 
-  test('Panel view with title and button', () => {
-    const tree = renderer
-      .create(<PanelView title='Test panel title' onButtonClick={() => null} buttonTitle='Test panel button'/>)
-      .toJSON();
-    expect(tree).toMatchSnapshot();
+  describe('Rendering', () => {
+
+    test('With title and button', () => {
+      const wrapper = shallow(<PanelView title='Test panel title'
+                                         onButtonClick={() => null}
+                                         buttonTitle='Test panel button'/>);
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    test('With title and body', () => {
+      const wrapper = shallow(<PanelView title='Test panel title'
+                                         body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'/>);
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    test('With title, body and button', () => {
+      const wrapper = shallow(<PanelView title='Test panel title'
+                                         body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
+                                         onButtonClick={() => null}
+                                         buttonTitle='Test panel button'/>);
+      expect(wrapper).toMatchSnapshot();
+    });
+
   });
 
-  test('Panel view with title and body', () => {
-    const tree = renderer
-      .create(<PanelView title='Test panel title'
-                         body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'/>)
-      .toJSON();
-    expect(tree).toMatchSnapshot();
-  });
+  describe('Behaviour', () => {
 
-  test('Panel view with title, body and button', () => {
-    const tree = renderer
-      .create(<PanelView title='Test panel title'
-                         body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
-                         onButtonClick={() => null}
-                         buttonTitle='Test panel button'/>)
-      .toJSON();
-    expect(tree).toMatchSnapshot();
+    test('Button press', () => {
+      const mockFunc = jest.fn();
+      const wrapper = shallow(<PanelView onButtonClick={mockFunc}/>);
+      const render = wrapper.dive();
+      const button = render.find(Button);
+      button.simulate('press');
+      expect(mockFunc).toHaveBeenCalledTimes(1);
+    });
+
   });
 
 });
