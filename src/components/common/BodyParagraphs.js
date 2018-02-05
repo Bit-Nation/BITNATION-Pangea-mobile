28d5d47086b0f133faa8c4f45dd86611f6de940e
Andrii Selivanov <seland2008@gmail.com>
2018-02-05 13:16:45 +0200

    [project] Extract multiple paragraphs render into BodyParagraphs component.

---
+++ src/components/common/BodyParagraphs.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import PropTypes from 'prop-types';
+import _ from 'lodash';
+
+import BodyParagraph from './BodyParagraph';
+
+/**
+ * @desc Component to render multiple paragraphs of body text.
+ * @type React.Component
+ */
+export default class BodyParagraphs extends Component {
+
+  render() {
+    return (
+      <View>
+        {_.map(this.props.paragraphs, (text, index) => <BodyParagraph text={text} key={index}/>)}
+      </View>
+    );
+  }
+
+}
+
+BodyParagraphs.propTypes = {
+  paragraphs: PropTypes.array,
+};
