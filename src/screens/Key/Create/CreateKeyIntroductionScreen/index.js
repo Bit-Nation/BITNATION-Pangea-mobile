5526363936c7cfc6995ec83240848c8ba3b95bad
Andrii Selivanov <seland2008@gmail.com>
2018-02-05 12:05:22 +0200

    [refactor] CreateKeyIntroductionScreen.

--- src/screens/Key/Create/CreateKeyIntroductionScreen/index.js
+++ src/screens/Key/Create/CreateKeyIntroductionScreen/index.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import {
+  ScrollView,
   View,
 } from 'react-native';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import styles from './styles';
-import Colors from '../../../../global/Colors';
 import { screen } from '../../../../global/Screens';
 import Button from '../../../../components/common/Button';
 import BackgroundImage from '../../../../components/common/BackgroundImage';
@@ -23,37 +23,46 @@ class CreateKeyIntroductionScreen extends KeyBaseScreen {
 
   render() {
     return (
-      <View style={styles.container}>
+      <View style={styles.screenContainer}>
         <BackgroundImage/>
         <FakeNavigationBar/>
+        <ScrollView contentContainerStyle={styles.bodyContainer}>
+          <View style={styles.bodyParagraph}>
+            <Text style={styles.body}>
+              To use a secure blockchain, you need a "private key". We will make your
+              private key for you.
+            </Text>
+          </View>
+          <View style={styles.bodyParagraph}>
+            <Text style={styles.body}>
+              We require you to write down your private key on paper. This process only
+              takes a few minutes.
+            </Text>
+          </View>
+          <View style={styles.bodyParagraph}>
+            <Text style={styles.body}>
+              If your phone is lost, stolen, broken, or upgraded, the only way to restore
+              your wallet is by entering your private key!
+            </Text>
+          </View>
+          <View style={styles.bodyParagraph}>
+            <Text style={styles.body}>
+              We recommend writing on paper because if your key is on your computer or
+              phone, someone might steal it.
+            </Text>
+          </View>
+          <View style={styles.bodyParagraph}>
+            <Text style={styles.body}>
+              Your private key protects everything in Bitnation, so be sure to put your
+              paper with with your private key in a safe place.
+            </Text>
+          </View>
 
-        <View style={styles.wholeText}>
-          <Text messageText style={styles.description}>
-            To use a secure blockchain, you need a "private key". We will make your
-            private key for you.
-          </Text>
-          <Text messageText style={styles.description}>
-            We require you to write down your private key on paper. This process only
-            takes a few minutes.
-          </Text>
-          <Text messageText style={styles.description}>
-            If your phone is lost, stolen, broken, or upgraded, the only way to restore
-            your wallet is by entering your private key!
-          </Text>
-          <Text messageText style={styles.description}>
-            We recommend writing on paper because if your key is on your computer or
-            phone, someone might steal it.
-          </Text>
-          <Text messageText style={styles.description}>
-            Your private key protects everything in Bitnation, so be sure to put your
-            paper with with your private key in a safe place.
-          </Text>
-        </View>
-
-        <View style={styles.buttonContainer}>
-          <Button title='Begin'
-                  onPress={() => this.onNextButtonPressed()}/>
-        </View>
+          <View style={styles.buttonContainer}>
+            <Button title='Begin'
+                    onPress={() => this.onNextButtonPressed()}/>
+          </View>
+        </ScrollView>
       </View>
     );
   }
