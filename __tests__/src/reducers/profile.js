eb16032f77b76eacd8ba45ece8d7023c9599629b
SeniorMobileDeveloper <frontdev8@gmail.com>
2018-02-06 21:08:33 +0100

    Update tests for profile reducer

--- __tests__/src/reducers/profile.js
+++ __tests__/src/reducers/profile.js
@@ -6,7 +6,7 @@ import reducer, { initialState } from '../../../src/reducers/profile';
 
 test('reducer - user profile creation', (done) => {
 	const state = reducer(initialState, startUserCreating())
-	expect(state.editingUser).toEqual({})
+	expect(state).toEqual({...initialState, editingUser: {}})
   done()
 })
 
@@ -16,24 +16,29 @@ test('reducer - set user profile', (done) => {
 		location: 'NYC'
 	}
 	const state = reducer(initialState, setUserProfile(user))
-	expect(state.user).toEqual(user)
+	expect(state).toEqual({...initialState, user: user})
 
 	const state2 = reducer(state, startUserEditing())
-	expect(state2.editingUser).toEqual(state.user)
+	expect(state2).toEqual({...state, editingUser: state.user})
 
 	const modifiedUser = {
 		name: ' Mike',
 		location: 'SanFransisco'
 	}
 	const state3 = reducer(state2, changeEditingUser(modifiedUser))
-	expect(state3.editingUser).toEqual(modifiedUser)
+	expect(state3).toEqual({...state2, editingUser: modifiedUser})
 
 	const state4 = reducer(state3, cancelUserEditing())
-	expect(state4.editingUser).toBe(null)
+	expect(state4).toEqual({...state3, editingUser: null})
 
 	const state5 = reducer(state3, doneUserEditing())
-	expect(state5.editingUser).toBe(null)
-	expect(state5.user).toEqual({...state3.editingUser, name: state3.editingUser.name.trim()})
-
+	expect(state5).toEqual({
+		...state3,
+		editingUser: null,
+		user: {
+			...state3.editingUser,
+			name: state3.editingUser.name.trim()
+		}
+	})
   done()
 })
