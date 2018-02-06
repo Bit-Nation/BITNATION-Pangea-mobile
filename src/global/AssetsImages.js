122087dc8484bb4d803aeb932dddcc30ba5833da
David Gross <davidigross@me.com>
2018-02-06 15:30:42 -0800

    Updating graphics

--- src/global/AssetsImages.js
+++ src/global/AssetsImages.js
@@ -8,12 +8,16 @@ const eth = require('../assets/images/ethereum_logo.png');
 const qrColor = require('../assets/images/QR_icon.png');
 const bitLogo = require('../assets/images/bitnation_logo.png');
 const bitLogoBig = require('../assets/images/bitnation_logo_big.png');
+
+// Photos and Graphics for Introduction Screens
 const build = require('../assets/images/Chrysler_Building.png');
 const monroe = require('../assets/images/monroe.png');
 const fern = require('../assets/images/fern.png');
 const moon = require('../assets/images/moon.png');
+
 const privateKeyDemo = require('../assets/images/privateKeyDemo.png');
 
+// Main Navigation Icons
 const dashboardTabIcon = require('../assets/images/dashboardTabIcon.png');
 const chatTabIcon = require('../assets/images/chatTabIcon.png');
 const nationsTabIcon = require('../assets/images/nationsTabIcon.png');
@@ -21,16 +25,18 @@ const walletTabIcon = require('../assets/images/walletTabIcon.png');
 const profileTabIcon = require('../assets/images/profileTabIcon.png');
 const QR = require('../assets/images/QR.png');
 
+// DEMO placeholders
 const holonsPlaceholder = require('../assets/images/holonsPlaceholder.png');
 const achievementsPlaceholder = require('../assets/images/achievementsPlaceholder.png');
 const mapPlaceholder = require('../assets/images/mapPlaceholder.png');
 const demoPlaceholder = require('../assets/images/demoImage.png');
 const avatarPlaceholder = require('../assets/images/avatarPlaceholder.png');
 
-const chatActionIcon = require('../assets/images/chatIcon.png');
-const mapActionIcon = require('../assets/images/mapIcon.png');
-const joinActionIcon = require('../assets/images/joinIcon.png');
-const leaveActionIcon = require('../assets/images/leaveIcon.png');
+// Tool Bar Icons
+const chatActionIcon = require('../assets/images/toolbarIconChatTool.png');
+const mapActionIcon = require('../assets/images/toolbarIconMapTool.png');
+const joinActionIcon = require('../assets/images/toolbarIconJoin.png');
+const leaveActionIcon = require('../assets/images/toolbarIconLeave.png');
 
 const Images = {
   logo,
