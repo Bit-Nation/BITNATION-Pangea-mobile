d84de4e0dc0e754795e5bfb2a49f8eb48c649d04
SeniorMobileDeveloper <frontdev8@gmail.com>
2018-02-06 20:51:32 +0100

    Update tests for nations reducer

--- __tests__/src/reducers/nations.js
+++ __tests__/src/reducers/nations.js
@@ -7,14 +7,14 @@ import reducer, { initialState } from '../../../src/reducers/nations';
 test('reducer - switchNationTab', (done) => {
 	const tab = 1
 	const state = reducer(initialState, switchNationTab(tab))
-	expect(state.selectedTab).toBe(tab)
+	expect(state).toEqual({...initialState, selectedTab: tab})
   done()
 })
 
 test('reducer - openNation', (done) => {
 	const id = 123
 	const state = reducer(initialState, openNation(id))
-	expect(state.openedNationId).toBe(id)
+	expect(state).toEqual({...initialState, openedNationId: id})
   done()
 })
 
@@ -25,34 +25,43 @@ test('reducer - createNation', (done) => {
 	};
 	const navigator = null;
 	const state = reducer(initialState, createNation(nationData, navigator))
-	expect(state.creatingNation).toEqual(nationData)
-	expect(state.inProgress).toBe(true)
-
+	expect(state).toEqual({
+		...initialState,
+		creatingNation: nationData,
+		inProgress: true
+	})
+	
 	const state2 = reducer(state, doneNationCreation())
-	expect(state2.nation).toEqual(nationData)
-	expect(state2.creatingNation).toBe(null)
-	expect(state2.inProgress).toBe(false)
-
+	expect(state2).toEqual({
+		...state,
+		nation: nationData,
+		creatingNation: null,
+		inProgress: false
+	})
+	
 	const state3 = reducer(state, cancelNationCreation())
-	expect(state3.creatingNation).toBe(null)
-	expect(state3.inProgress).toBe(false)
+	expect(state3).toEqual({
+		...state,
+		creatingNation: null,
+		inProgress: false
+	})
   done()
 })
 
 test('reducer - requestFetchNations', (done) => {
 	const state = reducer(initialState, requestFetchNations())
-	expect(state.inProgress).toBe(true)
+	expect(state).toEqual({...initialState, inProgress: true})
   done()
 })
 
 test('reducer - joinNation', (done) => {
 	const state = reducer(initialState, joinNation())
-	expect(state.inProgress).toBe(true)
+	expect(state).toEqual({...initialState, inProgress: true})
   done()
 })
 
 test('reducer - leaveNation', (done) => {
 	const state = reducer(initialState, leaveNation())
-	expect(state.inProgress).toBe(true)
+	expect(state).toEqual({...initialState, inProgress: true})
   done()
 })
