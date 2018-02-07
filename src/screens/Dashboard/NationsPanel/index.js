c574641af79cc0c86165c786546b60b9e8ca2420
Andrii Selivanov <seland2008@gmail.com>
2018-02-07 22:55:16 +0200

    [project] Create NationsPanel component.

---
+++ src/screens/Dashboard/NationsPanel/index.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import {
+  FlatList,
+  Text,
+  View,
+} from 'react-native';
+import PropTypes from 'prop-types';
+import _ from 'lodash';
+
+import styles from './styles';
+import PanelView from '../../../components/common/PanelView';
+import i18n from '../../../global/i18n';
+import NationListItem from '../../../components/common/NationListItem';
+
+const NEWEST_NATION_COUNT = 5;
+
+/**
+ * @desc Component to render nation panel on dashboard
+ * @type React.Component
+ */
+export default class NationsPanel extends Component {
+
+  render() {
+    const { style } = this.props;
+    const newestNations = _.take(_.sortBy(this.props.nations, nation => -nation.id), NEWEST_NATION_COUNT);
+
+    return (
+      <View style={style}>
+        <PanelView title={i18n.t('screens.dashboard.nationsPanel.title')}>
+          <View>
+            <Text style={styles.headline}>
+              {i18n.t('screens.dashboard.nationsPanel.nationsCount', { count: this.props.nations.length })}
+            </Text>
+          </View>
+          <FlatList
+            renderItem={(item) => {
+              console.log(item);
+              const nation = item.item;
+              return (<NationListItem text={nation.nationName} onPress={this.props.onSelectNation} id={nation.id}/>);
+            }}
+            keyExtractor={(item) => item.id}
+            data={newestNations}
+            style={styles.list}
+            scrollEnabled={false}
+          />
+        </PanelView>
+      </View>
+    );
+  }
+
+}
+
+NationsPanel.propTypes = {
+  /**
+   * @desc Array of all nations objects
+   */
+  nations: PropTypes.array,
+  /**
+   * @desc Callback on select nation to open. Takes one parameter - id of nation.
+   */
+  onSelectNation: PropTypes.func,
+};
+
+NationsPanel.defaultProps = {
+  nations: [],
+  onSelectNation: () => null,
+};
