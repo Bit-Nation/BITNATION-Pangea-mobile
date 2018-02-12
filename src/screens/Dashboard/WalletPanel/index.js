fe418a937a90ca1d1d50513c65ac6a51a179a1ee
Andrii Selivanov <seland2008@gmail.com>
2018-02-12 18:37:24 +0200

    [project] Update wallet panel layout.

--- src/screens/Dashboard/WalletPanel/index.js
+++ src/screens/Dashboard/WalletPanel/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {
+  Text,
   View,
 } from 'react-native';
 import PropTypes from 'prop-types';
@@ -8,6 +9,7 @@ import _ from 'lodash';
 import PanelView from '../../../components/common/PanelView';
 import i18n from '../../../global/i18n';
 import { prettyETHWalletBalance } from '../../../utils/formatters';
+import styles from '../../../global/Styles';
 
 /**
  * @desc Component to render wallet panel on dashboard
@@ -17,18 +19,31 @@ export default class WalletPanel extends Component {
 
   render() {
     const { style } = this.props;
+    const hasWallets = !_.isEmpty(this.props.wallets);
+    const wallet = hasWallets && this.props.wallets[0];
 
     return (
       <View style={style}>
         <PanelView
           title={i18n.t('screens.dashboard.walletPanel.title')}
-          body={
-            _.isEmpty(this.props.wallets) ?
-              i18n.t('screens.dashboard.walletPanel.empty')
-              :
-              prettyETHWalletBalance(this.props.wallets[0])
+          body={!hasWallets && i18n.t('screens.dashboard.walletPanel.empty')}
+        >
+          {
+            hasWallets &&
+            <Text style={styles.body}>
+              {wallet.currency}
+            </Text>
           }
-        />
+          {
+            hasWallets &&
+            <View>
+              <Text style={styles.panelTitle}>
+                {prettyETHWalletBalance(wallet, '', false)}
+              </Text>
+            </View>
+          }
+
+        </PanelView>
       </View>
     );
   }
