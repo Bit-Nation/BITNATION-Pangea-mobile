a94578a2a1e11c5072d83a9968cce43910314459
Andrii Selivanov <seland2008@gmail.com>
2018-02-13 15:21:38 +0200

    [tests] utils/nations.js

---
+++ __tests__/src/utils/nations.js
@@ -0,0 +1,29 @@
+import { resolveNation, convertToDatabase, convertFromDatabase } from '../../../src/utils/nations';
+
+test('resolveNation works', () => {
+  const expectedNation = {
+    id: 3, nationName: 'nation3',
+  };
+  const nations = [
+    { id: 2, info: 'nation2' },
+    { id: 1, info: 'nation1' },
+    { id: 4, info: 'nation4' },
+    expectedNation,
+    { id: 6, info: 'nation6' },
+    { id: 5, info: 'nation5' },
+  ];
+
+  expect(resolveNation(nations, expectedNation.id)).toEqual(expectedNation);
+});
+
+test('convert governance service to and from database works', () => {
+  const nationInApp = {
+    governanceService: ['A', 'B', 'C'],
+  };
+
+  const nationDB = convertToDatabase(nationInApp);
+  expect(nationDB.governanceService).toEqual('A, B, C');
+
+  const nationConvertedBack = convertFromDatabase(nationDB);
+  expect(nationConvertedBack.governanceService).toEqual(nationInApp.governanceService);
+});
