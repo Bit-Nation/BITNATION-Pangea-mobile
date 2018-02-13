e8da0dd59d5bf77212f202d3505618bd826c985d
David Gross <davidigross@me.com>
2018-02-13 15:10:54 -0800

    Convert ‘var’ to ‘let’

--- src/components/common/elizabot.js
+++ src/components/common/elizabot.js
@@ -956,22 +956,20 @@ ElizaBot.prototype.reset = function () {
 	this.mem = []
 	this.lastchoice = []
 	
-	for (var k = 0; k < this.elizaKeywords.length; k++) {
+	for (let k = 0; k < this.elizaKeywords.length; k++) {
 		this.lastchoice[k] = []
-		var rules = this.elizaKeywords[k][2]
-		for (var i = 0; i < rules.length; i++) this.lastchoice[k][i] = -1
+		let rules = this.elizaKeywords[k][2]
+		for (let i = 0; i < rules.length; i++) this.lastchoice[k][i] = -1
 	}
 }
 
 ElizaBot.prototype._init = function () {
-	// install ref to global object
-	var global = this
 	// parse data and convert it from canonical form to internal use
 	// prodoce synonym list
-	var synPatterns = {}
+	let synPatterns = {}
 	
 	if ((this.elizaSynons) && (typeof this.elizaSynons == 'object')) {
-		for (var i in this.elizaSynons) synPatterns[i] = '(' + i + '|' +
+		for (let i in this.elizaSynons) synPatterns[i] = '(' + i + '|' +
 			this.elizaSynons[i].join('|') + ')'
 	}
 	// check for keywords or install empty structure to prevent any errors
@@ -981,20 +979,20 @@ ElizaBot.prototype._init = function () {
 	}
 	// 1st convert rules to regexps
 	// expand synonyms and insert asterisk expressions for backtracking
-	var sre = /@(\S+)/
-	var are = /(\S)\s*\*\s*(\S)/
-	var are1 = /^\s*\*\s*(\S)/
-	var are2 = /(\S)\s*\*\s*$/
-	var are3 = /^\s*\*\s*$/
-	var wsre = /\s+/g
-	for (var k = 0; k < this.elizaKeywords.length; k++) {
-		var rules = this.elizaKeywords[k][2]
+	const sre = /@(\S+)/
+	const are = /(\S)\s*\*\s*(\S)/
+	const are1 = /^\s*\*\s*(\S)/
+	const are2 = /(\S)\s*\*\s*$/
+	const are3 = /^\s*\*\s*$/
+	const wsre = /\s+/g
+	for (let k = 0; k < this.elizaKeywords.length; k++) {
+		let rules = this.elizaKeywords[k][2]
 		this.elizaKeywords[k][3] = k // save original index for sorting
-		for (var i = 0; i < rules.length; i++) {
-			var r = rules[i]
+		for (let i = 0; i < rules.length; i++) {
+			let r = rules[i]
 			// check mem flag and store it as decomp's element 2
 			if (r[0].charAt(0) == '$') {
-				var ofs = 1
+				let ofs = 1
 				while (r[0].charAt[ofs] == ' ') ofs++
 				r[0] = r[0].substring(ofs)
 				r[2] = true
@@ -1003,9 +1001,9 @@ ElizaBot.prototype._init = function () {
 				r[2] = false
 			}
 			// expand synonyms (v.1.1: work around lambda function)
-			var m = sre.exec(r[0])
+			let m = sre.exec(r[0])
 			while (m) {
-				var sp = (synPatterns[m[1]]) ? synPatterns[m[1]] : m[1]
+				let sp = (synPatterns[m[1]]) ? synPatterns[m[1]] : m[1]
 				r[0] = r[0].substring(0, m.index) + sp +
 					r[0].substring(m.index + m[0].length)
 				m = sre.exec(r[0])
@@ -1017,8 +1015,8 @@ ElizaBot.prototype._init = function () {
 			else {
 				m = are.exec(r[0])
 				if (m) {
-					var lp = ''
-					var rp = r[0]
+					let lp = ''
+					let rp = r[0]
 					while (m) {
 						lp += rp.substring(0, m.index + 1)
 						if (m[1] != ')') lp += '\\b'
@@ -1032,13 +1030,13 @@ ElizaBot.prototype._init = function () {
 				}
 				m = are1.exec(r[0])
 				if (m) {
-					var lp = '\\s*(.*)\\s*'
+					let lp = '\\s*(.*)\\s*'
 					if ((m[1] != ')') && (m[1] != '\\')) lp += '\\b'
 					r[0] = lp + r[0].substring(m.index - 1 + m[0].length)
 				}
 				m = are2.exec(r[0])
 				if (m) {
-					var lp = r[0].substring(0, m.index + 1)
+					let lp = r[0].substring(0, m.index + 1)
 					if (m[1] != '(') lp += '\\b'
 					r[0] = lp + '\\s*(.*)\\s*'
 				}
@@ -1055,8 +1053,8 @@ ElizaBot.prototype._init = function () {
 	ElizaBot.prototype.posts = {}
 	
 	if ((this.elizaPres) && (this.elizaPres.length)) {
-		var a = new Array()
-		for (var i = 0; i < this.elizaPres.length; i += 2) {
+		let a = new Array()
+		for (let i = 0; i < this.elizaPres.length; i += 2) {
 			a.push(this.elizaPres[i])
 			ElizaBot.prototype.pres[this.elizaPres[i]] = this.elizaPres[i + 1]
 		}
@@ -1069,8 +1067,8 @@ ElizaBot.prototype._init = function () {
 	}
 	
 	if ((this.elizaPosts) && (this.elizaPosts.length)) {
-		var a = new Array()
-		for (var i = 0; i < this.elizaPosts.length; i += 2) {
+		let a = new Array()
+		for (let i = 0; i < this.elizaPosts.length; i += 2) {
 			a.push(this.elizaPosts[i])
 			ElizaBot.prototype.posts[this.elizaPosts[i]] = this.elizaPosts[i +
 			1]
@@ -1101,7 +1099,7 @@ ElizaBot.prototype._sortKeywords = function (a, b) {
 }
 
 ElizaBot.prototype.transform = function (text) {
-	var rpl = ''
+	let rpl = ''
 	this.quit = false
 	// unify text string
 	text = text.toLowerCase()
@@ -1111,22 +1109,22 @@ ElizaBot.prototype.transform = function (text) {
 	text = text.replace(/\s*\bbut\b\s*/g, '.')
 	text = text.replace(/\s{2,}/g, ' ')
 	// split text in part sentences and loop through them
-	var parts = text.split('.')
-	for (var i = 0; i < parts.length; i++) {
-		var part = parts[i]
+	let parts = text.split('.')
+	for (let i = 0; i < parts.length; i++) {
+		let part = parts[i]
 		if (part != '') {
 			// check for quit expression
-			for (var q = 0; q < this.elizaQuits.length; q++) {
+			for (let q = 0; q < this.elizaQuits.length; q++) {
 				if (this.elizaQuits[q] == part) {
 					this.quit = true
 					return this.getFinal()
 				}
 			}
 			// preprocess (v.1.1: work around lambda function)
-			var m = this.preExp.exec(part)
+			let m = this.preExp.exec(part)
 			if (m) {
-				var lp = ''
-				var rp = part
+				let lp = ''
+				let rp = part
 				while (m) {
 					lp += rp.substring(0, m.index) + this.pres[m[1]]
 					rp = rp.substring(m.index + m[0].length)
@@ -1136,7 +1134,7 @@ ElizaBot.prototype.transform = function (text) {
 			}
 			this.sentence = part
 			// loop trough keywords
-			for (var k = 0; k < this.elizaKeywords.length; k++) {
+			for (let k = 0; k < this.elizaKeywords.length; k++) {
 				if (part.search(
 						new RegExp('\\b' + this.elizaKeywords[k][0] + '\\b',
 							'i')) >= 0) {
@@ -1151,7 +1149,7 @@ ElizaBot.prototype.transform = function (text) {
 	// if nothing in mem, so try xnone
 	if (rpl == '') {
 		this.sentence = ' '
-		var k = this._getRuleIndexByKey('xnone')
+		let k = this._getRuleIndexByKey('xnone')
 		if (k >= 0) rpl = this._execRule(k)
 	}
 	// return reply or default string
@@ -1159,15 +1157,15 @@ ElizaBot.prototype.transform = function (text) {
 }
 
 ElizaBot.prototype._execRule = function (k) {
-	var rule = this.elizaKeywords[k]
-	var decomps = rule[2]
-	var paramre = /\(([0-9]+)\)/
-	for (var i = 0; i < decomps.length; i++) {
-		var m = this.sentence.match(decomps[i][0])
+	let rule = this.elizaKeywords[k]
+	let decomps = rule[2]
+	let paramre = /\(([0-9]+)\)/
+	for (let i = 0; i < decomps.length; i++) {
+		let m = this.sentence.match(decomps[i][0])
 		if (m != null) {
-			var reasmbs = decomps[i][1]
-			var memflag = decomps[i][2]
-			var ri = (this.noRandom) ? 0 : Math.floor(Math.random() *
+			let reasmbs = decomps[i][1]
+			let memflag = decomps[i][2]
+			let ri = (this.noRandom) ? 0 : Math.floor(Math.random() *
 				reasmbs.length)
 			if (((this.noRandom) && (this.lastchoice[k][i] > ri)) ||
 				(this.lastchoice[k][i] == ri)) {
@@ -1180,7 +1178,7 @@ ElizaBot.prototype._execRule = function (k) {
 			else {
 				this.lastchoice[k][i] = ri
 			}
-			var rpl = reasmbs[ri]
+			let rpl = reasmbs[ri]
 			if (this.debug) alert('match:\nkey: ' + this.elizaKeywords[k][0] +
 				'\nrank: ' + this.elizaKeywords[k][1] +
 				'\ndecomp: ' + decomps[i][0] +
@@ -1191,17 +1189,17 @@ ElizaBot.prototype._execRule = function (k) {
 				if (ki >= 0) return this._execRule(ki)
 			}
 			// substitute positional params (v.1.1: work around lambda function)
-			var m1 = paramre.exec(rpl)
+			let m1 = paramre.exec(rpl)
 			if (m1) {
-				var lp = ''
-				var rp = rpl
+				let lp = ''
+				let rp = rpl
 				while (m1) {
-					var param = m[parseInt(m1[1])]
+					let param = m[parseInt(m1[1])]
 					// postprocess param
-					var m2 = this.postExp.exec(param)
+					let m2 = this.postExp.exec(param)
 					if (m2) {
-						var lp2 = ''
-						var rp2 = param
+						let lp2 = ''
+						let rp2 = param
 						while (m2) {
 							lp2 += rp2.substring(0, m2.index) +
 								this.posts[m2[1]]
@@ -1229,7 +1227,7 @@ ElizaBot.prototype._postTransform = function (s) {
 	s = s.replace(/\s{2,}/g, ' ')
 	s = s.replace(/\s+\./g, '.')
 	if ((this.elizaPostTransforms) && (this.elizaPostTransforms.length)) {
-		for (var i = 0; i < this.elizaPostTransforms.length; i += 2) {
+		for (let i = 0; i < this.elizaPostTransforms.length; i += 2) {
 			s = s.replace(this.elizaPostTransforms[i],
 				this.elizaPostTransforms[i + 1])
 			this.elizaPostTransforms[i].lastIndex = 0
@@ -1237,15 +1235,15 @@ ElizaBot.prototype._postTransform = function (s) {
 	}
 	// capitalize first char (v.1.1: work around lambda function)
 	if (this.capitalizeFirstLetter) {
-		var re = /^([a-z])/
-		var m = re.exec(s)
+		let re = /^([a-z])/
+		let m = re.exec(s)
 		if (m) s = m[0].toUpperCase() + s.substring(1)
 	}
 	return s
 }
 
 ElizaBot.prototype._getRuleIndexByKey = function (key) {
-	for (var k = 0; k < this.elizaKeywords.length; k++) {
+	for (let k = 0; k < this.elizaKeywords.length; k++) {
 		if (this.elizaKeywords[k][0] == key) return k
 	}
 	return -1
@@ -1260,9 +1258,9 @@ ElizaBot.prototype._memGet = function () {
 	if (this.mem.length) {
 		if (this.noRandom) return this.mem.shift()
 		else {
-			var n = Math.floor(Math.random() * this.mem.length)
-			var rpl = this.mem[n]
-			for (var i = n + 1; i < this.mem.length; i++) this.mem[i -
+			let n = Math.floor(Math.random() * this.mem.length)
+			let rpl = this.mem[n]
+			for (let i = n + 1; i < this.mem.length; i++) this.mem[i -
 			1] = this.mem[i]
 			this.mem.length--
 			return rpl
@@ -1284,7 +1282,7 @@ ElizaBot.prototype.getInitial = function () {
 		this.elizaInitials.length)]
 }
 
-var elizaFinals = [
+let elizaFinals = [
 	'Goodbye.  It was nice talking to you.',
 // additions (not original)
 	'Goodbye.  This was really a nice talk.',
@@ -1300,8 +1298,8 @@ if (typeof Array.prototype.push == 'undefined') {
 if (typeof Array.prototype.shift == 'undefined') {
 	Array.prototype.shift = function () {
 		if (this.length == 0) return null
-		var e0 = this[0]
-		for (var i = 1; i < this.length; i++) this[i - 1] = this[i]
+		let e0 = this[0]
+		for (let i = 1; i < this.length; i++) this[i - 1] = this[i]
 		this.length--
 		return e0
 	}
