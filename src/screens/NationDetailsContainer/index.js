11a407b3004349939f8362574a7e2dd6f91119d0
Andrii Selivanov <seland2008@gmail.com>
2018-02-14 20:32:10 +0200

    [refactor] Rename fn.

--- src/screens/NationDetailsContainer/index.js
+++ src/screens/NationDetailsContainer/index.js
@@ -103,11 +103,11 @@ class NationDetailsContainer extends NavigatorComponent {
     this.performIfHasWallet(this.props.leaveNation);
   };
 
-  performIfHasWallet(fn) {
+  performIfHasWallet(functionToPerform) {
     if (_.isEmpty(this.props.wallets)) {
       this._showCreatePrivateKeyAlert();
     } else {
-      fn();
+      functionToPerform();
     }
   }
 
