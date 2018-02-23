04df379255879807160b565524c811152fb6aa9f
SeniorMobileDeveloper <frontdev8@gmail.com>
2018-02-23 23:26:41 +0100

    Apply eslint for profile saga

--- src/sagas/profile.js
+++ src/sagas/profile.js
@@ -4,6 +4,10 @@ import { getPangeaLibrary } from '../services/container';
 import { REQUEST_PROFILE_UPDATE, DONE_USER_EDITING, SET_USER_PROFILE, REQUEST_GET_PROFILE, CANCEL_USER_EDITING } from '../actions/profile';
 
 export const getProfileState = state => state.profile;
+/**
+ * @desc function generator for profile update saga
+  * @return {void}
+ */
 export function* updateProfile() {
   try {
     const pangeaLib = yield call(getPangeaLibrary);
@@ -19,14 +23,17 @@ export function* updateProfile() {
       version: '0',
       image: user.editingUser.avatar ? user.editingUser.avatar : '',
     };
-    const result = yield call(pangeaLib.profile.profile.setProfile, profile);
+    yield call(pangeaLib.profile.profile.setProfile, profile);
     yield put({ type: DONE_USER_EDITING });
   } catch (e) {
     yield put({ type: CANCEL_USER_EDITING });
-    console.log('Update profile error: ', e);
   }
 }
 
+/**
+ * @desc function generator for get profile saga
+  * @return {void}
+ */
 export function* getProfile() {
   try {
     const pangeaLib = yield call(getPangeaLibrary);
@@ -38,10 +45,13 @@ export function* getProfile() {
     yield put({ type: SET_USER_PROFILE, user });
   } catch (e) {
     yield put({ type: SET_USER_PROFILE, user: null });
-    console.log('Get profile error: ', e);
   }
 }
 
+/**
+ * @desc action watchers for profile saga
+  * @return {void}
+ */
 export default function* watchProfileUpdate() {
   yield takeEvery(REQUEST_PROFILE_UPDATE, updateProfile);
   yield takeEvery(REQUEST_GET_PROFILE, getProfile);
