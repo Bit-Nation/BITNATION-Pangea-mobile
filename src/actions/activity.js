574f3b09cb3870c505a93fc109eb7be967792b8b
Andrii Selivanov <seland2008@gmail.com>
2018-02-23 21:12:58 +0200

    [refactor] Activity actions and reducer are implemented using flow.

--- src/actions/activity.js
+++ src/actions/activity.js
@@ -3,19 +3,25 @@
 import { ACTIVITY_MESSAGES_LIMIT } from '../global/Constants';
 import { type ActivityLogMessage } from '../types/ActivityLogMessage';
 
-export const MESSAGE_ADDED = 'MESSAGE_ADDED';
-export const START_FETCH_MESSAGES = 'START_FETCH_MESSAGES';
-export const DONE_FETCH_MESSAGES = 'DONE_FETCH_MESSAGES';
-export const ADD_DUMMY_MESSAGE = 'ADD_DUMMY_MESSAGE';
+type MessageAddedAction = { type: 'MESSAGE_ADDED', message: ActivityLogMessage };
+type StartFetchMessagesAction = { type: 'START_FETCH_MESSAGES', limit: number };
+type DoneFetchMessagesAction = { type: 'DONE_FETCH_MESSAGES', messages: Array<ActivityLogMessage> };
+type AddDummyMessageAction = { type: 'ADD_DUMMY_MESSAGE' };
+
+export type Action =
+  | MessageAddedAction
+  | StartFetchMessagesAction
+  | DoneFetchMessagesAction
+  | AddDummyMessageAction;
 
 /**
  * @desc Action creator for an action that should be called once new activity log message added.
  * @param {ActivityLogMessage} message Activity log message that is added.
- * @returns {{type: string, message: *}} An action.
+ * @returns {MessageAddedAction} An action.
  */
-export function messageAdded(message: ActivityLogMessage) {
+export function messageAdded(message: ActivityLogMessage): MessageAddedAction {
   return {
-    type: MESSAGE_ADDED,
+    type: 'MESSAGE_ADDED',
     message,
   };
 }
@@ -25,9 +31,9 @@ export function messageAdded(message: ActivityLogMessage) {
  * @param {number} limit Limit of count of messages to fetch.
  * @returns {{type: string, limit: number}} An action.
  */
-export function startFetchMessages(limit: number = ACTIVITY_MESSAGES_LIMIT) {
+export function startFetchMessages(limit: number = ACTIVITY_MESSAGES_LIMIT): StartFetchMessagesAction {
   return {
-    type: START_FETCH_MESSAGES,
+    type: 'START_FETCH_MESSAGES',
     limit,
   };
 }
@@ -35,21 +41,21 @@ export function startFetchMessages(limit: number = ACTIVITY_MESSAGES_LIMIT) {
 /**
  * @desc Action creator for an action to be called when activity log messages is fetched.
  * @param {ActivityLogMessage[]} messages Activity log messages that was fetched.
- * @returns {{type: string, messages: ActivityLogMessage[]}} An action.
+ * @returns {DoneFetchMessagesAction} An action.
  */
-export function doneFetchMessages(messages: [ActivityLogMessage]) {
+export function doneFetchMessages(messages: Array<ActivityLogMessage>): DoneFetchMessagesAction {
   return {
-    type: DONE_FETCH_MESSAGES,
+    type: 'DONE_FETCH_MESSAGES',
     messages,
   };
 }
 
 /**
  * @desc Action for an action that adds dummy activity log message for testing purposes.
- * @returns {{type: string}} An action.
+ * @returns {AddDummyMessageAction} An action.
  */
-export function addDummyMessage() {
+export function addDummyMessage(): AddDummyMessageAction {
   return {
-    type: ADD_DUMMY_MESSAGE,
+    type: 'ADD_DUMMY_MESSAGE',
   };
 }
