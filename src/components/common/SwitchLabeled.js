2952d7a33b9fa512b9e7d420ab39ad83626ba1e4
Alberto Estarrona <alberto@estarrona.me>
2018-02-23 18:13:48 -0600

    [fix] SwitchLabeled changes

--- src/components/common/SwitchLabeled.js
+++ src/components/common/SwitchLabeled.js
@@ -5,44 +5,46 @@
  * @param props.value {Boolean} State for the switch
  */
 
-import React, { Component } from 'react'
-import { Switch, View, Text } from 'react-native'
-import { MediaQueryStyleSheet } from 'react-native-responsive'
-import PropTypes from 'prop-types'
-import GlobalStyles from '../../global/Styles'
-import Colors from '../../global/colors'
+import React, { Component } from 'react';
+import { Switch, View, Text } from 'react-native';
+import { MediaQueryStyleSheet } from 'react-native-responsive';
+import PropTypes from 'prop-types';
+import GlobalStyles from '../../global/Styles';
+import Colors from '../../global/colors';
 
 export default class SwitchLabeled extends Component {
-	
-	render () {
-		const {onValueChange} = this.props
-		
-		return (
-			<View style={styles.formRow}>
-				<View style={styles.switchContainer}>
-					<Switch style={styles.switchObject}
-					        onTintColor={Colors.BitnationHighlightColor}
-					        onValueChange={onValueChange}
-					        value={this.props.value}
-					/>
-					<Text style={styles.formSwitchLabelText}>{this.props.label}</Text>
-				</View>
-			</View>
-		)
-	}
+  render() {
+    const {
+      onValueChange,
+    } = this.props;
+    return (
+      <View style={styles.formRow}>
+        <View style={styles.switchContainer}>
+          <Switch
+            style={styles.switchObject}
+            onTintColor={Colors.BitnationHighlightColor}
+            onValueChange={onValueChange}
+            value={this.props.value}
+          />
+          <Text style={styles.formSwitchLabelText}>{this.props.label}</Text>
+        </View>
+      </View>
+    );
+  }
 }
 
 SwitchLabeled.propTypes = {
-	value: PropTypes.bool,
-	label: PropTypes.string,
+  value: PropTypes.bool,
+  label: PropTypes.string,
+  onValueChange: PropTypes.func,
 }
 
 SwitchLabeled.defaultProps = {
-	value: false,
-	label: '',
-}
+  value: false,
+  label: '',
+  onValueChange: () => null,
+};
 
 const styles = MediaQueryStyleSheet.create({
-	...GlobalStyles,
-	
-})
+  ...GlobalStyles,
+});
