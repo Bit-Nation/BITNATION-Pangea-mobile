79e3b69cbe303d91bac15e97e817ca7d03032b48
Andrii Selivanov <seland2008@gmail.com>
2018-02-26 19:51:50 +0200

    [refactor] Wallet actions and reducers.

--- src/actions/wallet.js
+++ src/actions/wallet.js
@@ -1,3 +1,26 @@
+// @flow
+
+import type { WalletType } from '../types/Wallet';
+
+type SelectWalletAction = { type: 'SELECT_WALLET', wallet: WalletType };
+type SendMoneyAction = { type: 'SEND_MONEY', amount: number, toEthAddress: string };
+type WalletListUpdatedAction = { type: 'WALLETS_LIST_UPDATED', wallets: Array<WalletType> };
+type UpdateWalletListAction = { type: 'UPDATE_WALLET_LIST' };
+type UpdateWalletBalanceAction = { type: 'UPDATE_WALLET_BALANCE', walletAddress: string };
+type SendMoneySuccessAction = { type: 'SEND_MONEY_SUCCESS' };
+type SendMoneyFailedAction = { type: 'SEND_MONEY_FAILED', error: Error };
+type WalletSyncFailedAction = { type: 'WALLET_SYNC_FAILED', walletAddress: string, error: Error };
+
+export type Action =
+  | SelectWalletAction
+  | SendMoneyAction
+  | WalletListUpdatedAction
+  | UpdateWalletListAction
+  | UpdateWalletBalanceAction
+  | SendMoneySuccessAction
+  | SendMoneyFailedAction
+  | WalletSyncFailedAction;
+
 export const SELECT_WALLET = 'SELECT_WALLET';
 export const SEND_MONEY = 'SEND_MONEY';
 export const SEND_MONEY_SUCCESS = 'SEND_MONEY_SUCCESS';
@@ -7,14 +30,25 @@ export const WALLET_SYNC_FAILED = 'WALLET_SYNC_FAILED';
 export const UPDATE_WALLET_LIST = 'UPDATE_WALLET_LIST';
 export const UPDATE_WALLET_BALANCE = 'UPDATE_WALLET_BALANCE';
 
-export function selectWallet(wallet) {
+/**
+ * @desc Action creator for an action that select a wallet to be currently active.
+ * @param {WalletType} wallet Wallet to select.
+ * @returns {SelectWalletAction} An action.
+ */
+export function selectWallet(wallet: WalletType): SelectWalletAction {
   return {
     type: SELECT_WALLET,
     wallet,
   };
 }
 
-export function sendMoney(amount, toEthAddress) {
+/**
+ * @desc Action creator for an action that sends money.
+ * @param {number} amount Amount to send in eth.
+ * @param {string} toEthAddress Address to send money.
+ * @returns {SendMoneyAction} An action.
+ */
+export function sendMoney(amount: number, toEthAddress: string): SendMoneyAction {
   return {
     type: SEND_MONEY,
     amount,
@@ -22,40 +56,69 @@ export function sendMoney(amount, toEthAddress) {
   };
 }
 
-export function walletsListUpdated(wallets) {
+/**
+ * @desc Action creator for an action that takes update of wallets available.
+ * @param {WalletType[]} wallets Array of wallets.
+ * @returns {WalletListUpdatedAction} An action.
+ */
+export function walletsListUpdated(wallets: Array<WalletType>): WalletListUpdatedAction {
   return {
     type: WALLETS_LIST_UPDATED,
     wallets,
   };
 }
 
-export function updateWalletList() {
+/**
+ * @desc Action creator for an action that starts wallets list update.
+ * @returns {UpdateWalletListAction} An action.
+ */
+export function updateWalletList(): UpdateWalletListAction {
   return {
     type: UPDATE_WALLET_LIST,
   };
 }
 
-export function updateWalletBalance(walletAddress) {
+/**
+ * @desc Action creator for an action that starts specific wallet update.
+ * @param {string} walletAddress Address of wallet to update.
+ * @returns {UpdateWalletBalanceAction} An action.
+ */
+export function updateWalletBalance(walletAddress: string): UpdateWalletBalanceAction {
   return {
     type: UPDATE_WALLET_BALANCE,
     walletAddress,
   };
 }
 
-export function sendMoneySuccess() {
+/**
+ * @desc Action creator for an action that should be called on send money success.
+ * @returns {SendMoneySuccessAction} An action.
+ */
+export function sendMoneySuccess(): SendMoneySuccessAction {
   return {
     type: SEND_MONEY_SUCCESS,
   };
 }
 
-export function sendMoneyFailed(error) {
+/**
+ * @desc Action creator for an action that should be called on send money failed.
+ * @param {Error} error An error that occurs.
+ * @returns {SendMoneyFailedAction} An action.
+ */
+export function sendMoneyFailed(error: Error): SendMoneyFailedAction {
   return {
     type: SEND_MONEY_FAILED,
     error,
   };
 }
 
-export function walletSyncFailed(walletAddress, error) {
+/**
+ * @desc Action creator for an action that should be called on wallet synchronization fail.
+ * @param {string} walletAddress Address of wallet that was synced.
+ * @param {Error} error An error that occurs.
+ * @returns {WalletSyncFailedAction} An action.
+ */
+export function walletSyncFailed(walletAddress: string, error: Error): WalletSyncFailedAction {
   return {
     type: WALLET_SYNC_FAILED,
     walletAddress,
