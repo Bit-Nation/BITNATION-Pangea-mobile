22a0a64e0c0d37917ff4f272da75605f2e08d4fb
Alberto Estarrona <alberto@estarrona.me>
2018-02-26 13:17:24 -0600

    [fix] Refactor SwitchLabeled as stateless function

--- src/components/common/SwitchLabeled.js
+++ src/components/common/SwitchLabeled.js
@@ -5,38 +5,47 @@
  * @param props.value {Boolean} State for the switch
  */
 
-import React, { Component } from 'react';
+import React from 'react';
 import { Switch, View, Text } from 'react-native';
 import { MediaQueryStyleSheet } from 'react-native-responsive';
 import PropTypes from 'prop-types';
 import GlobalStyles from '../../global/Styles';
 import Colors from '../../global/colors';
 
-export default class SwitchLabeled extends Component {
-  render() {
-    const { onValueChange } = this.props;
-    const styles = MediaQueryStyleSheet.create({
-      ...GlobalStyles,
-    });
-    return (
-      <View style={styles.formRow}>
-        <View style={styles.switchContainer}>
-          <Switch
-            style={styles.switchObject}
-            onTintColor={Colors.BitnationHighlightColor}
-            onValueChange={onValueChange}
-            value={this.props.value}
-          />
-          <Text style={styles.formSwitchLabelText}>{this.props.label}</Text>
-        </View>
+const SwitchLabeled = ({ onValueChange, value, label }) => {
+  const styles = MediaQueryStyleSheet.create({
+    ...GlobalStyles,
+  });
+  return (
+    <View style={styles.formRow}>
+      <View style={styles.switchContainer}>
+        <Switch
+          style={styles.switchObject}
+          onTintColor={Colors.BitnationHighlightColor}
+          onValueChange={onValueChange}
+          value={value}
+        />
+        <Text style={styles.formSwitchLabelText}>{label}</Text>
       </View>
-    );
-  }
-}
+    </View>
+  );
+};
 
 SwitchLabeled.propTypes = {
+  /**
+   * @desc Value of the Switch.
+   * @type boolean
+   */
   value: PropTypes.bool,
+  /**
+   * @desc Text label at the right of the Switch.
+   * @type string
+   */
   label: PropTypes.string,
+  /**
+   * @desc Callback to be called on Switch when changes value.
+   * @type func
+   */
   onValueChange: PropTypes.func,
 };
 
@@ -45,3 +54,5 @@ SwitchLabeled.defaultProps = {
   label: '',
   onValueChange: () => null,
 };
+
+export default SwitchLabeled;
