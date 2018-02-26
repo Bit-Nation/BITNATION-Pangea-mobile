3bd5bd1368493a4551c3cd2e19e3997a7733206f
Andrii Selivanov <seland2008@gmail.com>
2018-02-26 18:25:59 +0200

    [refactor] Profile actions and reducers.

---
+++ src/types/Profile.js
@@ -0,0 +1,10 @@
+// @flow
+
+// eslint-disable-next-line import/prefer-default-export
+export type { ProfileType as ProfileDatabaseType } from 'BITNATION-Pangea-libs/src/database/schemata';
+
+export type ProfileType = {
+  name: ?string,
+  location: ?string,
+  avatar: ?string,
+};
