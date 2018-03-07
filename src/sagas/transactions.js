634c54e65847c51fd84d649050f75f0e9e850faf
Andrii Selivanov <seland2008@gmail.com>
2018-03-07 21:50:29 +0200

    [tests] Add tests for transactions saga.

--- src/sagas/transactions.js
+++ src/sagas/transactions.js
@@ -2,10 +2,15 @@ import { call } from 'redux-saga/effects';
 
 import { getPangeaLibrary } from '../services/container';
 
+/**
+ * @desc Starts worker for processing transactions.
+ * @returns {void} Void.
+ */
 export default function* startProcessing() {
   const pangeaLibs = yield call(getPangeaLibrary);
+  const { txQueue } = pangeaLibs.queue;
   while (true) {
-    yield pangeaLibs.queue.txQueue.startProcessing();
+    yield call([txQueue, txQueue.startProcessing]);
   }
 }
 
