da901c45acf256ed98809dd2c741f4983b4da06e
Andrii Selivanov <seland2008@gmail.com>
2018-03-07 20:11:04 +0200

    [project] Add nations sync on activity log coming.

--- src/sagas/activity/sagas.js
+++ src/sagas/activity/sagas.js
@@ -3,17 +3,14 @@ import { eventChannel } from 'redux-saga';
 
 import { fetchMessages, getEventEmitter, addDummyMessage, MESSAGING_QUEUE_JOB_ADDED } from './serviceFunctions';
 import { doneFetchMessages, messageAdded } from '../../actions/activity';
+import { syncNations } from '../nations';
 
 function createMessagesQueueChannel(eventEmitter) {
   return eventChannel((emit) => {
     const handler = (data) => {
-      console.log('HANDLER');
-      console.log(data);
       emit(data);
     };
 
-    console.log('EE');
-    console.log(MESSAGING_QUEUE_JOB_ADDED);
     eventEmitter.on(MESSAGING_QUEUE_JOB_ADDED, handler);
 
     return () => eventEmitter.off(MESSAGING_QUEUE_JOB_ADDED, handler);
@@ -28,14 +25,11 @@ export function* fetchMessagesSaga(action) {
 export function* watchNewMessages() {
   const eventEmitter = yield call(getEventEmitter);
   const channel = yield call(createMessagesQueueChannel, eventEmitter);
-  console.log('CHANNEL');
-  console.log(channel);
 
   while (true) {
     const payload = yield take(channel);
-    console.log('PAYLOAD');
-    console.log(payload);
     yield put(messageAdded(payload));
+    yield fork(syncNations);
   }
 }
 
