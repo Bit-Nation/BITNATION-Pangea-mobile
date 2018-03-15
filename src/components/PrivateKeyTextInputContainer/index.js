394144fa9c99f688b0154143e88b21dc713e0900
Andrii Selivanov <seland2008@gmail.com>
2018-03-15 19:42:49 +0200

    [refactor] Lint and enable flow for PrivateKeyTextInputContainer component.

--- src/components/PrivateKeyTextInputContainer/index.js
+++ src/components/PrivateKeyTextInputContainer/index.js
@@ -1,92 +1,105 @@
-import React, { Component } from 'react';
+// @flow
+
+import * as React from 'react';
 import {
   Text,
   TextInput,
   View,
   findNodeHandle,
 } from 'react-native';
-import PropTypes from 'prop-types';
 
 import styles, { cursorColor } from './styles';
 
+type Props = {
+  /**
+   * @desc Used to identify component among each other.
+   */
+  index: number,
+  /**
+   * @desc Text label to be rendered below component. Used to show number of field.
+   */
+  label?: string,
+  /**
+   * @desc Text value of wrapped text field.
+   */
+  value: string,
+  /**
+   * @desc Determines if the field is last. This change behavior of keyboard handling.
+   */
+  isLast: boolean,
+  /**
+   * @desc Determines if text field should be editable.
+   */
+  editable: boolean,
+  /**
+   * @desc Callback on change text of wrapped text field.
+   */
+  onChange: (number, string) => void,
+  /**
+   * @desc Callback on submit of wrapped text field.
+   */
+  onSubmit: (number) => void,
+  /**
+   * @desc Callback on focus of wrapped text field.
+   */
+  onFocus: (number, ?number) => void,
+  /**
+   * @desc Style object to be applied on root view on top of default style.
+   */
+  style: any,
+}
+
 /**
  * Component that renders text input view for private key input process.
- * @type React.Component
+ * @type {React.Component}
  */
-class PrivateKeyTextInputContainer extends Component {
+class PrivateKeyTextInputContainer extends React.Component<Props> {
+  static defaultProps: Object;
 
-  constructor(props) {
+  constructor(props: Props) {
     super(props);
 
     this.textInput = null;
   }
 
+  textInput: React.Element<'TextInput'> | null;
+
   render() {
-    const { style } = this.props;
+    const {
+      style, editable, isLast, value, index, label, onFocus, onSubmit, onChange,
+    } = this.props;
 
     return (
       <View style={[styles.container, style]}>
         <TextInput
           underlineColorAndroid='transparent'
-          editable={this.props.editable}
+          editable={editable}
           autoCorrect={false}
           autoCapitalize='none'
-          returnKeyType={this.props.isLast ? 'done' : 'next'}
+          returnKeyType={isLast ? 'done' : 'next'}
           style={styles.textInput}
-          value={this.props.value}
-          blurOnSubmit={this.props.isLast}
+          value={value}
+          blurOnSubmit={isLast}
           selectionColor={cursorColor}
-          onFocus={event => this.props.onFocus(this.props.index, findNodeHandle(event.target))}
-          onChangeText={text => this.props.onChange(this.props.index, text)}
-          onSubmitEditing={() => this.props.onSubmit(this.props.index)}
-          ref={textInput => this.textInput = textInput}
+          onFocus={event => onFocus(index, findNodeHandle(event.target))}
+          onChangeText={text => onChange(index, text)}
+          onSubmitEditing={() => onSubmit(index)}
+          ref={textInput => (this.textInput = textInput)}
         />
-        <Text style={styles.text}>{this.props.label}</Text>
+        <Text style={styles.text}>{label}</Text>
       </View>
     );
   }
 }
-
-PrivateKeyTextInputContainer.propTypes = {
-  /**
-   * @desc Used to identify component among each other.
-   */
-  index: PropTypes.number,
-  /**
-   * @desc Text label to be rendered below component. Used to show number of field.
-   */
-  label: PropTypes.string,
-  /**
-   * @desc Text value of wrapped text field.
-   */
-  value: PropTypes.string,
-  /**
-   * @desc Determines if the field is last. This change behavior of keyboard handling.
-   */
-  isLast: PropTypes.bool,
-  /**
-   * @desc Determines if text field should be editable.
-   */
-  editable: PropTypes.bool,
-  /**
-   * @desc Callback on change text of wrapped text field.
-   */
-  onChange: PropTypes.func,
-  /**
-   * @desc Callback on submit of wrapped text field.
-   */
-  onSubmit: PropTypes.func,
-  /**
-   * @desc Callback on focus of wrapped text field.
-   */
-  onFocus: PropTypes.func,
-};
 
 PrivateKeyTextInputContainer.defaultProps = {
   editable: true,
   value: '',
   isLast: true,
   index: 0,
+  onFocus: () => undefined,
+  onSubmit: () => undefined,
+  onChange: () => undefined,
 };
 
 export default PrivateKeyTextInputContainer;
