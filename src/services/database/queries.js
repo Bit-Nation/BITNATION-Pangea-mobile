7dda3c42c4701a8e0fe025fe5dd06a2f7c5b55f8
Florian Lenz <flenz81@googlemail.com>
2018-03-16 21:08:37 +0100

    [db] removed queries

--- src/services/database/queries.js
+++
@@ -1,8 +0,0 @@
-// @flow
-
-import type {ProfileType} from './schemata';
-const Realm = require('realm');
-
-export default {
-    findProfiles: (realm: Realm): Array<ProfileType> => realm.objects('Profile'),
-};
