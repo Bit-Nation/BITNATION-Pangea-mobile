baa2316888487469dc56ebd095a4fbd1afd42eb8
swmd <frontdev8@gmail.com>
2018-03-16 21:26:16 +0100

    Add snapshot test of ChatNationsListScreen

---
+++ __tests__/src/screens/ChatNationsScreen/ChatNationsListScreen/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import configureStore from 'redux-mock-store';
+
+import ChatNationsListScreen from '../../../../../src/screens/ChatNationsScreen/ChatNationsListScreen';
+
+describe('ChatNationsListScreen', () => {
+  let chatListScreen;
+
+  const initialStateMock = {};
+  const storeMock = configureStore([]);
+  const propsMock = {
+    onSelectItem: jest.fn(),
+    selectedTab: 'ALL_NATIONS',
+    nations: [],
+    myNations: []
+  };
+
+  beforeEach(() => {
+    chatListScreen = shallow(<ChatNationsListScreen {...propsMock} store={storeMock(initialStateMock)} />);
+  });
+
+  test('Rendering', () => {
+    expect(chatListScreen).toMatchSnapshot();
+
+  });
+});
