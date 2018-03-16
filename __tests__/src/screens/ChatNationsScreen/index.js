27add71313d1d057f8c4666f7c2a3992319c7d8d
Andrii Selivanov <seland2008@gmail.com>
2018-03-16 17:05:34 +0200

    [project] Add redux-mock-store and comments.

--- __tests__/src/screens/ChatNationsScreen/index.js
+++ __tests__/src/screens/ChatNationsScreen/index.js
@@ -1,25 +1,27 @@
-import 'jsdom-global/register';
 import React from 'react';
 import { shallow } from 'enzyme';
+import configureStore from 'redux-mock-store';
 
 import ChatNationsScreen from '../../../../src/screens/ChatNationsScreen';
 
 describe('ChatNationsScreen', () => {
-
   let chatScreen;
+
+  const initialStateMock = {};
+  const storeMock = configureStore([]);
   const propsMock = {
     navigator: {
       push: jest.fn(),
-      setButtons: jest.fn()
+      setButtons: jest.fn(),
     },
     onSelectTab: jest.fn(),
     openNation: jest.fn(),
     fetchNations: jest.fn(),
-    startNationCreation: jest.fn()
+    startNationCreation: jest.fn(),
   };
 
   beforeEach(() => {
-    chatScreen = shallow(<ChatNationsScreen {...propsMock} />);
+    chatScreen = shallow(<ChatNationsScreen {...propsMock} store={storeMock(initialStateMock)} />);
   });
 
   test('Rendering', () => {
@@ -31,6 +33,9 @@ describe('ChatNationsScreen', () => {
   });
 
   test('initialization', () => {
+    // @todo This won't be true, since propsMock object is not passed directly to component, but it's copy is.
+    // @todo So, this function won't be called on original object.
+    // @todo Probably, you can check that in beforeEach as a post-condition of initialization.
     expect(propsMock.navigator.setButtons).toBeCalled();
     expect(propsMock.fetchNations).toBeCalled();
   });
