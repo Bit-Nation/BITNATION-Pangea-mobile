6eaec0ab08daa0e1ffffd0f125544457b1439bf1
Alberto Estarrona <alberto@estarrona.me>
2018-03-19 18:23:12 -0600

    [fix] Lint fixes for Nation Details screens

--- src/screens/NationDetailsContainer/NationDetailsScreen/index.js
+++ src/screens/NationDetailsContainer/NationDetailsScreen/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {
   View,
-  Text, ScrollView, Image, StatusBar, Alert,
+  Text, ScrollView,
 } from 'react-native';
 import PropTypes from 'prop-types';
 
@@ -12,56 +12,13 @@ import AssetsImage from '../../../global/AssetsImages';
 import PanelView from '../../../components/common/PanelView';
 import FakeNavigationBar from '../../../components/common/FakeNavigationBar';
 import i18n from '../../../global/i18n';
-import Colors from '../../../global/colors';
-import { screen } from '../../../global/Screens';
 import { openedNation } from '../../../reducers/nations';
 import PanelViewAlert from '../../../components/common/PanelViewAlert';
 import PanelViewCitizen from '../../../components/common/PanelViewCitizen';
 import { nationIsValid, resolveStatus } from '../../../utils/nations';
-import pangeaLibs from '../../../services/container';
 
 class NationDetailsScreen extends Component {
-
-  render() {
-    const nation = openedNation(this.props);
-    console.log('nation opened: ', nation);
-
-    if (!nation) {
-      this.props.navigator.pop();
-      return <BackgroundImage />;
-    }
-
-    const status = resolveStatus(nation);
-    const statusDescription = (status !== null ? i18n.ifExists(`screens.nationDetails.statusDescription.${status.key}`) : '');
-
-    return (
-      <View style={styles.screenContainer}>
-        <BackgroundImage />
-        <FakeNavigationBar navBarHidden='' />
-        <View style={styles.bodyContainer}>
-          {/* TITLE OF SCREEN */}
-          <View style={styles.titleContainer}>
-            <View style={styles.titleBarLarge}>
-              <Text style={styles.largeTitle}>{nation.nationName}</Text>
-            </View>
-          </View>
-
-          <ScrollView>
-            {statusDescription !== '' && this._buildStatusPanel(statusDescription)}
-
-            {this._buildAboutView(nation)}
-            {/*  Will show Panel of Citizenship if nation.joinend == true */}
-            {this._buildCitizenPanel(nation)}
-            {this._buildGovernmentalStructureView(nation)}
-            {this._buildFactsView(nation)}
-          </ScrollView>
-        </View>
-        {this._buildTabBar()}
-      </View>
-    );
-  }
-
-  _disableJoinButton(nation) {
+  static disableJoinButton(nation) {
     if (nation.tx && nation.tx.status === 200) {
       return true;
     }
@@ -73,7 +30,7 @@ class NationDetailsScreen extends Component {
     return false;
   }
 
-  _disableLeaveButton(nation) {
+  static disableLeaveButton(nation) {
     if (nation.tx && nation.tx.status === 200) {
       return true;
     }
@@ -85,7 +42,103 @@ class NationDetailsScreen extends Component {
     return false;
   }
 
-  _buildTabBar() {
+  static buildAboutView(nation) {
+    return (
+      <PanelView
+        style={styles.panelView}
+        childrenContainerStyle={styles.noflex}
+        title={i18n.t('screens.nationDetails.aboutInfo', { name: nation.nationName })}
+      >
+        <Text style={styles.panelSubTitle}>
+          {`${i18n.t('screens.nationDetails.description')}:`}
+        </Text>
+        <Text style={styles.body}>
+          {nation.nationDescription ? `${nation.nationDescription}\n` : ''}
+          {`${i18n.t('screens.nationDetails.locationInfo', {
+            name: nation.nationName,
+            locationType: nation.exists ?
+              i18n.t('enums.nation.locationType.geographical') :
+              i18n.t('enums.nation.locationType.virtual'),
+          })}\n`}
+        </Text>
+        <Text style={styles.panelSubTitle}>
+          {i18n.t('screens.nationDetails.ethereumAddress')}
+        </Text>
+        <Text style={styles.footnote}>
+          {nation.ethAddress}
+        </Text>
+      </PanelView>
+    );
+  }
+
+  static buildGovernmentalStructureView(nation) {
+    return (
+      <PanelView
+        style={styles.panelView}
+        childrenContainerStyle={styles.noflex}
+        title={i18n.t('common.governmentalStructure')}
+      >
+        <Text style={styles.body}>
+          {i18n.t('screens.nationDetails.legalSystemInfo', {
+            name: nation.nationName,
+            code: nation.nationCode,
+          })}
+          {' '}
+          {i18n.t('screens.nationDetails.lawEnforcementInfo', {
+            lawEnforcementMechanism: nation.lawEnforcementMechanism,
+          })}
+          {' '}
+          {i18n.t('screens.nationDetails.governmentInfo', {
+            decisionMakingProcess: nation.decisionMakingProcess,
+          })}
+        </Text>
+      </PanelView>
+    );
+  }
+
+  static buildFactsView(nation) {
+    return (
+      <PanelView
+        style={styles.panelView}
+        childrenContainerStyle={styles.noflex}
+        title={i18n.t('screens.nationDetails.funFacts')}
+      >
+        <Text style={styles.body}>
+          {nation.diplomaticRecognition ? (`${i18n.t('screens.nationDetails.diplomaticRecognitionInfo', { name: nation.nationName })}\n\n`) : ''}
+
+          {i18n.t('screens.nationDetails.serviceUsageInfo', {
+            allowance: nation.createNationsNonCitizensMayUseGovernanceServices ? i18n.t('enums.nation.usageAllowance.may') : i18n.t('enums.nation.usageAllowance.mayNot'),
+          })}
+          {'\n\n'}
+
+          {i18n.t('screens.nationDetails.profitInfo', {
+            profit: nation.profit ? i18n.t('enums.nation.profit.for') : i18n.t('enums.nation.profit.non'),
+          })}
+        </Text>
+      </PanelView>
+    );
+  }
+
+  static buildStatusPanel(status) {
+    return (
+      <PanelViewAlert
+        status={status}
+      />
+    );
+  }
+
+  static buildCitizenPanel(nation) {
+    if (nation.joined) {
+      return (
+        <PanelViewCitizen
+          nationName={nation.nationName}
+        />
+      );
+    }
+    return false;
+  }
+
+  buildTabBar() {
     const nation = openedNation(this.props);
 
     if (this.props.isDraft) {
@@ -124,124 +177,73 @@ class NationDetailsScreen extends Component {
         <NationActionButton
           iconSource={AssetsImage.Actions.join}
           title={i18n.t('screens.nations.toolbar.join')}
-          disable={this._disableJoinButton(nation)}
+          disable={this.disableJoinButton(nation)}
           onPress={this.props.joinNation}
         />
         <NationActionButton
           iconSource={AssetsImage.Actions.leave}
           title={i18n.t('screens.nations.toolbar.leave')}
-          disable={this._disableLeaveButton(nation)}
+          disable={this.disableLeaveButton(nation)}
           onPress={this.props.leaveNation}
         />
       </View>
     );
   }
+  render() {
+    const nation = openedNation(this.props);
 
-  // Useful Notes:
-  // PanelView Props: title = text, messageText = text, style, renderBottom = method, renderAdditionalInfo = method, children = main text of the display
-
-  _buildAboutView(nation) {
-    return (
-      <PanelView
-        style={styles.panelView}
-        childrenContainerStyle={{ flex: 0 }}
-        title={i18n.t('screens.nationDetails.aboutInfo', { name: nation.nationName })}
-      >
-        <Text style={styles.panelSubTitle}>
-          {`${i18n.t('screens.nationDetails.description')}:`}
-        </Text>
-        <Text style={styles.body}>
-          {nation.nationDescription ? `${nation.nationDescription}\n` : ''}
-          {`${i18n.t('screens.nationDetails.locationInfo', {
-            name: nation.nationName,
-            locationType: nation.exists ?
-              i18n.t('enums.nation.locationType.geographical') :
-              i18n.t('enums.nation.locationType.virtual'),
-          })}\n`}
-        </Text>
-        <Text style={styles.panelSubTitle}>
-          {i18n.t('screens.nationDetails.ethereumAddress')}
-        </Text>
-        <Text style={styles.footnote}>
-          {nation.ethAddress}
-        </Text>
-      </PanelView>
-    );
-  }
-
-  _buildGovernmentalStructureView(nation) {
-    return (
-      <PanelView
-        style={styles.panelView}
-        childrenContainerStyle={{ flex: 0 }}
-        title={i18n.t('common.governmentalStructure')}
-      >
-        <Text style={styles.body}>
-          {i18n.t('screens.nationDetails.legalSystemInfo', {
-            name: nation.nationName,
-            code: nation.nationCode,
-          })}
-          {' '}
-          {i18n.t('screens.nationDetails.lawEnforcementInfo', {
-            lawEnforcementMechanism: nation.lawEnforcementMechanism,
-          })}
-          {' '}
-          {i18n.t('screens.nationDetails.governmentInfo', {
-            decisionMakingProcess: nation.decisionMakingProcess,
-          })}
-        </Text>
-      </PanelView>
-    );
-  }
-
-  _buildFactsView(nation) {
-    return (
-      <PanelView
-        style={styles.panelView}
-        childrenContainerStyle={{ flex: 0 }}
-        title={i18n.t('screens.nationDetails.funFacts')}
-      >
-        <Text style={styles.body}>
-          {nation.diplomaticRecognition ? (`${i18n.t('screens.nationDetails.diplomaticRecognitionInfo', { name: nation.nationName })}\n\n`) : ''}
-
-          {i18n.t('screens.nationDetails.serviceUsageInfo', {
-            allowance: nation.createNationsNonCitizensMayUseGovernanceServices ? i18n.t('enums.nation.usageAllowance.may') : i18n.t('enums.nation.usageAllowance.mayNot'),
-          })}
-          {'\n\n'}
-
-          {i18n.t('screens.nationDetails.profitInfo', {
-            profit: nation.profit ? i18n.t('enums.nation.profit.for') : i18n.t('enums.nation.profit.non'),
-          })}
-        </Text>
-      </PanelView>
-    );
-  }
-
-  _buildStatusPanel(status) {
-    return (
-      <PanelViewAlert
-        status={status}
-      />
-    );
-  }
-
-  _buildCitizenPanel(nation) {
-    if (nation.joined) {
-      return (
-        <PanelViewCitizen
-          nationName={nation.nationName}/>
-      );
+    if (!nation) {
+      this.props.navigator.pop();
+      return <BackgroundImage />;
     }
+
+    const status = resolveStatus(nation);
+    const statusDescription = (status !== null ? i18n.ifExists(`screens.nationDetails.statusDescription.${status.key}`) : '');
+
+    return (
+      <View style={styles.screenContainer}>
+        <BackgroundImage />
+        <FakeNavigationBar navBarHidden='' />
+        <View style={styles.bodyContainer}>
+          <View style={styles.titleContainer}>
+            <View style={styles.titleBarLarge}>
+              <Text style={styles.largeTitle}>{nation.nationName}</Text>
+            </View>
+          </View>
+
+          <ScrollView>
+            {statusDescription !== '' && this.buildStatusPanel(statusDescription)}
+
+            {this.buildAboutView(nation)}
+            {this.buildCitizenPanel(nation)}
+            {this.buildGovernmentalStructureView(nation)}
+            {this.buildFactsView(nation)}
+          </ScrollView>
+        </View>
+        {this.buildTabBar()}
+      </View>
+    );
   }
 }
 
 NationDetailsScreen.propTypes = {
+  navigator: PropTypes.shape({ pop: {} }),
   isDraft: PropTypes.bool,
   joinNation: PropTypes.func,
   leaveNation: PropTypes.func,
   deleteDraft: PropTypes.func,
   submitDraft: PropTypes.func,
   openNationChat: PropTypes.func,
+};
+
+NationDetailsScreen.defaultProps = {
+  navigator: null,
+  isDraft: null,
+  joinNation: () => null,
+  leaveNation: () => null,
+  deleteDraft: () => null,
+  submitDraft: () => null,
+  openNationChat: () => null,
 };
 
 export default NationDetailsScreen;
