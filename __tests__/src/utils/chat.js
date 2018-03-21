856c231aebd2eedf1c452290de578a6b312668ec
swmd <frontdev8@gmail.com>
2018-03-21 19:19:02 +0100

    Add unit test of chat utility function

---
+++ __tests__/src/utils/chat.js
@@ -0,0 +1,25 @@
+import createGiftedChatMessageObject from '../../../src/utils/chat';
+
+test('createGiftedChatMessageObject', () => {
+	const messageData = [
+		{
+			_id: 1,
+			msg: 'hello',
+			createdAt: '21-03-2018 18:38:34',
+			userId: 4,
+			from: 'mockUser'
+		}
+	];
+	const result = [
+		{
+      _id: messageData[0]._id,
+      text: messageData[0].msg,
+      createdAt: messageData[0].createdAt,
+      user: {
+        _id: messageData[0].userId,
+        name: messageData[0].from,
+      },
+    }
+	];
+  expect(createGiftedChatMessageObject(messageData)).toEqual(result);
+});
