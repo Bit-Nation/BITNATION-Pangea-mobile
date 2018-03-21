25769431f7ab0e617a98f284b7fa5d74c4de13a5
swmd <frontdev8@gmail.com>
2018-03-21 19:59:32 +0100

    Implement flow type check of chat nations list screen

--- src/screens/ChatNationsScreen/index.js
+++ src/screens/ChatNationsScreen/index.js
@@ -1,3 +1,5 @@
+// @flow
+
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
@@ -8,10 +10,29 @@ import { screen } from '../../global/Screens';
 import { resolveNation } from '../../utils/nations';
 import NavigatorComponent from '../../components/common/NavigatorComponent';
 import { startNationCreation } from '../../actions/modifyNation';
+import type { NationType } from '../../services/database/schemata';
 
+type Props = {
+  /**
+   * @desc React Native Navigation navigator object.
+   */
+  navigator: any,
+  /**
+   * @desc List of nations
+   */
+  nations?: Array<NationType>,
+  /**
+   * @desc Function to retrieve nations from the database
+   */
+  fetchNations: () => void,
+  /**
+   * @desc Function to open a nation
+   */
+  openNation: () => void,
+};
 
-class ChatNationsScreen extends NavigatorComponent {
-  constructor(props) {
+class ChatNationsScreen extends NavigatorComponent<Props> {
+  constructor(props: Props) {
     super(props);
 
     this.props.navigator.setButtons({
