2c2ac95f55fd13f0c09d47808d674d3b395d2afd
Daniel França <daniel.franca@protonmail.com>
2018-03-29 00:05:46 +0200

    The documentation doesnt recognize import with suffixes like .ios/.android So I just renamed it to .android, so it works, not sure if it impacts everyhing else, in that case we need a better solution

---
+++ src/screens/WalletScreen/QRCodeScanner/index.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+import Colors from '../../../global/colors';
+import NavigatorComponent from '../../../components/common/NavigatorComponent';
+import FakeNavigationBar from '../../../components/common/FakeNavigationBar';
+
+export default class QRCodeScannerScreen extends NavigatorComponent {
+  static navigatorButtons = {
+    leftButtons: [{
+      id: 'cancel',
+      title: 'Cancel',
+      buttonColor: Colors.navigationButtonColor,
+    }],
+    rightButtons: [],
+  };
+
+  onNavBarButtonPress(id) {
+    if (id === 'cancel') {
+      this.props.navigator.dismissModal();
+    }
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1, backgroundColor: 'black' }}>
+        <FakeNavigationBar />
+        <Text>
+          Android QR code scanner is not supported yet.
+        </Text>
+      </View>
+    );
+  }
+}
+
+QRCodeScannerScreen.propTypes = {
+  onReadCode: PropTypes.func,
+};
