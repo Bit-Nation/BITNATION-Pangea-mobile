4d78faf73523d6698018aa45ae0ec2b09de685bf
Andrii Selivanov <seland2008@gmail.com>
2018-04-22 22:55:49 +0300

    [refactor] Make saga declarative.

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,4 +1,4 @@
-import { all } from 'redux-saga/effects';
+import { all, call } from 'redux-saga/effects';
 
 import accounts from './accounts';
 import navigation from './navigation';
@@ -9,7 +9,7 @@ import navigation from './navigation';
  */
 export default function* rootSaga() {
   yield all([
-    accounts(),
-    navigation(),
+    call(accounts),
+    call(navigation),
   ]);
 }
