168105d05466d70bbab86d45c36295d64208c4d4
Andrii Selivanov <seland2008@gmail.com>
2018-04-25 23:12:55 +0300

    [project] Add default props to enter/create passcode screens.
    Former-commit-id: a86b150340e297238dd8a465d847410553cdb695

--- src/screens/Passcode/CreatePasscodeContainer.js
+++ src/screens/Passcode/CreatePasscodeContainer.js
@@ -37,7 +37,7 @@ type Props = {
   /**
    * @desc Id of account which passcode will be created or edited.
    */
-  accountId: string,
+  accountId: ?string,
 }
 
 type Actions = {
@@ -61,6 +61,8 @@ type State = {
 }
 
 class CreatePasscodeContainer extends NavigatorComponent<Props & Actions & SettingsState, State> {
+  static defaultProps;
+
   constructor(props: Props & Actions & SettingsState) {
     super(props);
 
@@ -197,6 +199,13 @@ class CreatePasscodeContainer extends NavigatorComponent<Props & Actions & Setti
       />);
   }
 }
+
+CreatePasscodeContainer.defaultProps = {
+  onCancel: () => undefined,
+  onSuccess: () => undefined,
+  title: '',
+  accountId: null,
+};
 
 const mapStateToProps = state => ({
   ...state.settings,
