56d2673bee27290cda735702842c59627a1cd0d1
Andrii Selivanov <seland2008@gmail.com>
2018-05-03 17:25:53 +0300

    [project] Revert mnemonic join function changes.
    Former-commit-id: d84b4e0761c1939586794d605bd45ee9fd6d3eac

--- __tests__/src/utils/key.js
+++ __tests__/src/utils/key.js
@@ -3,5 +3,5 @@ import { compressMnemonic } from '../../../src/utils/key';
 test('compressMnemonic', () => {
   expect(compressMnemonic([
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'ij', 'k', 'l', 'mn', 'o', 'pqr', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'one', 'more',
-  ])).toBe('a,b,c,d,e,f,g,h,ij,k,l,mn,o,pqr,s,t,u,v,w,x,y,z,one,more');
+  ])).toBe('a b c d e f g h ij k l mn o pqr s t u v w x y z one more');
 });
