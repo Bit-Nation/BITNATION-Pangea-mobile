8ae903e1ed9821948436aaaa729a53b134461bb5
William <william2958@gmail.com>
2018-05-09 16:51:44 -0400

    Begin implementation of send transaction method
    Former-commit-id: 232aebf1f8aa2b3d7fb7f666c27db6b83834da0b

--- src/services/ethereum/index.js
+++ src/services/ethereum/index.js
@@ -1,11 +1,28 @@
 // @flow
+import ethers from 'ethers';
+
 
 export default class EthereumService {
+
+  static async initializeWallet(privateKey: string) {
+    this.wallet = new ethers.Wallet('0x0123456789012345678901234567890123456789012345678901234567890123');
+    this.wallet.provider = ethers.providers.getDefaultProvider();
+  }
+
   static async sendMoney(fromAddress: string, toAddress: string, amount: string): Promise<void> {
-    console.log('CALL!');
-    console.log(fromAddress);
-    console.log(toAddress);
-    console.log(amount);
-    // @todo Implement
+    console.log('Send Money called!');
+
+    const transaction = {
+      gasLimit: 1000000,
+      gasPrice: ethers.utils.bigNumberify('20000000000'),
+      to: '0x88a5C2d9919e46F883EB62F7b8Dd9d0CC45bc290',
+      data: '0x',
+      value: ethers.utils.parseEther('1.0'),
+    };
+
+    const gasEstimate = await this.wallet.estimateGas(transaction);
+    console.log('estimated gas: ', gasEstimate);
+
+    return this.wallet.send(transaction);
   }
 }
