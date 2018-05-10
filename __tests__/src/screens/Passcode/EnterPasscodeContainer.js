0a30ba1a97f88747a2d169a612b97a06af8945e0
swmd <frontdev8@gmail.com>
2018-05-10 03:35:55 +0200

    add tests of passcode screen
    Former-commit-id: f8fb88f1bcc2b91d1cd637a6b14b8a3f84d101b8

---
+++ __tests__/src/screens/Passcode/EnterPasscodeContainer.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import configureStore from 'redux-mock-store';
+
+import EnterPasscodeContainer from '../../../../src/screens/Passcode/EnterPasscodeContainer';
+
+describe('EnterPasscodeContainer', () => {
+  let enterPasscodeContainer;
+
+  const initialStateMock = {
+    accounts: {
+      login: {
+        error: 'Invalid'
+      }
+    },
+    settings: {
+      passcodeType: {
+        type: 'pinCode',
+        length: 6
+      }
+    }
+  };
+  const storeMock = configureStore([]);
+  const propsMock = {
+    navigator: {
+      push: jest.fn(),
+      pop: jest.fn(),
+      setOnNavigatorEvent: jest.fn()
+    },
+    checkPinCode: jest.fn(),
+    checkPassword: jest.fn(),
+    login: jest.fn(),
+    onSuccess: jest.fn(),
+    onCancel: jest.fn()
+  };
+
+  beforeEach(() => {
+    enterPasscodeContainer = shallow(<EnterPasscodeContainer {...propsMock} store={storeMock(initialStateMock)} />);
+  });
+
+  test('Rendering', () => {
+    expect(enterPasscodeContainer).toMatchSnapshot();
+  });
+
+  test('check required props', () => {
+    expect(enterPasscodeContainer.props().navigator).toBeDefined();
+  });
+});
