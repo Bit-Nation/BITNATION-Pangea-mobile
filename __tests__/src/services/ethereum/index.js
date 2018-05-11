36a7860b0e4af397727de0ee66743c182a840d9a
William <william2958@gmail.com>
2018-05-11 10:46:32 -0400

    Set up testing

--- __tests__/src/services/ethereum/index.js
+++ __tests__/src/services/ethereum/index.js
@@ -1,7 +1,8 @@
-import ethereum from '../../../../src/services/ethereum/index';
+import Ethereum from '../../../../src/services/ethereum/index';
 
 describe('ethereum', () => {
   test('check test', async () => {
+    const ethereum = new Ethereum();
     await ethereum.initializeWallet();
     console.log('initialized wallet');
   });
