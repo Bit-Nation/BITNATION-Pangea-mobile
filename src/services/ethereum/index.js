f6648398f869a299a5f79b072e3a476e2398f01d
William <william2958@gmail.com>
2018-05-11 10:46:32 -0400

    Set up testing
    Former-commit-id: 36a7860b0e4af397727de0ee66743c182a840d9a

--- src/services/ethereum/index.js
+++ src/services/ethereum/index.js
@@ -8,6 +8,7 @@ export default class EthereumService {
   async initializeWallet() {
     try {
       const testingPanthalassa = NativeModules.Panthalassa;
+      console.log('testingPanthalassa: ', testingPanthalassa);
       await testingPanthalassa.PanthalassaStart({ accountStore: ' {“password”:“{\\“cipher_text\\“:\\“olDYk8en3X88H1faxQ_CGBIkvyUSnw==\\“,\\“scrypt_key\\“:{\\“n\\“:16384,\\“r\\“:8,\\“p\\“:1,\\“key_len\\“:32,\\“salt\\“:\\“eHunCXrfM2u4xtiv8/UeFrBOmVuxFTK0L5AYiT/VUZrKLyBCblapLwXSWfGkrC4RoDk=\\“}}“,”encrypted_key_store”:“{\\“cipher_text\\“:\\“8A-HZtEIjC_w764MJe30HRLY-BV74tmt5sN_zfW9riChwtJX4TcShjUFFLOa6MscQ61_e5q3k37BkFE0g1_al105kvq_CKVX1AIjAez4AdgxW4kceZ76SzwmSi6gJ2U3WXs-HvWTmh_FZHEU-8cbc9d15eNjyQBRXy3uLueuY97P9jhB35PmG9ImlPXdIQkR-6caGBllhBI1IcGBg3MbP2WVG_mZIdU7VugQp1Q_j_XGiS3vcwP6Utc13PXWIqq7HvxvtIB0CiqxH62KB8wCGa1qk1M89YqK_cd6wBscxTNtHs2ZRZNWrkilvH3ScA-ivIpZepTBwegZBdHtOq7OGMzm7nLfjkcHEUPZ-AoQbIfX3cERXK0cTQ0yueGIjXRhcjxfxQ==\\“,\\“scrypt_key\\“:{\\“n\\“:16384,\\“r\\“:8,\\“p\\“:1,\\“key_len\\“:32,\\“salt\\“:\\“MU3tzj7CevzBGfs3Z9/9brOJO5BBXhkctzl4ygnGwQKo/AArVWkAF/SMit7G+pEs1vM=\\“}}“,”version”:1}', password: '111111' });
       const result = await testingPanthalassa.PanthalassaEthPrivateKey();
       console.log('Panthalassa: ', result);
