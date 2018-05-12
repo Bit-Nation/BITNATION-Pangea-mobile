335c5f7e5bb407437a79ba8ea0215546df45e732
Alberto Estarrona <alberto@estarrona.me>
2018-05-12 13:44:55 -0500

    [ui] Fixed bug in UI with screen size on empty wallet

--- src/screens/WalletScreen/EmptyState/index.js
+++ src/screens/WalletScreen/EmptyState/index.js
@@ -1,7 +1,7 @@
 // @flow
 
 import React from 'react';
-import { View } from 'react-native';
+import { View, ScrollView } from 'react-native';
 
 import styles from './styles';
 import PanelView from '../../../components/common/PanelView';
@@ -26,20 +26,22 @@ type Props = {
  */
 const EmptyWalletScreen = ({ onCreateWallet, onRestoreWallet }: Props) => ((
   <View>
-    <PanelView
-      style={styles.panelViewTransparent}
-      title={i18n.t('screens.wallet.empty.createWalletPanel.title')}
-      body={i18n.t('screens.wallet.empty.createWalletPanel.text')}
-      buttonTitle={i18n.t('screens.wallet.empty.createWalletPanel.button')}
-      onButtonClick={onCreateWallet}
-    />
-    <PanelView
-      style={styles.panelViewTransparent}
-      title={i18n.t('screens.wallet.empty.restoreWalletPanel.title')}
-      body={i18n.t('screens.wallet.empty.restoreWalletPanel.text')}
-      buttonTitle={i18n.t('screens.wallet.empty.restoreWalletPanel.button')}
-      onButtonClick={onRestoreWallet}
-    />
+    <ScrollView style={styles.scrollView} contentContainerStyle={styles.noflex}>
+      <PanelView
+        style={styles.panelViewTransparent}
+        title={i18n.t('screens.wallet.empty.createWalletPanel.title')}
+        body={i18n.t('screens.wallet.empty.createWalletPanel.text')}
+        buttonTitle={i18n.t('screens.wallet.empty.createWalletPanel.button')}
+        onButtonClick={onCreateWallet}
+      />
+      <PanelView
+        style={styles.panelViewTransparent}
+        title={i18n.t('screens.wallet.empty.restoreWalletPanel.title')}
+        body={i18n.t('screens.wallet.empty.restoreWalletPanel.text')}
+        buttonTitle={i18n.t('screens.wallet.empty.restoreWalletPanel.button')}
+        onButtonClick={onRestoreWallet}
+      />
+    </ScrollView>
   </View>
 ));
 
