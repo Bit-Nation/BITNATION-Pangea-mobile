d5053b0f569299c1e0b6420f4408f358094a3cd3
swmd <frontdev8@gmail.com>
2018-05-12 05:53:16 +0200

    create utility functions for activity

---
+++ src/utils/mapping/activity.js
@@ -0,0 +1,36 @@
+// @flow
+
+import type { ActivityLogMessage } from '../../types/ActivityLogMessage';
+import { type MessageJobType as DBMessage } from '../../services/database/schemata';
+
+/**
+ * @desc Converts Realm object to app-level plain model.
+ * @param {DBMessage} messageJob Realm message object.
+ * @return {ActivityLogMessage} Converted message object.
+ */
+export function convertFromDatabase(messageJob: DBMessage): ActivityLogMessage {
+  return {
+    id: messageJob.id,
+    msg: messageJob.msg,
+    params: messageJob.params,
+    interpret: messageJob.interpret,
+  };
+}
+
+/**
+ * @desc Converts message object to Realm object if it's possible.
+ * @param {ActivityLogMessage} messageObject Message object to be converted.
+ * @return {?DBMessage} Converted object.
+ */
+export function convertToDatabase(messageObject: ActivityLogMessage): DBMessage | null {
+  return {
+    id: messageObject.id,
+    heading: null,
+    msg: messageObject.msg,
+    params: messageObject.params,
+    version: 1,
+    display: true,
+    interpret: messageObject.interpret,
+    created_at: new Date()
+  };
+}
