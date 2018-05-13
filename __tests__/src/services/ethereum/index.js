36a64ef1b7f699ab1d9598322b7ecb2f3f8dde83
William <william2958@gmail.com>
2018-05-13 13:50:31 -0400

    Added parameters to sendMoney

--- __tests__/src/services/ethereum/index.js
+++ __tests__/src/services/ethereum/index.js
@@ -1,9 +1,15 @@
 import Ethereum from '../../../../src/services/ethereum/index';
 
 describe('ethereum', () => {
-  test('check test', async () => {
-    const ethereum = new Ethereum();
+  let ethereum;
+  beforeAll(() => {
+    ethereum = new Ethereum();
+  });
+  test('Initialize wallet', async () => {
     await ethereum.initializeWallet();
     console.log('initialized wallet');
+  });
+  test('Send ether', async () => {
+    await ethereum.sendMoney('0x88a5C2d9919e46F883EB62F7b8Dd9d0CC45bc290', '1.0', '2000000000')
   });
 });
--- src/services/ethereum/index.js
+++ src/services/ethereum/index.js
@@ -1,40 +1,43 @@
 import ethers from 'ethers';
 import { Alert, NativeModules } from 'react-native';
 
+const myMock = jest.fn();
+myMock.mockReturnValueOnce('0x0123456789012345678901234567890123456789012345678901234567890123');
+
 export default class EthereumService {
 
   wallet = {};
 
   async initializeWallet() {
     try {
-      const testingPanthalassa = NativeModules.Panthalassa;
-      console.log('testingPanthalassa: ', testingPanthalassa);
-      await testingPanthalassa.PanthalassaStart({ accountStore: ' {“password”:“{\\“cipher_text\\“:\\“olDYk8en3X88H1faxQ_CGBIkvyUSnw==\\“,\\“scrypt_key\\“:{\\“n\\“:16384,\\“r\\“:8,\\“p\\“:1,\\“key_len\\“:32,\\“salt\\“:\\“eHunCXrfM2u4xtiv8/UeFrBOmVuxFTK0L5AYiT/VUZrKLyBCblapLwXSWfGkrC4RoDk=\\“}}“,”encrypted_key_store”:“{\\“cipher_text\\“:\\“8A-HZtEIjC_w764MJe30HRLY-BV74tmt5sN_zfW9riChwtJX4TcShjUFFLOa6MscQ61_e5q3k37BkFE0g1_al105kvq_CKVX1AIjAez4AdgxW4kceZ76SzwmSi6gJ2U3WXs-HvWTmh_FZHEU-8cbc9d15eNjyQBRXy3uLueuY97P9jhB35PmG9ImlPXdIQkR-6caGBllhBI1IcGBg3MbP2WVG_mZIdU7VugQp1Q_j_XGiS3vcwP6Utc13PXWIqq7HvxvtIB0CiqxH62KB8wCGa1qk1M89YqK_cd6wBscxTNtHs2ZRZNWrkilvH3ScA-ivIpZepTBwegZBdHtOq7OGMzm7nLfjkcHEUPZ-AoQbIfX3cERXK0cTQ0yueGIjXRhcjxfxQ==\\“,\\“scrypt_key\\“:{\\“n\\“:16384,\\“r\\“:8,\\“p\\“:1,\\“key_len\\“:32,\\“salt\\“:\\“MU3tzj7CevzBGfs3Z9/9brOJO5BBXhkctzl4ygnGwQKo/AArVWkAF/SMit7G+pEs1vM=\\“}}“,”version”:1}', password: '111111' });
-      const result = await testingPanthalassa.PanthalassaEthPrivateKey();
-      console.log('Panthalassa: ', result);
+      // const testingPanthalassa = NativeModules.Panthalassa;
+      // await testingPanthalassa.PanthalassaStart({ accountStore: ' {“password”:“{\\“cipher_text\\“:\\“olDYk8en3X88H1faxQ_CGBIkvyUSnw==\\“,\\“scrypt_key\\“:{\\“n\\“:16384,\\“r\\“:8,\\“p\\“:1,\\“key_len\\“:32,\\“salt\\“:\\“eHunCXrfM2u4xtiv8/UeFrBOmVuxFTK0L5AYiT/VUZrKLyBCblapLwXSWfGkrC4RoDk=\\“}}“,”encrypted_key_store”:“{\\“cipher_text\\“:\\“8A-HZtEIjC_w764MJe30HRLY-BV74tmt5sN_zfW9riChwtJX4TcShjUFFLOa6MscQ61_e5q3k37BkFE0g1_al105kvq_CKVX1AIjAez4AdgxW4kceZ76SzwmSi6gJ2U3WXs-HvWTmh_FZHEU-8cbc9d15eNjyQBRXy3uLueuY97P9jhB35PmG9ImlPXdIQkR-6caGBllhBI1IcGBg3MbP2WVG_mZIdU7VugQp1Q_j_XGiS3vcwP6Utc13PXWIqq7HvxvtIB0CiqxH62KB8wCGa1qk1M89YqK_cd6wBscxTNtHs2ZRZNWrkilvH3ScA-ivIpZepTBwegZBdHtOq7OGMzm7nLfjkcHEUPZ-AoQbIfX3cERXK0cTQ0yueGIjXRhcjxfxQ==\\“,\\“scrypt_key\\“:{\\“n\\“:16384,\\“r\\“:8,\\“p\\“:1,\\“key_len\\“:32,\\“salt\\“:\\“MU3tzj7CevzBGfs3Z9/9brOJO5BBXhkctzl4ygnGwQKo/AArVWkAF/SMit7G+pEs1vM=\\“}}“,”version”:1}', password: '111111' });
+      // const result = await testingPanthalassa.PanthalassaEthPrivateKey();
+      const result = await myMock();
       if (result !== null) {
         console.log('Private Key successfully obtained');
         this.wallet = new ethers.Wallet('0x0123456789012345678901234567890123456789012345678901234567890123');
         this.wallet.provider = ethers.providers.getDefaultProvider();
+        console.log('wallet: ', this.wallet);
       }
     } catch (e) {
       console.error(e.message);
     }
   }
 
-  async sendMoney(fromAddress: string, toAddress: string, amount: string): Promise<void> {
-    console.log('Send Money called!');
+  async sendMoney(toAddress: string, amount: string, gasPrice: string): Promise<void> {
 
     const transaction = {
-      gasLimit: 1000000,
-      gasPrice: ethers.utils.bigNumberify('20000000000'),
-      to: '0x88a5C2d9919e46F883EB62F7b8Dd9d0CC45bc290',
+      gasLimit: 21000,
+      gasPrice: ethers.utils.bigNumberify(gasPrice),
+      to: toAddress,
       data: '0x',
-      value: ethers.utils.parseEther('1.0'),
+      value: ethers.utils.parseEther(amount),
     };
 
     const gasEstimate = await this.wallet.estimateGas(transaction);
     console.log('estimated gas: ', gasEstimate);
+    console.log('transaction: ', transaction);
 
     return this.wallet.send(transaction);
   }
