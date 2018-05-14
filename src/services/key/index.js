e034c71a8073603f44856726902b729c7500e250
Alberto Estarrona <alberto@estarrona.me>
2018-05-14 13:24:11 -0500

    [project] Wallet refactoring

--- src/services/key/index.js
+++ src/services/key/index.js
@@ -1,37 +1,8 @@
 // @flow
 /* eslint-disable */
 
-import containerPromise from '../../services/container';
 import { compressMnemonic } from '../../utils/key';
 
 export default class KeyService {
-  static async createPrivateKey() {
-    const container = await containerPromise;
-    return await container.eth.utils.createPrivateKey();
-  }
 
-  static async privateKeyToMnemonic(privateKey) {
-    const container = await containerPromise;
-    return await container.eth.utils.privateKeyToMnemonic(privateKey);
-  }
-
-  static async mnemonicToPrivateKey(mnemonic) {
-    const container = await containerPromise;
-    return await container.eth.utils.mnemonicToPrivateKey(compressMnemonic(mnemonic));
-  }
-
-  static async savePrivateKey(privateKey) {
-    const container = await containerPromise;
-    return await container.eth.utils.savePrivateKey(privateKey);
-  }
-
-  static async verifyMnemonic(mnemonic) {
-    const container = await containerPromise;
-    return container.eth.utils.mnemonicValid(compressMnemonic(mnemonic));
-  }
-
-  static async removePrivateKey(address) {
-    const container = await containerPromise;
-    return container.eth.utils.deletePrivateKey(address);
-  }
 }
