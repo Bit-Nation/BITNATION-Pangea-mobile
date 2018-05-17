4504a5df1a01da8045a99639dd588afe1f3dade9
swmd <frontdev8@gmail.com>
2018-05-17 03:49:14 +0200

    update message id format
    Former-commit-id: db06919b1524f049e4ed7bd2d2d52c9e98a41415

--- src/services/database/schema/v3.js
+++ src/services/database/schema/v3.js
@@ -129,7 +129,7 @@ export const DHTValueSchema = {
  * @property {date} created_at
  */
 export type MessageJobType = {
-  id: string,
+  id: number,
   heading: string,
   params: string,
   msg: string,
