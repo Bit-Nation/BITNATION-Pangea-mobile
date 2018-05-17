43253d8561c58652a596a44d4ceab2a7572eb06e
swmd <frontdev8@gmail.com>
2018-05-17 11:56:24 +0200

    remove unit test of activity sagas

--- src/sagas/activity/index.js
+++ src/sagas/activity/index.js
@@ -5,18 +5,14 @@ import { all, takeEvery } from 'redux-saga/effects';
 import { ADD_NEW_MESSAGE, START_FETCH_MESSAGES } from '../../actions/activity';
 import { addNewMessageSaga, fetchMessagesSaga, watchNewMessages } from './sagas';
 
-export function* watchStartFetchMessages() {
-  yield takeEvery(START_FETCH_MESSAGES, fetchMessagesSaga);
-}
-
-export function* watchAddNewMessage() {
-  yield takeEvery(ADD_NEW_MESSAGE, addNewMessageSaga);
-}
-
+/**
+ * @desc Root activity saga.
+ * @return {void}
+ */
 export default function* rootSaga() {
   yield all([
-    watchStartFetchMessages(),
     watchNewMessages(),
-    watchAddNewMessage(),
+    takeEvery(START_FETCH_MESSAGES, fetchMessagesSaga),
+    takeEvery(ADD_NEW_MESSAGE, addNewMessageSaga)
   ]);
 }
