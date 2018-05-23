a778e185d269baaa7f36c7cdd9cc733982adc6ed
William <william2958@gmail.com>
2018-05-23 14:48:49 -0400

    Added custom sendTransaction function
    Former-commit-id: 805ccb57ac40dac7b2d47fceeb6705dd7015d20a

--- __tests__/src/services/ethereum/factory.js
+++ __tests__/src/services/ethereum/factory.js
@@ -16,21 +16,17 @@ describe('factory', () => {
   test('Full send money transaction process', async () => {
     const ethereum = await factory({ private_key: '0xefc27ba5330258fcfb75e28e4e6efd88458751086998bbfad99257035fb3e160', provider_type: 'rinkeby' });
     const ethereumService = ethereum.service;
-    const wallet = ethereum.wallet;
     let gasEstimate;
     let result;
     try {
       gasEstimate = await ethereumService.estimateGas('2000000000', '0x');
-      // const transaction = await ethereumService.sendMoney('0x88a5C2d9919e46F883EB62F7b8Dd9d0CC45bc290', '0.00001', '2000000000');
-      await ethereumService.signRandom();
-      console.log('signed');
-      console.log('HELLO?: ', wallet.blah);
-      // const transactionHash = transaction.hash;
-      // result = await ethereumService.trackTransaction(transactionHash);
+      const transaction = await ethereumService.sendMoney('0xF0D346A86A68086846363185d24D5893F4353A78', '0.00001', '2000000000');
+      const transactionHash = transaction.hash;
+      result = await ethereumService.trackTransaction(transactionHash);
     } catch (e) {
       console.log('error: ', e);
     }
-    // expect(gasEstimate).toEqual(42000000000000);
-    // expect(result.from).toEqual('0xF0D346A86A68086846363185d24D5893F4353A78');
+    expect(gasEstimate).toEqual(42000000000000);
+    expect(result.from).toEqual('0xF0D346A86A68086846363185d24D5893F4353A78');
   }, 60000);
 });
