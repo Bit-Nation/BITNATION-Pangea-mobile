a42cec1fb6d4d623ca894a429e611744e8313383
Hoang Hung <hth321@gmail.com>
2018-05-28 16:54:49 +0700

    Fix eslint
    Former-commit-id: 8f5859c83254cb6355d63b21959dc4b34164752e

--- src/global/colors.js
+++ src/global/colors.js
@@ -10,11 +10,11 @@ const BitnationLightColor = '#C0C0C0'; /* rgb(114, 164, 222) */
 const BitnationVeryLightColor = '#FFFFFF'; /* rgb(188, 220, 255) */
 const BitnationBackgroundColor = '#3e9eff'; /* rgb(62, 158, 255) */
 
-const BitnationLightGrayColor = '#BBBDBF';
-const BitnationGrayColor = '#929497';
+// const BitnationLightGrayColor = '#BBBDBF';
+// const BitnationGrayColor = '#929497';
 const BitnationDarkGrayColor = '#58595B';
 const BitnationLinkOrangeColor = '#FF8B00';
-const BitnationHighlightYellowColor = '#FFCE00';
+// const BitnationHighlightYellowColor = '#FFCE00';
 const BitnationWhiteAlphaColor = 'rgba(255,255,255,0.45)';
 
 // Highlight Color
