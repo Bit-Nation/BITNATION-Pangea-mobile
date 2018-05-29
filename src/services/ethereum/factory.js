79c2801572e989fa9f20325d3d6def50446bb17b
William <william2958@gmail.com>
2018-05-29 11:41:46 -0400

    send money successful
    Former-commit-id: f722351f3b943c3a6351c38fc0395897363838d7 [formerly ab41c3fcb23377b33e5a9a2f3de9838db53bec89]
    Former-commit-id: 6ab205586608a3b3db4e3f283bf1899f0d6fd770

--- src/services/ethereum/factory.js
+++ src/services/ethereum/factory.js
@@ -9,6 +9,10 @@ import CustomSigner from './CustomSigner';
  * @param {Array<any>} config Array with: private_key Private Key base for the services, provider_type Type of Network
  * @returns {{wallet: ethers.Wallet, service: EthereumService}} The service
  */
+
+// Mock private key: 0xefc27ba5330258fcfb75e28e4e6efd88458751086998bbfad99257035fb3e160
+// Public key: 0xF0D346A86A68086846363185d24D5893F4353A78
+
 export default function factory(config: {private_key: string, provider_type: string}) {
   const privateKey: string = config.private_key;
   const providerType: string = config.provider_type;
