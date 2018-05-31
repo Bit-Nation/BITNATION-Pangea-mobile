f7e12b152d1981c6db2d9e744e59753b53beb767
Alberto Estarrona <alberto@estarrona.me>
2018-05-31 17:57:27 -0500

    [tests] Updated tests and Snapshots
    Former-commit-id: f9bda06ff195a50e8200e8a96755cb84dc0cf58c

--- __tests__/src/screens/ConfirmationContainer/ConfirmationScreen/index.js
+++ __tests__/src/screens/ConfirmationContainer/ConfirmationScreen/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import ConfirmationScreen from '../../../../../src/screens/ConfirmationContainer/ConfirmationScreen';
+import navigatorMock from '../../../../../__mocks__/Navigator';
 
 describe('ConfirmationScreen', () => {
   let confirmationScreen;
   const propsMock = {
     gasPrice: 2,
+    navigator: navigatorMock,
   };
 
   beforeEach(() => {
