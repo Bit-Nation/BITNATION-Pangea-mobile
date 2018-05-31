586ef441d107129fa974dc10e9b1dda67087f3f4
William <william2958@gmail.com>
2018-05-31 11:53:40 -0400

    Added changes

--- __tests__/src/services/ethereum/index.js
+++ __tests__/src/services/ethereum/index.js
@@ -17,12 +17,12 @@ describe('ethereum', () => {
     const wallet = new ethers.Wallet('0xefc27ba5330258fcfb75e28e4e6efd88458751086998bbfad99257035fb3e160');
     wallet.provider = new providers.InfuraProvider('rinkeby');
     ethereum = new Ethereum(wallet);
+    let gasEstimate;
     let response;
     try {
+      gasEstimate = await ethereum.estimateGas('2000000000', '0x0');
       response = await ethereum.sendMoney('0x88a5C2d9919e46F883EB62F7b8Dd9d0CC45bc290', '0.00001', '2000000000');
-      // console.log('RESPONSE: ', response);
     } catch (e) {
-      console.log('error: ', e.responseText);
       throw (e);
     }
     expect(response.from).toEqual('0xF0D346A86A68086846363185d24D5893F4353A78');
@@ -32,20 +32,7 @@ describe('ethereum', () => {
     wallet.provider = new providers.InfuraProvider('rinkeby');
     ethereum = new Ethereum(wallet);
     let balance;
-    try {
-      balance = await ethereum.getBalance();
-      console.log('ETH balance: ', balance);
-      // console.log('RESPONSE: ', response);
-    } catch (e) {
-      console.log('error: ', e.responseText);
-      throw (e);
-    }
-    try {
-      balance = await ethereum.getTokenBalance('0x9EDCb9A9c4d34b5d6A082c86cb4f117A1394F831');
-      console.log('token balance: ', balance);
-    } catch (e) {
-      console.log('error: ', e.responseText);
-      throw (e);
-    }
+    balance = await ethereum.getBalance();
+    balance = await ethereum.getTokenBalance('0x9EDCb9A9c4d34b5d6A082c86cb4f117A1394F831');
   });
 });
