d765ffff953b28152748a3f5f6ae72ff9f5508ec
Alberto Estarrona <alberto@estarrona.me>
2018-05-31 17:26:01 -0500

    [fix] Restore `prettyBalance`function
    Former-commit-id: 3b6f25d72bf10ae94e04c8637b328f4e222f04b8 [formerly 253b50e2ebd62c5e2d8eb2b1ba879bfe252a32ee]
    Former-commit-id: 0cc8e8fee6755647c5c054dd6faf6c3a16adc609

--- src/utils/formatters.js
+++ src/utils/formatters.js
@@ -3,7 +3,7 @@
 import { BigNumber } from 'bignumber.js';
 
 import i18n from '../global/i18n';
-import type { WalletType } from '../types/Wallet';
+import type { WalletType, CurrencyType } from '../types/Wallet';
 
 /**
  * @desc Round to 5 digits
@@ -19,12 +19,19 @@ export function roundEth(eth: string): string {
 /**
  * @desc Provide string for wallet balance even if it is undefined
  * @param {object} wallet Wallet object
+ * @param {object} currency Current symbol after amount, default is "ETH"
+ * @param {object} successfulSuffix Suffix to add if balance is present
  * @return {string} Formatted balance + successful suffix if balance defined,
  * corresponding string if not defined.
  */
-export function prettyWalletBalance(wallet: WalletType) {
+export function prettyWalletBalance(
+  wallet: WalletType,
+  currency: ?CurrencyType,
+  successfulSuffix: ?string,
+) {
+  const suffix = successfulSuffix || '';
   if (wallet.balance !== null) {
-    return `${roundEth(wallet.balance)} ${wallet.currency}`;
+    return `${roundEth(wallet.balance)} ${currency || ''}${suffix}`;
   }
   return !wallet.synchronizationError ? i18n.t('common.updating') : i18n.t('common.updateFailed');
 }
