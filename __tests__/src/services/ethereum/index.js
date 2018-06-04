7027a9137f9cf4f8adc6fe9b770f433fffc5d4b1
William <william2958@gmail.com>
2018-06-04 15:39:27 -0400

    Changed constructor
    Former-commit-id: 6e843f14c4064c91c051577994c77969abeb4cae

--- __tests__/src/services/ethereum/index.js
+++ __tests__/src/services/ethereum/index.js
@@ -6,19 +6,10 @@ import Ethereum from '../../../../src/services/ethereum/index';
 
 describe('ethereum', () => {
   let ethereum;
-  test('Check send function', async () => {
-    const wallet = {
-      sendTransaction: transactionObject => (transactionObject),
-      estimateGas: () => ({}),
-    };
-    ethereum = new Ethereum(wallet);
-    const result = await ethereum.sendMoney('0x88a5C2d9919e46F883EB62F7b8Dd9d0CC45bc290', '1.0', '2000000000');
-    expect(result.to).toEqual('0x88a5C2d9919e46F883EB62F7b8Dd9d0CC45bc290');
-  });
   test('Initialize wallet and send ether', async () => {
     const wallet = new ethers.Wallet('0xefc27ba5330258fcfb75e28e4e6efd88458751086998bbfad99257035fb3e160');
     wallet.provider = new providers.InfuraProvider('rinkeby');
-    ethereum = new Ethereum(wallet);
+    ethereum = new Ethereum(wallet, 'dev');
     let gasEstimate;
     let response;
     try {
@@ -32,7 +23,7 @@ describe('ethereum', () => {
   test('Check balance and token balance', async () => {
     const wallet = new ethers.Wallet('0xefc27ba5330258fcfb75e28e4e6efd88458751086998bbfad99257035fb3e160');
     wallet.provider = new providers.InfuraProvider('rinkeby');
-    ethereum = new Ethereum(wallet);
+    ethereum = new Ethereum(wallet, 'dev');
     let balance;
     balance = await ethereum.getBalance();
     balance = await ethereum.getTokenBalance('0x9EDCb9A9c4d34b5d6A082c86cb4f117A1394F831');
@@ -40,8 +31,8 @@ describe('ethereum', () => {
   test('Check nations object', async () => {
     const wallet = new ethers.Wallet('0xefc27ba5330258fcfb75e28e4e6efd88458751086998bbfad99257035fb3e160');
     wallet.provider = new providers.InfuraProvider('rinkeby');
-    ethereum = new Ethereum(wallet);
-    const nationsObject = await ethereum.initNations('dev');
+    ethereum = new Ethereum(wallet, 'dev');
+    const nationsObject = ethereum.nations;
     const numCitizens = await nationsObject.getNumCitizens(0);
     expect(numCitizens.toNumber()).toEqual(0);
   });
