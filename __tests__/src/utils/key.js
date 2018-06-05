9674aeb3b6dc17960ebf81d5c276bb28ae620f3d
Andrii Selivanov <seland2008@gmail.com>
2018-06-05 10:16:57 +0300

    [tests] Service container, key utils.

--- __tests__/src/utils/key.js
+++ __tests__/src/utils/key.js
@@ -1,4 +1,4 @@
-import { compressMnemonic, decompressMnemonic } from '../../../src/utils/key';
+import { compressMnemonic, decompressMnemonic, normalizeEthPrivateKey } from '../../../src/utils/key';
 
 test('compressMnemonic', () => {
   expect(compressMnemonic([
@@ -11,4 +11,11 @@ test('decompressMnemonic', () => {
     .toEqual([
       'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'ij', 'k', 'l', 'mn', 'o', 'pqr', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'one', 'more',
     ]);
+});
+
+test('normalizeEthPrivateKey', () => {
+  expect(normalizeEthPrivateKey('0xefc27ba5330258fcfb75e28e4e6efd88458751086998bbfad99257035fb3e160'))
+    .toEqual('0xefc27ba5330258fcfb75e28e4e6efd88458751086998bbfad99257035fb3e160');
+  expect(normalizeEthPrivateKey('efc27ba5330258fcfb75e28e4e6efd88458751086998bbfad99257035fb3e160'))
+    .toEqual('0xefc27ba5330258fcfb75e28e4e6efd88458751086998bbfad99257035fb3e160');
 });
