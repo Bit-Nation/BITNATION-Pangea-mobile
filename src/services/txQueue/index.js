c7ed9bad8d497d315a4f467ec87e739c5453abbd
Andrii Selivanov <seland2008@gmail.com>
2018-06-05 19:52:55 +0300

    [project] Added transaction job factory and constants.
    Former-commit-id: a35df6d02c759f8a3fffc6c057473aafedabb6c8

---
+++ src/services/txQueue/index.js
@@ -0,0 +1,32 @@
+// @flow
+
+import { TX_JOB_STATUS, TX_JOB_TYPE } from '../../global/Constants';
+import { InvalidTxHash, InvalidTxType } from '../../global/errors/txQueue';
+import type { TransactionJobType } from '../database/schemata';
+
+/**
+ * @desc Factory to create a transaction job object.
+ * @param {string} txHash Transaction hash.
+ * @param {string} type Transaction type, types list are under TX_JOB_TYPE constant.
+ * @return {Promise<TransactionJobType>} Built job
+ */
+export async function jobFactory(txHash: string, type: string): Promise<TransactionJobType> {
+  const allowedTypes = Object.keys(TX_JOB_TYPE);
+
+  if (allowedTypes.includes(type) === false) {
+    throw new InvalidTxType(type);
+  }
+
+  if (!/^0x([A-Fa-f0-9]{64})$/.exec(txHash)) {
+    throw new InvalidTxHash(txHash);
+  }
+
+  const job: TransactionJobType = {
+    txHash,
+    status: TX_JOB_STATUS.UNKNOWN,
+    type,
+    nation: null,
+  };
+
+  return job;
+}
