f99dc4531c6ba1e52babbe92d75e9f9235036f06
Andrii Selivanov <seland2008@gmail.com>
2018-06-05 19:53:27 +0300

    [nations] Implement part of service.
    Former-commit-id: f71af59acddb84e41508d338fd683cec0d667d10 [formerly 05a42b4ff6fa19449f24b97f199bc0ffded3ba8a]
    Former-commit-id: df6e1726e17dd048816306e30768d9047ae69c32

---
+++ src/services/nations/index.js
@@ -0,0 +1,172 @@
+// @flow
+
+import Realm from 'realm';
+
+import EthereumService from '../ethereum';
+import type { NationType, EditingNationType, DBNationType, NationIdType } from '../../types/Nation';
+import { convertDraftToDatabase, convertNationToBlockchain } from '../../utils/nations';
+import { NationAlreadySubmitted, SaveDraftFailed, StateMutateNotPossible } from '../../global/errors/nations';
+import { DatabaseWriteFailed } from '../../global/errors/common';
+import { jobFactory } from '../txQueue';
+import { TX_JOB_STATUS, TX_JOB_TYPE } from '../../global/Constants';
+
+export default class NationsService {
+  constructor(ethereumService: EthereumService, dbPromise: Promise<Realm>) {
+    this.ethereumService = ethereumService;
+    this.dbPromise = dbPromise;
+  }
+
+  ethereumService: EthereumService;
+  dbPromise: Promise<Realm>;
+
+  // Drafts operations
+
+  async updateDraft(nationId: number, nationData: EditingNationType): Promise<void> {
+    const db = await this.dbPromise;
+    const oldNation = await this.nationById(nationId);
+
+    // idInSmartContract >= 0 only when the nation was written to the blockchain
+    // @todo we really really need to check for if the nation was already submitted. But we need to have the tx queue thing.
+    if (oldNation.idInSmartContract >= 0) {
+      throw new NationAlreadySubmitted();
+    }
+
+    try {
+      db.write(() => {
+        db.create('Nation', convertDraftToDatabase(nationData, nationId), true);
+      });
+    } catch (error) {
+      throw new DatabaseWriteFailed();
+    }
+  }
+
+  async saveDraft(nationData: EditingNationType): Promise<DBNationType> {
+    const db = await this.dbPromise;
+    try {
+      let nation: DBNationType;
+      db.write(() => {
+        const sorted = db.objects('Nation').sorted('id', true);
+        const newId = (sorted.length === 0 ? 1 : sorted[0].id + 1);
+        nation = db.create('Nation', convertDraftToDatabase(nationData, newId));
+      });
+
+      return (nation: any);
+    } catch (_) {
+      throw new SaveDraftFailed();
+    }
+  }
+
+  async submitDraft(nationId: number): Promise<void> {
+    const db = await this.dbPromise;
+    const nation = await this.nationById(nationId);
+
+    // idInSmartContract >= 0 only when the nation was written to the blockchain
+    // @todo we really really need to check for if the nation was already submitted. But we need to have the tx queue thing.
+    if (nation.idInSmartContract >= 0) {
+      throw new NationAlreadySubmitted();
+    }
+
+    if (this.canMutateNationState(nation) === false) {
+      throw new StateMutateNotPossible();
+    }
+
+    const nationData = convertNationToBlockchain(nation);
+    const txHash = await this.ethereumService.nations.createNation(JSON.stringify(nationData));
+
+    const txJob = await jobFactory(txHash, TX_JOB_TYPE.NATION_CREATE);
+    try {
+      db.write(() => {
+        nation.tx = txJob;
+        nation.stateMutateAllowed = false;
+      });
+    } catch (_) {
+      throw new DatabaseWriteFailed();
+    }
+  }
+
+  async saveAndSubmit(nationData: EditingNationType): Promise<void> {
+    const draft = await this.saveDraft(nationData);
+    return this.submitDraft(draft.id);
+  }
+
+  async deleteDraft(nationId: number): Promise<void> {
+    const db = await this.dbPromise;
+    const nation = await this.nationById(nationId);
+
+    if (nation.idInSmartContract >= 0) {
+      throw new NationAlreadySubmitted();
+    }
+
+    try {
+      db.write(db => db.delete(nation));
+    } catch (_) {
+      throw new DatabaseWriteFailed();
+    }
+  }
+
+  // Join/leave
+
+  async joinNation(nationData: NationType): Promise<void> {
+    const db = await this.dbPromise;
+    const nation = await this.nationById(nationData.id);
+
+    if (this.canMutateNationState(nation) === false) {
+      throw new StateMutateNotPossible();
+    }
+
+    const txHash = this.ethereumService.nations.joinNation(nation.idInSmartContract);
+    const txJob = await jobFactory(txHash, TX_JOB_TYPE.NATION_JOIN);
+    try {
+      db.write(() => {
+        nation.tx = txJob;
+        nation.stateMutateAllowed = false;
+      });
+    } catch (_) {
+      throw new DatabaseWriteFailed();
+    }
+  }
+
+  async leaveNation(nationData: NationType): Promise<void> {
+    const db = await this.dbPromise;
+    const nation = await this.nationById(nationData.id);
+
+    if (this.canMutateNationState(nation) === false) {
+      throw new StateMutateNotPossible();
+    }
+
+    const txHash = this.ethereumService.nations.leaveNation(nation.idInSmartContract);
+    const txJob = await jobFactory(txHash, TX_JOB_TYPE.NATION_LEAVE);
+    try {
+      db.write(() => {
+        nation.tx = txJob;
+        nation.stateMutateAllowed = false;
+      });
+    } catch (_) {
+      throw new DatabaseWriteFailed();
+    }
+  }
+
+  // Utilities
+
+  async nationById(id: NationIdType): Promise<DBNationType> {
+    const db = await this.dbPromise;
+    const nations = await db.query(realm => realm.objects('Nation').filtered(`id = "${id}"`));
+    if (nations.length === 0) {
+      throw new Error('system_error.nation.does_not_exist');
+    }
+
+    return nations[0];
+  }
+
+  /**
+   * @desc Check if the blockchain state of the nation can be mutated
+   * @param {DBNationType | NationType} nation Nation to check
+   * @returns {boolean} Return true if state can be mutated
+   */
+  canMutateNationState = (nation: DBNationType | NationType): boolean => {
+    if (nation.tx == null) {
+      return true;
+    }
+    return nation.tx.status === TX_JOB_STATUS.SUCCESS;
+  };
+}
