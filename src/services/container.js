05a42b4ff6fa19449f24b97f199bc0ffded3ba8a
Andrii Selivanov <seland2008@gmail.com>
2018-06-05 19:53:27 +0300

    [nations] Implement part of service.

--- src/services/container.js
+++ src/services/container.js
@@ -3,14 +3,17 @@
 import EthereumServiceFactory from './ethereum/factory';
 import EthereumService from './ethereum';
 import WalletService from './wallet';
+import NationsService from './nations';
 import type { Account } from '../types/Account';
 import { normalizeEthPrivateKey } from '../utils/key';
+import defaultDB from './database';
 
 export default class ServiceContainer {
   static instance: ServiceContainer = new ServiceContainer();
 
   ethereumService: EthereumService | null = null;
   walletService: WalletService | null = null;
+  nationsService: NationsService | null = null;
 
   initServices(account: Account, ethPrivateKey: string) {
     this.ethereumService = EthereumServiceFactory({
@@ -18,10 +21,12 @@ export default class ServiceContainer {
       providerType: account.networkType === 'dev' ? 'rinkeby' : 'homestead',
     }).service;
     this.walletService = new WalletService(this.ethereumService);
+    this.nationsService = new NationsService(this.ethereumService, defaultDB);
   }
 
   destroyServices() {
     this.ethereumService = null;
     this.walletService = null;
+    this.nationsService = null;
   }
 }
