bcb395ade472be82e24f06c042915a00103516f4
Andrii Selivanov <seland2008@gmail.com>
2018-06-06 23:10:53 +0300

    [project] Nations saga and inProgress flag handling.
    Former-commit-id: ef3a56de0289c90085e44293fba8030edebaed1d

--- src/sagas/nations/index.js
+++ src/sagas/nations/index.js
@@ -4,6 +4,7 @@ import { all, takeEvery, call } from 'redux-saga/effects';
 
 import { REQUEST_JOIN_NATION, REQUEST_LEAVE_NATION } from '../../actions/nations';
 import { joinNation, leaveNation, startDatabaseListening, startNationIndexingWorker } from './sagas';
+import { SERVICES_CREATED } from '../../actions/serviceContainer';
 
 /**
  * @desc Root modify nation saga.
@@ -13,7 +14,7 @@ export default function* rootSaga(): Generator<*, *, *> {
   yield all([
     yield takeEvery(REQUEST_JOIN_NATION, joinNation),
     yield takeEvery(REQUEST_LEAVE_NATION, leaveNation),
-    yield call(startNationIndexingWorker),
+    yield takeEvery(SERVICES_CREATED, startNationIndexingWorker),
     yield call(startDatabaseListening),
   ]);
 }
