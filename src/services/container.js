ab9c68f864bfcbea818d9d2d2cf851f58411695d
Andrii Selivanov <seland2008@gmail.com>
2018-06-06 22:55:55 +0300

    [project] Implement index method and small adjustments.
    Former-commit-id: bda605ba0818915100f41ffcb0108387b689a240 [formerly 6e9a0d7f7e8a0fb351930d284a9e09038d65b7f4]
    Former-commit-id: 0f3c043521cabe5118bdf152c37c67ac02019703

--- src/services/container.js
+++ src/services/container.js
@@ -17,16 +17,19 @@ export default class ServiceContainer {
 
   initServices(account: Account, ethPrivateKey: string) {
     this.ethereumService = EthereumServiceFactory({
-      privateKey: normalizeEthPrivateKey(ethPrivateKey),
+      privateKey: normalizeEthPrivateKey('0x14397c5443a68bf84eda0a893f05bf02534b3b73d49f12bc657f8c291bc69b28'),
       providerType: account.networkType === 'dev' ? 'rinkeby' : 'homestead',
     }).service;
     this.walletService = new WalletService(this.ethereumService);
-    this.nationsService = new NationsService(this.ethereumService, defaultDB);
+    this.nationsService = new NationsService(this.ethereumService, defaultDB, account.id);
   }
 
   destroyServices() {
     this.ethereumService = null;
     this.walletService = null;
+    if (this.nationsService !== null) {
+      this.nationsService.cleanUp();
+    }
     this.nationsService = null;
   }
 }
