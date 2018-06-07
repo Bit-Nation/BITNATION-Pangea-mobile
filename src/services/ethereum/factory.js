cd9138b7c067fc1c9f339c9cc2f28edca736ea45
Andrii Selivanov <seland2008@gmail.com>
2018-06-07 17:11:29 +0300

    [project] Update ethereum service to use NetworkType type.

--- src/services/ethereum/factory.js
+++ src/services/ethereum/factory.js
@@ -2,21 +2,22 @@
 
 import EthereumService from './index';
 import CustomSigner from './CustomSigner';
+import type { NetworkType } from '../../types/Account';
 
 /**
  * @desc Factory for the Ethereum service based on EthJS.
  * @param {Object<any>} config Array with: privateKey Private Key base for the services, providerType Type of Network
  * @returns {{wallet: ethers.Wallet, service: EthereumService}} The service
  */
-export default function factory(config: {privateKey: string, providerType: string}) {
+export default function factory(config: {privateKey: string, networkType: NetworkType}) {
   const { privateKey } = config;
-  const { providerType } = config;
+  const { networkType } = config;
 
   // @todo check if valid private key - exit if not
-  const customSigner = new CustomSigner(privateKey, providerType);
+  const customSigner = new CustomSigner(privateKey, networkType === 'dev' ? 'rinkeby' : 'homestead');
 
   // Ethereum service
-  const ethereumService = new EthereumService(customSigner, providerType === 'rinkeby' ? 'dev' : 'prod');
+  const ethereumService = new EthereumService(customSigner, networkType);
 
   return {
     wallet: customSigner,
