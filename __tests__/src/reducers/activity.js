6b4f83b8ce0f9c148b4e5e97d3f555d2bbedf16d
Andrii Selivanov <seland2008@gmail.com>
2018-06-07 19:53:03 +0300

    [tests] Update.
    Former-commit-id: 9d250531be8e5aec54c3d9a7725ae11b9c4d83ab [formerly 0768495f1bf2fd0fe00f610a14c875adc2d4163f]
    Former-commit-id: c4516c445df51a452ffb18cbb6524a85f1c35391

--- __tests__/src/reducers/activity.js
+++ __tests__/src/reducers/activity.js
@@ -1,45 +1,13 @@
 import reducer, {
   initialState,
-  mergeMessages,
 } from '../../../src/reducers/activity';
 import { messagesUpdated } from '../../../src/actions/activity';
 
-const message0 = {
+const message = {
   id: 0,
   msg: 'Test message 0',
   interpret: true,
 };
-const message1 = {
-  id: 1,
-  msg: 'Test message 1',
-  interpret: false,
-};
-const message2 = {
-  id: 2,
-  msg: 'Test message 2',
-  interpret: true,
-};
-
-describe('mergeMessages', () => {
-  test('full merging', () => {
-    expect(mergeMessages([message0, message1], [message2], 3)).toEqual([
-      message2,
-      message1,
-      message0,
-    ]);
-    expect(mergeMessages([message2, message1], [message1, message0, message2], 10)).toEqual([message2, message1, message0]);
-    expect(mergeMessages([message2, message0], [message2, message1], 5)).toEqual([message2, message1, message0]);
-  });
-
-  test('merging with limit', () => {
-    expect(mergeMessages([message0, message1], [message2], 2)).toEqual([
-      message2,
-      message1,
-    ]);
-    expect(mergeMessages([message1, message0], [message2, message0], 1)).toEqual([message2]);
-    expect(mergeMessages([message2, message0, message1], [message2, message0], 2)).toEqual([message2, message1]);
-  });
-});
 
 describe('activity reducer action handling', () => {
   test('default returns the same state', () => {
@@ -48,10 +16,10 @@ describe('activity reducer action handling', () => {
 
   test('messagesUpdated after initial state', () => {
     const stateBefore = initialState;
-    const stateAfter = reducer(stateBefore, messagesUpdated(message0));
+    const stateAfter = reducer(stateBefore, messagesUpdated([message]));
     expect(stateAfter).toEqual({
       ...stateBefore,
-      messages: [message0],
+      messages: [message],
     });
   });
 });
