2b2d7726c25f12c966e5682a89db28389fd7da6f
William <william2958@gmail.com>
2018-06-19 10:42:53 -0400

    Changed confirmation screen
    Former-commit-id: cf996616aeffacdf728533917be8bf4c82cabb53

--- src/screens/ConfirmationContainer/ConfirmationScreen/styles.js
+++ src/screens/ConfirmationContainer/ConfirmationScreen/styles.js
@@ -3,7 +3,11 @@ import GlobalStyles from '../../../global/Styles';
 
 const styles = MediaQueryStyleSheet.create({
   ...GlobalStyles,
-
+  bodyParagraph: {
+    borderBottomWidth: 1,
+    borderBottomColor: 'rgba(0,0,0,0.12)',
+    justifyContent: 'space-between',
+  },
 });
 
 export default styles;
