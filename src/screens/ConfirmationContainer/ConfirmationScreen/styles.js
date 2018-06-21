f06b837ff974bd018004b49c0059da78004af150
William <william2958@gmail.com>
2018-06-21 15:59:08 -0400

    [fix] Added slider changes to confirmation screen
    Former-commit-id: 634b04699636fac40843b090f05a5af182a9f4e9

--- src/screens/ConfirmationContainer/ConfirmationScreen/styles.js
+++ src/screens/ConfirmationContainer/ConfirmationScreen/styles.js
@@ -3,6 +3,17 @@ import GlobalStyles from '../../../global/Styles';
 
 const styles = MediaQueryStyleSheet.create({
   ...GlobalStyles,
+  textCon: {
+    width: 320,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  colorGrey: {
+    color: '#929497',
+  },
+  colorYellow: {
+    color: '#FF8B00',
+  },
 });
 
 export default styles;
