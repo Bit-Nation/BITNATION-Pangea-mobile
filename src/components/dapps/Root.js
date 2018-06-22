0bb52cba168a46fd3847efff6a2fdc5edc0abcf4
Andrii Selivanov <seland2008@gmail.com>
2018-06-23 02:25:06 +0300

    [refactoring] Extract renderer. Add docs.
    Former-commit-id: 33298e7dcce2b32c83336c9a2238a262f364d8af

--- src/components/dapps/Root.js
+++ src/components/dapps/Root.js
@@ -2,52 +2,7 @@ import React, { Component } from 'react';
 
 import { View as ReactNativeView } from 'react-native';
 
-import View from './View';
-import Text from './Text';
-
-export type ComponentsJSON = { type: string, props: Object, children: Array<Object> };
-
-export const getTypeElementFromText = (key) => {
-  switch (key) {
-    case 'Text':
-      return Text;
-    case 'View':
-      return View;
-    default:
-      return View;
-  }
-};
-
-export const validateProps = (props: Object, validPropNames: Array<string>, type: string) => {
-  const filteredProps = {};
-  Object.keys(props).forEach((propName) => {
-    if (validPropNames.includes(propName)) {
-      filteredProps[propName] = props[propName];
-    } else {
-      console.warn(`Prop ${propName} is not allowed on component ${type}`);
-    }
-  });
-
-  return filteredProps;
-};
-
-export const renderJSON = (json: ComponentsJSON, key: any, customPropsProvider: (type: string, ownProps: Object) => Object) => {
-  const { type, children } = json;
-  const component = getTypeElementFromText(type);
-  let { props } = json;
-
-  if (Array.isArray(component.validProps) && component.validProps !== undefined) {
-    props = validateProps(json.props, component.validProps, type);
-  }
-
-  return React.createElement(
-    component,
-    { ...props, ...customPropsProvider(type, props), key },
-    children
-      ? children.map((child, index) => renderJSON(child, index, customPropsProvider))
-      : null,
-  );
-};
+import { type ComponentsJSON, renderJSON } from '../../utils/dapps/renderer';
 
 export type Props = {
   /**
