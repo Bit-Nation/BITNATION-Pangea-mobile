a8ce7e768d2b947270044e6350d9387cd8d7e522
swmd <frontdev8@gmail.com>
2018-06-22 05:48:58 +0200

    add private chat screen
    Former-commit-id: 986fb882e1f0b26c831ff6c45452d68827e99c36 [formerly d96c52d8e55c2cc9b1237f74c2d5ad2af53b278d]
    Former-commit-id: a7ef3962d3011da6d200c06b4b45f8da1c9cf9a7

---
+++ src/screens/PrivateChat/Chat/styles.js
@@ -0,0 +1,42 @@
+import { MediaQueryStyleSheet } from 'react-native-responsive';
+import Colors from '../../../global/colors';
+
+const styles = MediaQueryStyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  composer: {
+    backgroundColor: Colors.shadeOfBitnationLightColor(0.2),
+    borderWidth: 1,
+    borderColor: Colors.borderColor,
+    color: Colors.BitnationDarkGrayColor,
+    paddingLeft: 4,
+    marginRight: 5,
+  },
+  inputToolbar: {
+    backgroundColor: 'transparent',
+    borderTopWidth: 0,
+    paddingRight: 5,
+  },
+  customTextStyle: {
+    fontFamily: 'Roboto',
+    fontWeight: 'normal',
+    color: Colors.BitnationDarkGrayColor,
+  },
+  leftBubbleWrapper: {
+    flex: 1,
+    backgroundColor: Colors.white,
+  },
+  rightBubbleWrapper: {
+    flex: 1,
+    backgroundColor: Colors.BitnationHighlightYellowColor,
+  },
+  leftTextStyle: {
+    color: Colors.BitnationDarkGrayColor,
+  },
+  rightTextStyle: {
+    color: Colors.BitnationDarkGrayColor,
+  },
+});
+
+export default styles;
