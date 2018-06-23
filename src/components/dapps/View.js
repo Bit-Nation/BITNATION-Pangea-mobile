bf828faaea6d666309e410268c62e28cc92bd041
Andrii Selivanov <seland2008@gmail.com>
2018-06-23 15:44:49 +0300

    [project] Implemented Text Input component. Added handling for set/get state properties.
    Former-commit-id: b2f47de0f2abe3dd10d5073dfad0fefb8434c09e [formerly b93fb62b7094fbd6d77855c2211af61f2ac2cdb2]
    Former-commit-id: 60f7085ebb8bdd64e6891dcf48d473ddd25e25bc

--- src/components/dapps/View.js
+++ src/components/dapps/View.js
@@ -3,15 +3,13 @@ import React, { Component } from 'react';
 import { View as ReactNativeView } from 'react-native';
 
 export default class View extends Component {
-  static validProps = [
+  static validNativeProps = [
     'style',
   ];
 
   render() {
     return (
-      <ReactNativeView style={this.props.style}>
-        {this.props.children}
-      </ReactNativeView>
+      <ReactNativeView {...this.props} />
     );
   }
 }
