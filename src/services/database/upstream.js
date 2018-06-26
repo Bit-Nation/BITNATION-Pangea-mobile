d7f8e33aec13f3584aa23c4898ee7b8f2e7e5163
William <william2958@gmail.com>
2018-06-25 20:01:23 -0400

    Added handling for each message type
    Former-commit-id: 4d52156cf7fbf1252c4d50de8b573d1abfa6eb0c

--- src/services/database/upstream.js
+++ src/services/database/upstream.js
@@ -1,5 +1,6 @@
 import { NativeEventEmitter, NativeModules } from 'react-native';
-import protobuf from 'protobufjs';
+// Javascript static code of the proto file
+import { Request } from './compiledReqeust';
 
 const { Panthalassa } = NativeModules;
 
@@ -13,11 +14,42 @@ export default class UpstreamService {
       'RequestEvent',
       (request) => this.handleRequest(request),
     );
-  }
-  handleRequest = request => {
+  };
+  handleRequest = (request) => {
+    const decoded = Request.decode(request);
+    if (decoded.DRKeyStoreGet !== null) {
+      this.handleDRKeyStoreGet(decoded.DRKeyStoreGet.drKey, decoded.DRKeyStoreGet.messageNumber);
+    } else if (decoded.DRKeyStorePut !== null) {
+      this.handleDRKeyStorePut(decoded.DRKeyStorePut.key, decoded.DRKeyStorePut.messageNumber, decoded.DRKeyStorePut.messageKey);
+    } else if (decoded.DRKeyStoreDeleteMK !== null) {
+      this.handleDRKeyStoreDeleteMK(decoded.DRKeyStoreDeleteMK.key, decoded.DRKeyStoreDeleteMK.msgNum);
+    } else if (decoded.DRKeyStoreDeleteKeys !== null) {
+      this.handleDRKeyStoreDeleteKeys(decoded.DRKeyStoreDeleteKeys.key);
+    } else if (decoded.DRKeyStoreDeleteKeys !== null) {
+      this.handleDRKeyStoreCount(decoded.DRKeyStoreDeleteKeys.key);
+    } else if (decoded.ShowModal !== null) {
+      this.handleShowModal(decoded.ShowModal.title, decoded.ShowModal.layout);
+    }
+  };
+  handleDRKeyStoreGet = (drKey, messageNumber) => {
 
-  }
+  };
+  handleDRKeyStorePut = (key, messageNumber, messageKey) => {
+
+  };
+  handleDRKeyStoreDeleteMK = (key, msgNum) => {
+
+  };
+  handleDRKeyStoreDeleteKeys = (key) => {
+
+  };
+  handleDRKeyStoreCount = (key) => {
+
+  };
+  handleShowModal = (title, layout) => {
+
+  };
   unsubscribe = () => {
     this.eventsSubscription.remove();
-  }
+  };
 }
