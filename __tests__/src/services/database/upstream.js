ea55ebb217fe8fa3c42f1d6efdc942d1a5892087
Hoang Hung <hth321@gmail.com>
2018-06-26 22:47:41 +0700

    update proto file, write test for request data

---
+++ __tests__/src/services/database/upstream.js
@@ -0,0 +1,55 @@
+import { api_proto } from '../../../../src/services/database/compiledRequest';
+
+const message = {
+  requestID: '1',
+
+  dRKeyStoreGet: {
+    drKey: 1,
+    messageNumber: 2,
+  },
+
+  dRKeyStorePut: {
+    key: 1,
+    messageNumber: 2,
+    messageKey: 3,
+  },
+
+  dRKeyStoreDeleteMK: {
+    key: 1,
+    msgNum: 2,
+  },
+
+  dRKeyStoreDeleteKeys: {
+    key: 1,
+  },
+
+  dRKeyStoreCount: {
+    key: 1,
+  },
+
+  dRKeyStoreAll: {},
+
+  showModal: {
+    title: '1',
+    layout: '2',
+  },
+  sendEthereumTransaction: {
+    value: 1,
+    to: 2,
+    data: 3,
+  },
+  saveDApp: {
+    appName: '1',
+    code: '2',
+    signature: '3',
+    signingPublicKey: 4,
+  },
+};
+
+describe('upstream', () => {
+  test('encode request', () => {
+    const { Request } = api_proto;
+    const buffer = Request.encode(message).finish();
+    expect(buffer).toBeDefined();
+  });
+});
