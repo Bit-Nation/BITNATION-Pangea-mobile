3e5d0158c3274077e3bbdf5bd5f38bd1454e8168
swmd <frontdev8@gmail.com>
2018-06-26 19:54:25 +0200

    modify chat schemas
    Former-commit-id: e5d1d9b6aa1cfc209ed7988f65d4ffa72a95ebd4

--- src/utils/key.js
+++ src/utils/key.js
@@ -34,3 +34,37 @@ export function normalizeEthPrivateKey(ethPrivateKey: string): string {
 
   return `0x${ethPrivateKey}`;
 }
+
+/**
+ * @desc Convert HEX string into Base64 string
+ * @param {string} hexstring hex string to be converted
+ * @return {string} Result Base64 string
+ */
+export function hexToBase64(hexstring: string): string {
+  return btoa(hexstring.match(/\w{2}/g).map(a => String.fromCharCode(parseInt(a, 16))).join(''));
+}
+
+/**
+ * @desc Convert Base64 string into Hex string
+ * @param {string} str Base64 string to be converted
+ * @return {string} Result Hex string
+ */
+export function base64ToHex(str: string): string {
+  for (let i = 0, bin = atob(str.replace(/[ \r\n]+$/, '')), hex = []; i < bin.length; i++) {
+    let tmp = bin.charCodeAt(i).toString(16);
+    if (tmp.length === 1) tmp = `0${tmp}`;
+    hex[hex.length] = tmp;
+  }
+  return hex.join('');
+}
+
+/**
+ * @desc Convert byte array into Hex string
+ * @param {byte[]} byteArray byte array to be converted
+ * @return {string} Result Hex string
+ */
+export function byteToHexString(byteArray: byte[]): string {
+  return Array.from(byteArray, (byte) => {
+    return ('0' + (byte & 0xFF).toString(16)).slice(-2);
+  }).join('');
+}
