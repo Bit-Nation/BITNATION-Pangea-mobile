0395899d82a7c8b4632ad54ce97fc6d53708916a
William <william2958@gmail.com>
2018-06-26 19:46:38 -0400

    Added handling for sending transaction
    Former-commit-id: 3e7a692ba611f7380850df0e42e427ef7c091964 [formerly 4765d84798e9d950cfd7d98cde8b73642a811003]
    Former-commit-id: f50f82c26a378e212b172a677aeeb152eee0a438

--- src/services/upstream/upstream.js
+++ src/services/upstream/upstream.js
@@ -1,13 +1,19 @@
+import ethers from 'ethers';
 import { NativeEventEmitter, NativeModules } from 'react-native';
 // Javascript static code of the proto file
 import { api_proto } from './compiledRequest';
+import compiledResponse from './compiledResponse';
+import EthereumService from '../ethereum';
 
 const { Panthalassa } = NativeModules;
+const { Response } = compiledResponse.api_proto;
 
 export default class UpstreamService {
   eventsSubscription;
   request;
-  constructor() {
+  ethereumService: EthereumService;
+  constructor(ethereumService: EthereumService) {
+    this.ethereumService = ethereumService;
     const emitter = new NativeEventEmitter(Panthalassa);
     this.eventsSubscription = emitter.addListener(
       'PanthalassaUpStream',
@@ -39,6 +45,8 @@ export default class UpstreamService {
       this.handleDRKeyStoreCount(decoded.dRKeyStoreDeleteKeys.key);
     } else if (decoded.ShowModal !== null) {
       this.handleShowModal(decoded.ShowModal.title, decoded.ShowModal.layout);
+    } else if (decoded.SendEthereumTransaction !== null) {
+      this.handleSendEthereumTransaction(decoded.requestId, decoded.SendEthereumTransaction.value, decoded.SendEthereumTransaction.to, decoded.SendEthereumTransaction.data);
     } else {
       this.handleErrorMessage();
     }
@@ -49,6 +57,43 @@ export default class UpstreamService {
   handleDRKeyStoreDeleteKeys = (key) => {};
   handleDRKeyStoreCount = (key) => {};
   handleShowModal = (title, layout) => {};
+  handleSendEthereumTransaction = (id, value, to, data) => {
+    const transaction = {
+      to,
+      data,
+      value: ethers.utils.parseEther(value),
+    };
+    this.ethereumService.wallet.sendTransaction(transaction)
+      .then((txDetails) => {
+        Panthalassa.PanthalassaSendResponse({
+          id,
+          data: Response.encode({
+            sendEthereumTransaction: {
+              nonce: txDetails.nonce,
+              gasPrice: txDetails.gasPrice.toString(),
+              gasLimit: txDetails.gasLimit.toString(),
+              value: txDetails.value.toString(),
+              to: txDetails.to,
+              data: txDetails.data,
+              chainId: txDetails.chainId,
+              from: txDetails.from,
+              hash: txDetails.hash,
+            },
+          }).finish(),
+          error: '',
+          timeout: 1,
+        });
+      })
+      .catch((err) => {
+        Panthalassa.PanthalassaSendResponse({
+          id,
+          data: {},
+          error: err,
+          timeout: 1,
+        });
+      });
+  };
+  handleErrorMessage = () => {};
   unsubscribe = () => {
     if (this.eventsSubscription) {
       // this.eventsSubscription.remove();
