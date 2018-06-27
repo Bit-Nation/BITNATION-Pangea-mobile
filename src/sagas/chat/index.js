127c8d069c3a5cc32625572c59312f7ce203c0f7
swmd <frontdev8@gmail.com>
2018-06-27 17:26:35 +0200

    add timer

--- src/sagas/chat/index.js
+++ src/sagas/chat/index.js
@@ -1,12 +1,14 @@
 import { all, call, takeEvery } from 'redux-saga/effects';
 
-import { SAVE_PROFILE, SAVE_PRE_KEY_BUNDLE, NEW_CHAT_SESSION, OPEN_CHAT_SESSION } from '../../actions/chat';
+import { SAVE_PROFILE, SAVE_PRE_KEY_BUNDLE, NEW_CHAT_SESSION, OPEN_CHAT_SESSION, FETCH_MESSAGES } from '../../actions/chat';
 import {
   startDatabaseListening,
   saveProfile,
   savePreKeyBundle,
   createChatSession,
   openChatSession,
+  listenMessages,
+  fetchMessages,
 } from './sagas';
 
 /**
@@ -16,9 +18,11 @@ import {
 export default function* rootSaga() {
   yield all([
     call(startDatabaseListening),
+    call(listenMessages),
     takeEvery(SAVE_PROFILE, saveProfile),
     takeEvery(SAVE_PRE_KEY_BUNDLE, savePreKeyBundle),
     takeEvery(NEW_CHAT_SESSION, createChatSession),
     takeEvery(OPEN_CHAT_SESSION, openChatSession),
+    takeEvery(FETCH_MESSAGES, fetchMessages),
   ]);
 }
