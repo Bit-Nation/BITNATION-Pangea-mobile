ec8f84354dcf683e6d1f854d530a27301ce95d11
Andrii Selivanov <seland2008@gmail.com>
2018-06-27 19:33:21 +0300

    [project] Add dapp qr code scanner to settings.
    Former-commit-id: c2c80156561487f6dd439a24193e3d6e7d503dee [formerly 88aa598d4016419e35c01a580ffefd59e9d3e9e0]
    Former-commit-id: 9a3e55749c9e8fd45b8923c4dd6c87fd87526c9c

--- src/screens/Settings/SettingsList/index.js
+++ src/screens/Settings/SettingsList/index.js
@@ -56,6 +56,9 @@ class SettingsListScreen extends NavigatorComponent<Props> {
       case 'confirmPrivateKey':
         this.props.navigator.push(screen('CONFIRM_KEY_INSTRUCTION_SCREEN'));
         break;
+      case 'connectToDAppHost':
+        this.props.navigator.push(screen('QR_CODE_DAPP_SCREEN'));
+        break;
       default:
         break;
     }
@@ -72,6 +75,7 @@ class SettingsListScreen extends NavigatorComponent<Props> {
       'identity',
       'security',
       currentAccount.confirmedMnemonic ? 'viewPrivateKey' : 'confirmPrivateKey',
+      'connectToDAppHost',
     ];
 
     return (
