281a95ffcaeb88d25f2fc313ec3d9903a36792c9
swmd <frontdev8@gmail.com>
2018-06-28 06:18:28 +0200

    add scheduler for fetching messages
    Former-commit-id: d0a1673cecdfd922ba0d157ae3619dbbeae77ca4

--- src/sagas/chat/sagas.js
+++ src/sagas/chat/sagas.js
@@ -1,7 +1,10 @@
 import { put, call, take, fork, cancel } from 'redux-saga/effects';
 import type { Realm } from 'realm';
 
-import { SaveProfileAction, SavePreKeyBundleAction, NewChatSessionAction, OpenChatAction, ChatListenAction, chatsUpdated, selectProfile, FETCH_MESSAGES } from '../../actions/chat';
+import {
+  SaveProfileAction, SavePreKeyBundleAction, NewChatSessionAction, OpenChatAction, chatsUpdated, selectProfile,
+  FETCH_MESSAGES, START_FETCH_MESSAGES, STOP_FETCH_MESSAGES,
+} from '../../actions/chat';
 import defaultDB from '../../services/database';
 import ChatService from '../../services/chat';
 import type { ChatSessionType as DBChatSession } from '../../services/database/schemata';
@@ -46,21 +49,24 @@ export function* startDatabaseListening(): Generator<*, *, *> {
 export function* saveProfile(action: SaveProfileAction) {
   const db = yield defaultDB;
   const { profile: { information } } = action;
-  const profile = {
-    name: information.name,
-    location: information.location,
-    image: information.image,
-    identity_pub_key: information.identity_pub_key,
-    ethereum_pub_Key: information.ethereum_pub_Key,
-    chat_id_key: information.chat_id_key,
-    timestamp: information.timestamp,
-    version: information.version,
-    identity_key_signature: information.identity_key_signature,
-    ethereum_key_signature: information.ethereum_key_signature,
-  };
-  db.write(() => {
-    db.create('Profile', profile);
-  });
+  const results = db.objects('Profile').filtered(`identity_pub_key == '${information.identity_pub_key}'`);
+  if (results.length === 0) {
+    const profile = {
+      name: information.name,
+      location: information.location,
+      image: information.image,
+      identity_pub_key: information.identity_pub_key,
+      ethereum_pub_Key: information.ethereum_pub_Key,
+      chat_id_key: information.chat_id_key,
+      timestamp: information.timestamp,
+      version: information.version,
+      identity_key_signature: information.identity_key_signature,
+      ethereum_key_signature: information.ethereum_key_signature,
+    };
+    db.write(() => {
+      db.create('Profile', profile);
+    });
+  }
 }
 
 /**
@@ -125,11 +131,13 @@ export function* openChatSession(action: OpenChatAction) {
   }
 }
 
+export const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+
 /**
  * @desc Fetch messages
  * @return {void}
  */
-function* tick() {
+export function* tick() {
   while (true) {
     yield call(delay, 8000);
     yield put({ type: FETCH_MESSAGES });
@@ -141,10 +149,17 @@ function* tick() {
  * @param {ChatListenAction} action LISTEN_CHAT action
  * @return {void}
  */
-export function* listenMessages(action: ChatListenAction) {
-  const bgSyncTask = yield fork(tick);
-  yield take('STOP_FETCH_MESSAGES');
-  yield cancel(bgSyncTask);
+export function* listenMessages() {
+  while (yield take(START_FETCH_MESSAGES)) {
+    // starts the task in the background
+    const bgSyncTask = yield fork(tick);
+
+    // wait for the user stop action
+    yield take(STOP_FETCH_MESSAGES);
+    // user clicked stop. cancel the background task
+    // this will throw a SagaCancellationException into task
+    yield cancel(bgSyncTask);
+  }
 }
 
 /**
