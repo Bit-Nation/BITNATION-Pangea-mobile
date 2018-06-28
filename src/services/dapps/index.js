7630876f783882566bd182d2e290586091714e77
Andrii Selivanov <seland2008@gmail.com>
2018-06-28 19:41:24 +0300

    [project] Update Panthalassa calls in DAppsService
    Former-commit-id: 0d9982923053ef84505f600edab042e8f983f0b8

--- src/services/dapps/index.js
+++ src/services/dapps/index.js
@@ -3,21 +3,32 @@
 import { NativeModules } from 'react-native';
 
 import type { DApp } from '../../types/DApp';
+import { convertToPanthalassa } from '../../utils/mapping/dapp';
 
 export default class DAppsService {
   static async startDApp(dApp: DApp): Promise<boolean> {
     const { Panthalassa } = NativeModules;
-    return Panthalassa.PanthalassaStartDApp(JSON.stringify(dApp));
+    return Panthalassa.PanthalassaStartDApp({
+      dApp: JSON.stringify(convertToPanthalassa(dApp)),
+    });
   }
 
-  static async openDApp(publicKey: string): Promise<boolean> {
+  static async openDApp(publicKey: string, context: Object): Promise<boolean> {
     const { Panthalassa } = NativeModules;
-    return Panthalassa.PanthalassaOpenDApp(publicKey);
+    return Panthalassa.PanthalassaOpenDApp({
+      id: publicKey,
+      context: JSON.stringify(context),
+    });
   }
 
   static async connectToDAppHost(address: string): Promise<boolean> {
     const { Panthalassa } = NativeModules;
     return Panthalassa.PanthalassaConnectToDAppDevHost({ address });
+  }
+
+  static async performDAppCallback(dappPublicKey: string, id: string, params: Object) {
+    const { Panthalassa } = NativeModules;
+    return Panthalassa.PanthalassaCallDAppFunction(dappPublicKey, id, params);
   }
 
   // async renderDAppMessage(publicKey: string, message: )
