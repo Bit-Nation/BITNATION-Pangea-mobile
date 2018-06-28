2581a75ea8c391209e5f905c1df9a3fda285f6d3
Andrii Selivanov <seland2008@gmail.com>
2018-06-29 02:32:47 +0300

    [project] Implement upstream handling.
    Former-commit-id: e58c0387c268444fd2081026db3ea808151519eb

--- src/services/upstream/upstream.js
+++ src/services/upstream/upstream.js
@@ -1,7 +1,9 @@
+// @flow
+
 import ethers from 'ethers';
 import { NativeEventEmitter, NativeModules } from 'react-native';
-import _ from 'lodash';
-import defaultDB from '../../services/database';
+import Realm from 'realm';
+import Navigation from 'react-native-navigation';
 
 import type { MessageKeyType } from '../../types/Message';
 
@@ -9,52 +11,58 @@ import type { MessageKeyType } from '../../types/Message';
 import { api_proto as apiProto } from './compiled';
 
 import EthereumService from '../ethereum';
-import { DatabaseWriteFailed } from '../../global/errors/common';
-
+import { errorAlert } from '../../global/alerts';
+import i18n from '../../global/i18n';
 
 const { Panthalassa } = NativeModules;
 const { Response, Request } = apiProto;
 
+const RESPONSE_TIMEOUT = 20;
+
 export default class UpstreamService {
-  eventsSubscription;
+  eventsSubscription: any;
   ethereumService: EthereumService;
-  constructor(ethereumService: EthereumService) {
+  dbPromise: Promise<Realm>;
+  currentAccountId: string;
+
+  constructor(ethereumService: EthereumService, dbPromise: Promise<Realm>, accountId: string) {
     this.ethereumService = ethereumService;
+    this.dbPromise = dbPromise;
+    this.currentAccountId = accountId;
   }
 
-  startSubscribe=() => {
+  startSubscribe = () => {
     const emitter = new NativeEventEmitter(Panthalassa);
     this.eventsSubscription = emitter.addListener(
       'PanthalassaUpStream',
       request => this.handleRequest(request),
     );
-  }
+  };
 
-  handleRequest = (request) => {
+  handleRequest = async (request: any) => {
     try {
       const decoded = Request.decode(request);
 
       if (decoded.dRKeyStoreGet !== null) {
-        this.handleDRKeyStoreGet({ ...decoded.dRKeyStoreGet });
+        await this.handleDRKeyStoreGet(decoded.requestID, decoded.dRKeyStoreGet);
       } else if (decoded.dRKeyStorePut !== null) {
-        this.handleDRKeyStorePut({ ...decoded.dRKeyStorePut });
+        await this.handleDRKeyStorePut(decoded.requestID, decoded.dRKeyStorePut);
       } else if (decoded.dRKeyStoreDeleteMK !== null) {
-        this.handleDRKeyStoreDeleteMK({
-          messageKey: decoded.dRKeyStoreDeleteMK.key,
-          messageNumber: decoded.dRKeyStoreDeleteMK.msgNum,
-        });
+        await this.handleDRKeyStoreDeleteMK(decoded.requestID, decoded.dRKeyStoreDeleteMK);
       } else if (decoded.dRKeyStoreDeleteKeys !== null) {
-        this.handleDRKeyStoreDeleteKeys(decoded.dRKeyStoreDeleteKeys.key);
-      } else if (decoded.dRKeyStoreDeleteKeys !== null) {
-        this.handleDRKeyStoreCount(decoded.dRKeyStoreDeleteKeys.key);
+        await this.handleDRKeyStoreDeleteKeys(decoded.requestID, decoded.dRKeyStoreDeleteKeys);
+      } else if (decoded.dRKeyStoreCount !== null) {
+        await this.handleDRKeyStoreCount(decoded.requestID, decoded.dRKeyStoreCount);
+      } else if (decoded.dRKeyStoreAll !== null) {
+        await this.handleDRKeyStoreAll(decoded.requestID);
       } else if (decoded.showModal !== null) {
-        this.handleShowModal(decoded.showModal.title, decoded.showModal.layout);
+        await this.handleShowModal(decoded.requestID, decoded.showModal);
       } else if (decoded.sendEthereumTransaction !== null) {
-        this.handleSendEthereumTransaction(decoded.requestId, decoded.sendEthereumTransaction.value, decoded.sendEthereumTransaction.to, decoded.sendEthereumTransaction.data);
+        await this.handleSendEthereumTransaction(decoded.requestID, decoded.sendEthereumTransaction);
       } else if (decoded.saveDApp !== null) {
-        this.handleSaveDApp(decoded.saveDApp);
+        await this.handleSaveDApp(decoded.requestID, decoded.saveDApp);
       } else {
-        this.handleErrorMessage();
+        await this.handleErrorMessage(decoded.requestID, decoded);
       }
     } catch (e) {
       console.log('====================================');
@@ -63,102 +71,183 @@ export default class UpstreamService {
     }
   };
 
-   handleDRKeyStoreGet = async ({ messageNumber }) => {
-     const db = await defaultDB;
-     return db.objects('MessageKey').filtered(`messageNumber == '${messageNumber}'`);
-   };
-  handleDRKeyStorePut = async ({
-    messageKey,
-    messageNumber,
-  }:MessageKeyType) => {
-    const db = await defaultDB;
+  handleDRKeyStoreGet = async (id: string, info: any) => {
+    const { drKey, messageNumber } = info;
+    try {
+      const message = await this.getMessageKeyModel(drKey, messageNumber);
+      return this.sendSuccessResponse(id, {
+        messageKey: message.messageKey,
+      });
+    } catch (error) {
+      return this.sendErrorResponse(id, error);
+    }
+  };
+
+  handleDRKeyStorePut = async (id: string, info: any) => {
+    const { key: drKey, messageNumber, messageKey } = info;
+    const db = await this.dbPromise;
+    let drKeyModel;
+    try {
+      drKeyModel = await this.getDRKeyModel(drKey);
+    } catch (_) {
+      try {
+        db.write(() => {
+          drKeyModel = db.create('DoubleRatchetKey', {
+            accountId: this.currentAccountId,
+            doubleRatchetKey: drKey,
+          });
+        });
+      } catch (error) {
+        return this.sendErrorResponse(id, error);
+      }
+    }
     try {
       db.write(() => {
-        db.create('MessageKey', {
+        const createdKey = db.create('MessageKey', {
           messageKey,
           messageNumber,
         });
+        drKeyModel.messageKeys.push(createdKey);
       });
+      return this.sendSuccessResponse(id, {});
     } catch (error) {
-      throw new DatabaseWriteFailed(error);
+      return this.sendErrorResponse(id, error);
     }
   };
-  handleDRKeyStoreDeleteMK = async ({
-    messageKey,
-    messageNumber,
-  }:MessageKeyType) => {
-    const db = await defaultDB;
-    const message = db.objects('MessageKey').filtered(`messageKey == '${messageKey}'  AND messageNumber == '${messageNumber}'`);
+
+  handleDRKeyStoreDeleteMK = async (id: string, info: any) => {
+    const { key: drKey, msgNum: messageNumber } = info;
     try {
+      const db = await this.dbPromise;
+      const message = await this.getMessageKeyModel(drKey, messageNumber);
       db.write(() => {
         db.delete(message);
       });
+      return this.sendSuccessResponse(id, {});
     } catch (error) {
-      throw new DatabaseWriteFailed(error);
+      return this.sendErrorResponse(id, error);
     }
-  };
-  handleDRKeyStoreDeleteKeys =async (messageKey) => {
-    const db = await defaultDB;
-    const messages = db.objects('MessageKey').filtered(`messageKey == '${messageKey}'`);
-    try {
-      db.write(() => {
-        db.delete(messages);
-      });
-    } catch (error) {
-      throw new DatabaseWriteFailed(error);
-    }
-  };
-  handleDRKeyStoreCount = async (messageKey) => {
-    const db = await defaultDB;
-    const messages = db.objects('MessageKey').filtered(`messageKey == '${messageKey}'`);
-    return _.size(messages);
   };
 
-  handleShowModal = () => {};
-  handleSaveDApp = () => {};
-  handleSendEthereumTransaction = (id, value, to, data) => {
+  handleDRKeyStoreDeleteKeys = async (id: string, info: any) => {
+    const { key: drKey } = info;
+    try {
+      const db = await this.dbPromise;
+      const drKeyModel = await this.getDRKeyModel(drKey);
+      db.write(() => {
+        drKeyModel.messageKeys.forEach((messageKey) => {
+          db.delete(messageKey);
+        });
+      });
+      return this.sendSuccessResponse(id, {});
+    } catch (error) {
+      return this.sendErrorResponse(id, error);
+    }
+  };
+  handleDRKeyStoreCount = async (id: string, info: any) => {
+    const { key: drKey } = info;
+    try {
+      const drKeyModel = await this.getDRKeyModel(drKey);
+      return this.sendSuccessResponse(id, {
+        count: drKeyModel.messageKeys.length,
+      });
+    } catch (error) {
+      return this.sendErrorResponse(id, error);
+    }
+  };
+  handleDRKeyStoreAll = async (id: string) => {
+    const db = await this.dbPromise;
+    const drKeyModels = db.objects('DoubleRatchetKey').filtered(`accountId == '${this.currentAccountId}'`);
+    return this.sendSuccessResponse(id, {
+      all: drKeyModels.map((drKeyModel) => {
+        const messageKeys = {};
+        drKeyModel.messageKeys.forEach((messageKeyModel) => {
+          messageKeys[messageKeyModel.messageKeys] = messageKeyModel.messageKey;
+        });
+
+        return {
+          key: drKeyModel.doubleRatchetKey,
+          messageKeys,
+        };
+      }),
+    });
+  };
+
+  handleShowModal = (id: string, info: any) => {
+  };
+  handleSaveDApp = () => {
+  };
+  handleSendEthereumTransaction = async (id: string, info: any) => {
+    const { value, to, data } = info;
+
     const transaction = {
       to,
       data,
       value: ethers.utils.parseEther(value),
     };
-    this.ethereumService.wallet.sendTransaction(transaction)
-      .then((txDetails) => {
-        Panthalassa.PanthalassaSendResponse({
-          id,
-          data: Response.encode({
-            sendEthereumTransaction: {
-              nonce: txDetails.nonce,
-              gasPrice: txDetails.gasPrice.toString(),
-              gasLimit: txDetails.gasLimit.toString(),
-              value: txDetails.value.toString(),
-              to: txDetails.to,
-              data: txDetails.data,
-              chainId: txDetails.chainId,
-              from: txDetails.from,
-              hash: txDetails.hash,
-              v: txDetails.v,
-              r: txDetails.r,
-              s: txDetails.s,
-            },
-          }).finish(),
-          error: '',
-          timeout: 20,
-        });
-      })
-      .catch((err) => {
-        Panthalassa.PanthalassaSendResponse({
-          id,
-          data: Response.encode({}),
-          error: err,
-          timeout: 20,
-        });
+    try {
+      const txDetails = await this.ethereumService.wallet.sendTransaction(transaction);
+      return this.sendSuccessResponse(id, {
+        nonce: txDetails.nonce,
+        gasPrice: txDetails.gasPrice.toString(),
+        gasLimit: txDetails.gasLimit.toString(),
+        value: txDetails.value.toString(),
+        to: txDetails.to,
+        data: txDetails.data,
+        chainId: txDetails.chainId,
+        from: txDetails.from,
+        hash: txDetails.hash,
+        v: txDetails.v,
+        r: txDetails.r,
+        s: txDetails.s,
       });
+    } catch (error) {
+      return this.sendErrorResponse(id, error);
+    }
   };
-  handleErrorMessage = () => {};
+
+  handleErrorMessage = async (id: string, info: any) => this.sendErrorResponse(id, new Error(`Unknown upstream request ${info}`));
+
   unsubscribe = () => {
     if (this.eventsSubscription) {
       // this.eventsSubscription.remove();
     }
+  };
+
+  // Common functions
+
+  sendErrorResponse = async (id: string, error: Error) => Panthalassa.PanthalassaSendResponse({
+    id,
+    data: Response.encode({}),
+    responseError: error,
+    timeout: RESPONSE_TIMEOUT,
+  });
+
+  sendSuccessResponse = async (id: string, data: any) => Panthalassa.PanthalassaSendResponse({
+    id,
+    data: Response.encode(data),
+    responseError: '',
+    timeout: RESPONSE_TIMEOUT,
+  });
+
+  getDRKeyModel = async (drKey: string) => {
+    const db = await this.dbPromise;
+    const drKeyModels = db.objects('DoubleRatchetKey')
+      .filtered(`accountId == '${this.currentAccountId}' && doubleRatchetKey == '${drKey}'`);
+    if (drKeyModels.length === 0) {
+      throw new Error(`No message with drKey ${drKey} found!`);
+    }
+
+    return drKeyModels[0];
+  };
+
+  getMessageKeyModel = async (drKey: string, messageNumber: string) => {
+    const drKeyModel = await this.getDRKeyModel(drKey);
+    const messages = drKeyModel.messageKeys.filtered(`messageNumber == ${messageNumber}`);
+    if (messages.length === 0) {
+      throw new Error(`No message with number ${messageNumber} found!`);
+    }
+
+    return messages[0];
   };
 }
