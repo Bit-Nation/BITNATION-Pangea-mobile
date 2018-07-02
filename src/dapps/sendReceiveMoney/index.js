0ecdc52ce27305f3bceedb326c87215cf92ddfa5
Florian Lenz <flenz81@googlemail.com>
2018-07-02 18:34:37 +0300

    [dapps] added base

---
+++ src/dapps/sendReceiveMoney/index.js
@@ -0,0 +1,15 @@
+// @flow
+import DAppType from '../';
+
+const dApp:DAppType = {
+  name: 'send / receive money',
+  identityPublicKey: '0x@todo',
+  openDApp: (payload:Object, services:Object) => {
+
+  },
+  renderMessage: (payload:Object, services:Object) => {
+
+  },
+};
+
+export default dApp;
