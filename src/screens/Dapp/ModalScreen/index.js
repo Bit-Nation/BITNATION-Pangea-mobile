3c29de9e03f30930383daf140a5d531f6d1d3858
Andrii Selivanov <seland2008@gmail.com>
2018-07-02 22:57:02 +0300

    [project] Implement sending DApp messages.

--- src/screens/Dapp/ModalScreen/index.js
+++ src/screens/Dapp/ModalScreen/index.js
@@ -12,6 +12,11 @@ import type { WalletType } from '../../../types/Wallet';
 import View from '../../../components/dApps/View';
 import FakeNavigationBar from '../../../components/common/FakeNavigationBar';
 import BackgroundImage from '../../../components/common/BackgroundImage';
+import { sendDAppMessage } from '../../../actions/dApps';
+import { getCurrentAccount } from '../../../reducers/accounts';
+import { getSelectedSession } from '../../../utils/chat';
+import type { ChatSessionType, DAppMessageType, GiftedChatMessageType } from '../../../types/Chat';
+import type { Account } from '../../../types/Account';
 
 type OwnProps = {
   /**
@@ -22,13 +27,29 @@ type OwnProps = {
    * @desc Public key of DApp that controls the screen.
    */
   dAppPublicKey: string,
+  /**
+   * @desc Shared secret for the chat session
+   */
+  chatSecret: string,
+  /**
+   * @desc Current user.
+   */
+  user: Account
 }
 
 type Props = {
   /**
-   * @desc List of wallets
+   * @desc List of wallets.
    */
   wallets: Array<WalletType>,
+  /**
+   * @desc List of chat.
+   */
+  sessions: Array<ChatSessionType>,
+  /**
+   * @desc Function to send a DApp message.
+   */
+  sendMessage: (message: DAppMessageType, session: ChatSessionType, callback: (message: ?GiftedChatMessageType) => void) => void,
 }
 
 
@@ -49,12 +70,22 @@ class DAppModalScreen extends NavigatorComponent<Props & OwnProps> {
   }
 
   render() {
+    const session = getSelectedSession(this.props.sessions, this.props.chatSecret);
+
+    if (session == null) {
+      this.props.navigator.dismissModal();
+      return null;
+    }
+
     return (
       <View style={GlobalStyles.screenContainer}>
         <BackgroundImage />
         <FakeNavigationBar />
         <View style={GlobalStyles.bodyContainer}>
-          {React.createElement(dAppProvider(this.props)(this.props.component))}
+          {React.createElement(dAppProvider({
+            ...this.props,
+            session,
+          })(this.props.component))}
         </View>
       </View>
     );
@@ -63,8 +94,14 @@ class DAppModalScreen extends NavigatorComponent<Props & OwnProps> {
 
 const mapStateToProps = state => ({
   wallets: state.wallet.wallets,
+  sessions: state.chat.chats,
+  user: getCurrentAccount(state.accounts),
 });
 
-const mapDispatchToProps = dispatch => ({});
+const mapDispatchToProps = dispatch => ({
+  sendMessage(message, session, callback) {
+    dispatch(sendDAppMessage(message, session, callback));
+  },
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(DAppModalScreen);
