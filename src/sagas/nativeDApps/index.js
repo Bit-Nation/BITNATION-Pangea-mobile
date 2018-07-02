aad1e1bc279857fcfcb179fc6b0ce359bd5fb002
Andrii Selivanov <seland2008@gmail.com>
2018-07-02 21:53:34 +0300

    [project] Implement nativeDApp sagas.

---
+++ src/sagas/nativeDApps/index.js
@@ -0,0 +1,14 @@
+import { all, takeEvery } from 'redux-saga/effects';
+import { openDApp, sendDAppMessage } from './sagas';
+import { OPEN_DAPP, SEND_DAPP_MESSAGE } from '../../actions/dApps';
+
+/**
+ * @desc Root key saga.
+ * @return {void}
+ */
+export default function* rootSaga() {
+  yield all([
+    yield takeEvery(OPEN_DAPP, openDApp),
+    yield takeEvery(SEND_DAPP_MESSAGE, sendDAppMessage),
+  ]);
+}
