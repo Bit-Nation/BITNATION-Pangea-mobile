8962ca73afee986abd2bc53c43792621a37321c2
Andrii Selivanov <seland2008@gmail.com>
2018-07-02 21:53:13 +0300

    [project] Create nativeDApp reducer.
    Former-commit-id: 3d17b9a4da0db9e67bfe73fce7727a7584920dcb

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -10,7 +10,7 @@ import activity, { type State as ActivityState } from './activity';
 import testingMode, { type State as TestingModeState } from './testingMode';
 import chat, { type State as ChatState } from './chat';
 import settings, { type State as SettingsState } from './settings';
-import dApps, { type State as DAppsState } from './dApps';
+import nativeDApps, { type State as NativeDAppsState } from './nativeDApps';
 
 export type State = {
   accounts: AccountsState,
@@ -22,7 +22,7 @@ export type State = {
   testingMode: TestingModeState,
   chat: ChatState,
   settings: SettingsState,
-  dApps: DAppsState,
+  dApps: NativeDAppsState,
 }
 
 export const subReducers = {
@@ -35,7 +35,7 @@ export const subReducers = {
   testingMode,
   chat,
   settings,
-  dApps,
+  dApps: nativeDApps,
 };
 
 export default combineReducers(subReducers);
