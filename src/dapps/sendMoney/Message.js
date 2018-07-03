b76e622a6f4f6c76f3ac0636156a1d232871453f
Andrii Selivanov <seland2008@gmail.com>
2018-07-03 11:53:36 +0300

    [project] Finish sendMoney DApp.
    Former-commit-id: 5c2fc2b4c4585c7f952564249b939f1108fb3a10

--- src/dapps/sendMoney/Message.js
+++ src/dapps/sendMoney/Message.js
@@ -1,7 +1,10 @@
+/* eslint-disable prefer-destructuring,class-methods-use-this */
 import * as React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import Colors from '../../global/colors';
 import type { ProvidedProps as MessageProvidedProp } from '../../components/nativeDApps/MessageProvider';
+import { SendMoneyMessageData } from './Modal';
+import type { ProfileType } from '../../types/Chat';
 
 const styles = StyleSheet.create({
   container: { margin: 5 },
@@ -14,14 +17,76 @@ const styles = StyleSheet.create({
   },
 
 });
-const MessageView = ({ context: { dAppMessage: { params = '' } } }:MessageProvidedProp) => {
-  const { data = {} } = JSON.parse(params);
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>
-      Send <Text style={styles.textBold}>{`${data.amount} ${data.currency}`} </Text>to {data.to}
-      </Text>
-    </View>
-  );
-};
-export default MessageView;
+
+type Status = 'pending' | 'failed' | 'success';
+
+type State = {
+  status: Status,
+  txHash: string,
+  to: ProfileType,
+  amount: string,
+  currency: string,
+  invalidMessage: boolean,
+}
+
+export default class Message extends React.Component<MessageProvidedProp, State> {
+  constructor(props) {
+    super(props);
+
+    const params: string = this.props.context.dAppMessage.params;
+    const data: SendMoneyMessageData = JSON.parse(params);
+
+    this.state = {
+      status: 'pending',
+      ...data,
+      invalidMessage: this.validateParams(data) === false,
+    };
+
+    this.trackTransaction();
+  }
+
+  validateParams(data: SendMoneyMessageData) {
+    return data.amount != null
+      && data.currency != null
+      && data.to != null
+      && data.to.name != null
+      && data.fromAddress != null
+      && data.txHash != null;
+  }
+
+  trackTransaction = async () => this.props.services.ethereumService.trackTransaction(this.state.txHash).then((result) => {
+    this.setState({ status: 'success' });
+  }).catch(() => {
+    this.setState({ status: 'failed' });
+  });
+
+  render() {
+    if (this.state.invalidMessage) {
+      return null;
+    }
+
+    const statusText = (() => {
+      switch (this.state.status) {
+        case 'pending':
+          return 'Money sending';
+        case 'failed':
+          return 'Send failed';
+        case 'success':
+          return 'Money sent';
+        default:
+          return '';
+      }
+    })();
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.textBold}>
+          {statusText}
+        </Text>
+        <Text style={styles.text}>
+          Send {this.state.amount} {this.state.currency} to {this.state.to.name}
+        </Text>
+      </View>
+    );
+  }
+}
