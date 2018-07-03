3dca13b36450af1a00c7f14b2e6b66fbc6ba9ef7
Hoang Hung <hth321@gmail.com>
2018-07-03 14:24:18 +0700

    [dapps] UI for messages sent money
    Former-commit-id: 3d9f2cafcce6056f3a7ace300fd88969ecb402e0 [formerly fa20b8b1d24d4bb8b176545334bb93d35685d01e]
    Former-commit-id: d2b03ebbece61bfe5459e2dd41b1753993f5f36a

--- src/dapps/sendMoney/Message.js
+++ src/dapps/sendMoney/Message.js
@@ -1,16 +1,27 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
-
+import { Text, View, StyleSheet } from 'react-native';
+import Colors from '../../global/colors';
 import type { ProvidedProps as MessageProvidedProp } from '../../components/nativeDApps/MessageProvider';
 
-export default class Message extends React.Component<MessageProvidedProp> {
-  render() {
-    return (
-      <View>
-        <Text>
-          {this.props.context.dAppMessage.params}
-        </Text>
-      </View>
-    );
-  }
-}
+const styles = StyleSheet.create({
+  container: { margin: 5 },
+  text: {
+    fontSize: 17,
+    color: Colors.BitnationDarkGrayColor,
+  },
+  textBold: {
+    fontWeight: 'bold',
+  },
+
+});
+const MessageView = ({ context: { dAppMessage: { params = '' } } }:MessageProvidedProp) => {
+  const { data = {} } = JSON.parse(params);
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>
+      Send <Text style={styles.textBold}>{`${data.amount} ${data.currency}`} </Text>to {data.to}
+      </Text>
+    </View>
+  );
+};
+export default MessageView;
