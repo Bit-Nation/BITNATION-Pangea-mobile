3e4818ecb1a45e5ea9808cee041e2f78f1f812c3
Andrii Selivanov <seland2008@gmail.com>
2018-07-05 01:53:59 +0300

    [project] Implement creating ethereumService for each DApp to get correct CustomSigner. Remove walletService and pass only sendMoney method to DApps.
    Former-commit-id: 1dd3529f39c8af24e42471e9a8d619e40b95f7f0 [formerly 83b39921a16fcd8dc8ab1fbb10b5c6c6650e9829]
    Former-commit-id: dfd43d7430cc0fd56c722b3ec34ee737f0b6abd8

--- src/services/dAppsWalletService/index.js
+++ src/services/dAppsWalletService/index.js
@@ -8,41 +8,70 @@ import {
 } from '../../global/Constants';
 import EthereumService from '../ethereum';
 import type { CurrencyType } from '../../types/Wallet';
+import EthereumServiceFactory from '../ethereum/factory';
+import { normalizeEthPrivateKey } from '../../utils/key';
 
 export default class DAppsWalletService {
-  constructor(ethereumService: EthereumService, account: Account) {
+  constructor(ethereumService: EthereumService, serviceBuilder: (app: string) => EthereumService, account: Account) {
     this.ethereumService = ethereumService;
     this.currentAccount = account;
+    this.serviceBuilder = serviceBuilder;
   }
 
   ethereumService: EthereumService;
+  dAppsEthereumServices: { [string]: EthereumService } = {};
   currentAccount: Account;
+  serviceBuilder: (app: string) => EthereumService;
 
-  async sendMoney(currency: CurrencyType, toAddress: string, amount: string): Promise<void> {
+  /**
+   * @desc Function to send money.
+   * @param {string} app Name of the app that requests send.
+   * @param {CurrencyType} currency Currency name (ETH, XPAT, etc).
+   * @param {string} toAddress Address to send ether to.
+   * @param {string} amount Amount in base currency unit (ether, XPAT, etc.)
+   * @return {Promise<void>} Promise
+   */
+  async sendMoney(app: string, currency: CurrencyType, toAddress: string, amount: string): Promise<void> {
     switch (currency) {
-      case 'ETH': return this.sendEther(toAddress, amount);
-      case 'XPAT': return this.sendXPAT(toAddress, amount);
-      default: return Promise.reject(new Error(`Currency ${currency} is not supported`));
+      case 'ETH':
+        return this.sendEther(app, toAddress, amount);
+      case 'XPAT':
+        return this.sendXPAT(app, toAddress, amount);
+      default:
+        return Promise.reject(new Error(`Currency ${currency} is not supported`));
     }
+  }
+
+  getEthereumServiceForDApp(app: string) {
+    if (app == null || app === '') {
+      throw Error('app parameter should be specified on send money request');
+    }
+    if (this.dAppsEthereumServices[app] == null) {
+      this.dAppsEthereumServices[app] = this.serviceBuilder(app);
+    }
+
+    return this.dAppsEthereumServices[app];
   }
 
   /**
    * @desc Function to send some ether to specificied address.
+   * @param {string} app Name of the app that requests send.
    * @param {string} toAddress Address to send ether to.
    * @param {string} amount Amount in ether.
    * @return {Promise<void>} Promise
    */
-  async sendEther(toAddress: string, amount: string): Promise<void> {
-    return this.ethereumService.sendMoney(toAddress, amount);
+  async sendEther(app: string, toAddress: string, amount: string): Promise<void> {
+    return this.getEthereumServiceForDApp(app).sendMoney(toAddress, amount);
   }
 
   /**
    * @desc Function to send some XPAT to specificied address.
+   * @param {string} app Name of the app that requests send.
    * @param {string} toAddress Address to send ether to.
    * @param {string} amount Amount in XPAT.
    * @return {Promise<void>} Promise
    */
-  async sendXPAT(toAddress: string, amount: string) {
-    return this.ethereumService.sendTokens(this.currentAccount.networkType === 'dev' ? PAT_DEV_ADDRESS : PAT_PROD_ADDRESS, toAddress, amount);
+  async sendXPAT(app: string, toAddress: string, amount: string) {
+    return this.getEthereumServiceForDApp(app).sendTokens(this.currentAccount.networkType === 'dev' ? PAT_DEV_ADDRESS : PAT_PROD_ADDRESS, toAddress, amount);
   }
 }
