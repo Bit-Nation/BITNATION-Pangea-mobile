c60e907fad50c4bb589f78a158b3849fa7d05889
Andrii Selivanov <seland2008@gmail.com>
2018-07-06 21:35:12 +0300

    [project] Start implementing ExchangeInitiatedMessage for escrow DApp.
    Former-commit-id: 527293b86049ca8f9107db64029835f56196ef32

--- src/dapps/escrow/Message.js
+++ src/dapps/escrow/Message.js
@@ -1,94 +1,54 @@
+// @flow
+
 /* eslint-disable prefer-destructuring,class-methods-use-this */
 import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import Colors from '../../global/colors';
-import type { ProvidedProps as MessageProvidedProp } from '../../components/nativeDApps/MessageProvider';
-import { SendMoneyMessageData } from './Modal';
-import type { ProfileType } from '../../types/Chat';
+import { View, StyleSheet } from 'react-native';
+import type { ProvidedProps as MessageProvidedProps } from '../../components/nativeDApps/MessageProvider';
+import { MESSAGE_TYPES } from './Constants';
+import ExchangeInitiatedMessage from './ExchangeInitiatedMessage';
 
 const styles = StyleSheet.create({
   container: { margin: 5 },
-  text: {
-    fontSize: 17,
-    color: Colors.BitnationDarkGrayColor,
-  },
-  textBold: {
-    fontWeight: 'bold',
-  },
-
 });
 
-type Status = 'pending' | 'failed' | 'success';
-
-type State = {
-  status: Status,
-  txHash: string,
-  to: ProfileType,
-  amount: string,
-  currency: string,
-  fromAddress: string,
-  fromName: string,
-  invalidMessage: boolean,
-}
-
-export default class Message extends React.Component<MessageProvidedProp, State> {
-  constructor(props) {
+export default class Message extends React.Component<MessageProvidedProps, *> {
+  constructor(props: MessageProvidedProps) {
     super(props);
 
     const params: string = this.props.context.dAppMessage.params;
-    const data: SendMoneyMessageData = JSON.parse(params);
+    const messageType = this.props.context.dAppMessage.type;
+    const data = JSON.parse(params);
 
     this.state = {
-      status: 'pending',
-      ...data,
-      invalidMessage: this.validateParams(data) === false,
+      data,
+      invalidMessage: this.validateParams(data, messageType) === false,
     };
-
-    this.trackTransaction();
   }
 
-  validateParams(data: SendMoneyMessageData) {
-    return data.amount != null
-      && data.currency != null
-      && data.to != null
-      && data.to.name != null
-      && data.fromAddress != null
-      && data.toAddress != null
-      && data.txHash != null;
+  validateParams(data: Object, type: string) {
+    switch (type) {
+      case MESSAGE_TYPES.EXCHANGE_INITIATED:
+        return data.amount != null
+          && data.currency != null
+          && data.to != null
+          && data.to.name != null
+          && data.fromAddress != null
+          && data.toAddress != null
+          && data.txHash != null;
+      default:
+        return false;
+    }
   }
-
-  trackTransaction = async () => this.props.services.ethereumService.trackTransaction(this.state.txHash).then(() => {
-    this.setState({ status: 'success' });
-  }).catch(() => {
-    this.setState({ status: 'failed' });
-  });
 
   render() {
     if (this.state.invalidMessage) {
       return null;
     }
 
-    const statusText = (() => {
-      switch (this.state.status) {
-        case 'pending':
-          return 'Checking status';
-        case 'failed':
-          return 'Send failed';
-        case 'success':
-          return 'Money sent';
-        default:
-          return '';
-      }
-    })();
-
     return (
       <View style={styles.container}>
-        <Text style={styles.textBold}>
-          {statusText}
-        </Text>
-        <Text style={styles.text}>
-          Send {this.state.amount} {this.state.currency} from {this.state.fromName} to {this.state.to.name}
-        </Text>
+        {this.props.context.dAppMessage.type === MESSAGE_TYPES.EXCHANGE_INITIATED
+        && <ExchangeInitiatedMessage {...this.state.data} />}
       </View>
     );
   }
