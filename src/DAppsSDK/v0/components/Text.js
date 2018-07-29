4a420790b79517f6f776fb8954dbfb262ec1abf9
Andrii Selivanov <seland2008@gmail.com>
2018-07-29 13:28:06 +0300

    [project] Changes to VM DApps infrastructure.
    Former-commit-id: 08af880163b4ea6d19af4f5c8b45b6616e72e915

---
+++ src/DAppsSDK/v0/components/Text.js
@@ -0,0 +1,79 @@
+// @flow
+
+import React, { Component } from 'react';
+import { Text as ReactNativeText } from 'react-native';
+
+import styles from '../../../global/Styles';
+
+type Props = {
+  /**
+   * @desc Props that should be passed as they are to backed native component.
+   */
+  nativeProps: Object,
+  /**
+   * @desc Name of predefined type of component.
+   */
+  type: string,
+  /**
+   * @desc Style object.
+   */
+  style?: Object
+}
+
+const types = [
+  'largeTitle',
+  'title1',
+  'title2',
+  'title3',
+  'headline',
+  'body',
+  'bodyBold',
+  'bodyBoldBlack',
+  'bodyBoldBlackSmall',
+  'bodyBlack',
+  'callout',
+  'subhead',
+  'footnote',
+  'caption1',
+  'caption2',
+  'disabledText',
+];
+
+export default class Text extends Component<Props> {
+  static validNativeProps = [
+    'numberOfLines',
+    'ellipsizeMode',
+  ];
+
+  static customProps = [
+    'style',
+    'type',
+  ];
+
+  static defaultProps = {
+    type: 'body',
+  };
+
+  styleForType(type: string) {
+    if (types.includes(type) === false) {
+      return null;
+    }
+
+    return styles[type];
+  }
+
+  render() {
+    const typeStyle = this.props.type ? this.styleForType(this.props.type) : null;
+    if (this.props.type != null && typeStyle == null) {
+      console.warn(`Invalid value '${this.props.type}' for 'type' property of 'Text' component`);
+    }
+
+    return (
+      <ReactNativeText
+        {...this.props.nativeProps}
+        style={[typeStyle, this.props.style]}
+      />
+    );
+  }
+}
+
---
+++ src/DAppsSDK/v0/components/index.js
@@ -0,0 +1,11 @@
+import Text from '../components/Text';
+import View from '../components/View';
+import TextInput from '../components/TextInput';
+import Button from '../components/Button';
+
+export default {
+  Text,
+  View,
+  TextInput,
+  Button,
+};
