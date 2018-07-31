4960dc6e39d522d6c3be6d935e1694d46815ea36
prameetc <prameet.c@gmail.com>
2018-07-31 22:46:24 +0530

    [migration] Fixed npm test errors
    Former-commit-id: c999e8e26cdd83aa7478dc2c02dd86efdd25e445 [formerly 233183c7b2683475f2947769ef62c995c1f8b00c]
    Former-commit-id: 577a1ddae1efb1056bdbcb4b77a79753d8de2b01

--- __tests__/src/sagas/index.js
+++ __tests__/src/sagas/index.js
@@ -15,7 +15,6 @@ import nations from '../../../src/sagas/nations';
 import txProcessor from '../../../src/sagas/txProcessor';
 import dApps from '../../../src/sagas/nativeDApps';
 import chat from '../../../src/sagas/chat';
-import migration from '../../../src/sagas/migration';
 
 test('rootSaga', () => {
   const iterator = rootSaga();
@@ -27,7 +26,6 @@ test('rootSaga', () => {
     call(key),
     call(settings),
     call(wallet),
-    call(migration),
     call(serviceContainer),
     call(modifyNation),
     call(nations),
