233183c7b2683475f2947769ef62c995c1f8b00c
prameetc <prameet.c@gmail.com>
2018-07-31 22:46:24 +0530

    [migration] Fixed npm test errors

--- __tests__/src/reducers/migration.js
+++ __tests__/src/reducers/migration.js
@@ -1,14 +1,14 @@
-import reducer, { initialState } from '../../../src/reducers/key';
+import reducer, { initialState } from '../../../src/reducers/migration';
 import { storeVersion } from '../../../src/actions/migration';
 
 describe('migration reducer action handling', () => {
-  const version = '1.0.7';
+  const migrationVersion = '1.0.7';
   test('storeVersion', () => {
     const stateBefore = initialState;
-    const stateAfter = reducer(stateBefore, storeVersion(version));
+    const stateAfter = reducer(stateBefore, storeVersion(migrationVersion));
     expect(stateAfter).toEqual({
       ...stateBefore,
-      version,
+      migrationVersion,
     });
   });
 });
