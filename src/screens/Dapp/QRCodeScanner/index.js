131a726edbb6550e36ef7e01c2a17f5aa4f64003
Andrii Selivanov <seland2008@gmail.com>
2018-08-02 18:24:42 +0300

    [revert] Returned QRCode scanner to connect to dev host.

--- src/screens/Dapp/QRCodeScanner/index.js
+++ src/screens/Dapp/QRCodeScanner/index.js
@@ -1,14 +1,75 @@
-import { compose, withHandlers, hoistStatics } from 'recompose';
-import { Alert } from 'react-native';
-import View from './view';
+// @flow
 
-export default hoistStatics(compose(withHandlers({
-  onRead: () => (result) => {
-    Alert.alert(result.data);
-  },
-  onNavBarButtonPress: ({ navigator }) => (id) => {
+import React from 'react';
+
+import { Alert, Text, View } from 'react-native';
+import QRCodeScanner from 'react-native-qrcode-scanner';
+
+import DAppsService from '../../../services/dApps';
+import styles from './styles';
+import NavigatorComponent from '../../../components/common/NavigatorComponent';
+import i18n from '../../../global/i18n';
+import Loading from '../../../components/common/Loading';
+
+type State = {
+  /**
+   * @desc Flag that shows if connecting is currently in progress.
+   */
+  isConnecting: boolean,
+}
+
+export default class DAppQRCodeScannerScreen extends NavigatorComponent<void, State> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = { isConnecting: false };
+  }
+
+  scanner: ?QRCodeScanner;
+
+  onNavBarButtonPress(id: string) {
     if (id === 'cancel') {
-      navigator.dismissModal();
+      this.props.navigator.pop();
     }
-  },
-})))(View);
+  }
+
+  onReadQRCode = (address: string) => {
+    this.setState({ isConnecting: true });
+    DAppsService.connectToDAppHost(address).then(() => {
+      Alert.alert('Success', null, [
+        {
+          text: i18n.t('common.ok'),
+          onPress: () => { this.props.navigator.pop(); },
+        },
+      ]);
+    }).catch((error) => {
+      Alert.alert(`Connect failed with error ${error}`, null, [
+        {
+          text: i18n.t('common.ok'),
+          onPress: () => {
+            if (this.scanner) {
+              this.scanner.reactivate();
+            }
+          },
+        },
+      ]);
+    }).finally(() => {
+      this.setState({ isConnecting: false });
+    });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <QRCodeScanner
+          onRead={result => this.onReadQRCode(result.data)}
+          topContent={
+            <Text style={styles.centerText}>{i18n.t('screens.dAppQRCodeScanner.instruction')}</Text>
+        }
+          ref={scanner => (this.scanner = scanner)}
+        />
+        {this.state.isConnecting === true ? <Loading /> : null}
+      </View>
+    );
+  }
+}
