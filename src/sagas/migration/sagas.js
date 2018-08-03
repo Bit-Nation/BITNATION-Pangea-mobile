9c719ac556756bb961006757f3a7f3317a6f3083
Andrii Selivanov <seland2008@gmail.com>
2018-08-03 03:24:28 +0300

    [migration] Implement dummy migration.

--- src/sagas/migration/sagas.js
+++ src/sagas/migration/sagas.js
@@ -1,6 +1,9 @@
 // @flow
 
-import { select } from 'redux-saga/effects';
+import { select, call } from 'redux-saga/effects';
+import defaultDB from '../../services/database';
+import { getCurrentAccount } from '../accounts/sagas';
+import { launchLoggedInFlow } from '../navigation/sagas';
 
 /**
  * @desc Start migration.
@@ -8,6 +11,13 @@ import { select } from 'redux-saga/effects';
  */
 export function* startMigration(): Generator<*, *, *> {
   // @todo All migration functions will go here
+  const db = yield defaultDB;
+  const currentAccount = yield getCurrentAccount();
+  db.write(() => {
+    db.create('Account', { ...currentAccount, lastMigrationVersion: '1.1.0' }, true);
+  });
+
+  yield call(launchLoggedInFlow);
 }
 
 /**
@@ -15,8 +25,8 @@ export function* startMigration(): Generator<*, *, *> {
  * @return {void}
  */
 export function* isMigration(): Generator<*, *, any> {
-  const { migration } = yield select();
-  if (migration.migrationVersion !== '1.1.0') {
+  const currentAccount = yield getCurrentAccount();
+  if (currentAccount.lastMigrationVersion !== '1.1.0') {
     return true;
   }
   return false;
