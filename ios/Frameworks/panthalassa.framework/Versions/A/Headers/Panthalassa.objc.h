f96a22ddd738aa99cc45eb22cf23340761331a4f
Florian Lenz <flenz81@googlemail.com>
2018-08-04 10:55:58 +0300

    [ios] updated panthalassa
    Former-commit-id: 18df1c0eedad94e9e5d837547115c555bbba09a8 [formerly 5bbb270d15d78bab0dfa9110707b153e06a23078]
    Former-commit-id: e236e5e044d7ec5e5cf3b633c33e1976f0933b1e

--- ios/Frameworks/panthalassa.framework/Versions/A/Headers/Panthalassa.objc.h
+++ ios/Frameworks/panthalassa.framework/Versions/A/Headers/Panthalassa.objc.h
@@ -25,19 +25,8 @@
 
 - (instancetype)initWithRef:(id)ref;
 - (instancetype)init;
-/**
- * add friend to peer store
- */
 - (BOOL)addContact:(NSString*)pubKey error:(NSError**)error;
-/**
- * Export account with the given password
- */
 - (NSString*)export:(NSString*)pw pwConfirm:(NSString*)pwConfirm error:(NSError**)error;
-/**
- * Stop the panthalassa instance
-this becomes interesting when we start
-to use the mesh network
- */
 - (BOOL)stop:(NSError**)error;
 @end
 
@@ -63,55 +52,30 @@ FOUNDATION_EXPORT BOOL PanthalassaCallDAppFunction(NSString* dAppId, long id_, N
 
 FOUNDATION_EXPORT BOOL PanthalassaConnectLogger(NSString* address, NSError** error);
 
-/**
- * connect the host to DApp development server
- */
 FOUNDATION_EXPORT BOOL PanthalassaConnectToDAppDevHost(NSString* address, NSError** error);
 
 FOUNDATION_EXPORT NSString* PanthalassaDApps(NSError** error);
 
 FOUNDATION_EXPORT NSString* PanthalassaEthAddress(NSError** error);
 
-/**
- * Eth Private key
- */
 FOUNDATION_EXPORT NSString* PanthalassaEthPrivateKey(NSError** error);
 
-/**
- * converts an ethereum public key to address
- */
 FOUNDATION_EXPORT NSString* PanthalassaEthPubToAddress(NSString* pub, NSError** error);
 
-/**
- * Export the current account store with given password
- */
 FOUNDATION_EXPORT NSString* PanthalassaExportAccountStore(NSString* pw, NSString* pwConfirm, NSError** error);
 
-/**
- * fetch the identity public key of the
- */
 FOUNDATION_EXPORT NSString* PanthalassaGetIdentityPublicKey(NSError** error);
 
 FOUNDATION_EXPORT NSString* PanthalassaGetMnemonic(NSError** error);
 
 FOUNDATION_EXPORT NSString* PanthalassaIdentityPublicKey(NSError** error);
 
-/**
- * Check if mnemonic is valid
- */
 FOUNDATION_EXPORT BOOL PanthalassaIsValidMnemonic(NSString* mne);
 
 FOUNDATION_EXPORT NSString* PanthalassaMessages(NSString* partner, int64_t start, long amount, NSError** error);
 
-/**
- * Creates an new set of encrypted account key's
- */
 FOUNDATION_EXPORT NSString* PanthalassaNewAccountKeys(NSString* pw, NSString* pwConfirm, NSError** error);
 
-/**
- * Create new account store from mnemonic
-This can e.g. be used in case you need to recover your account
- */
 FOUNDATION_EXPORT NSString* PanthalassaNewAccountKeysFromMnemonic(NSString* mne, NSString* pw, NSString* pwConfirm, NSError** error);
 
 FOUNDATION_EXPORT BOOL PanthalassaOpenDApp(NSString* id_, NSString* context, NSError** error);
@@ -126,26 +90,14 @@ FOUNDATION_EXPORT BOOL PanthalassaSetLogger(NSString* level, NSError** error);
 
 FOUNDATION_EXPORT NSString* PanthalassaSignProfile(NSString* name, NSString* location, NSString* image, NSError** error);
 
-/**
- * sign profile
- */
 FOUNDATION_EXPORT NSString* PanthalassaSignProfileStandAlone(NSString* name, NSString* location, NSString* image, NSString* keyManagerStore, NSString* password, NSError** error);
 
-/**
- * start panthalassa
- */
 FOUNDATION_EXPORT BOOL PanthalassaStart(NSString* dbDir, NSString* config, NSString* password, id<PanthalassaUpStream> client, id<PanthalassaUpStream> uiUpstream, NSError** error);
 
 FOUNDATION_EXPORT BOOL PanthalassaStartDApp(NSString* dAppSingingKeyStr, long timeout, NSError** error);
 
-/**
- * create a new panthalassa instance with the mnemonic
- */
 FOUNDATION_EXPORT BOOL PanthalassaStartFromMnemonic(NSString* dbDir, NSString* config, NSString* mnemonic, id<PanthalassaUpStream> client, id<PanthalassaUpStream> uiUpstream, NSError** error);
 
-/**
- * Stop panthalassa
- */
 FOUNDATION_EXPORT BOOL PanthalassaStop(NSError** error);
 
 FOUNDATION_EXPORT BOOL PanthalassaStopDApp(NSString* dAppSingingKeyStr, NSError** error);
