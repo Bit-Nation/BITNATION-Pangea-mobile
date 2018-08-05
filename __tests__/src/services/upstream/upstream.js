d821175bc00fa525f3d0f2a1e10bf5a80013d93b
Andrii Selivanov <seland2008@gmail.com>
2018-08-06 00:20:40 +0300

    [tests] Updated upstream tests.
    Former-commit-id: 6ffeb60e984f06090bfeb982275363af9101a40d [formerly 44daf15b3a62a7e773bd1ae9dbd4f03373239dc9]
    Former-commit-id: 0ffb79eab4173843116daecf0bc374cb658fa66d

--- __tests__/src/services/upstream/upstream.js
+++ __tests__/src/services/upstream/upstream.js
@@ -2,7 +2,6 @@ import { Buffer } from 'buffer';
 
 import UpstreamService from '../../../../src/services/upstream/upstream';
 import { api_proto as apiProto } from '../../../../src/services/upstream/compiled';
-import { buildRandomPathDatabase } from '../../../../src/services/database';
 
 jest.mock('NativeModules', () => ({
   Panthalassa: {
@@ -21,51 +20,6 @@ jest.mock('react-native-navigation', () => ({
 }));
 
 const ethereumServiceMock = {};
-const dbPromise = buildRandomPathDatabase();
-
-const dRKeyStoreGet = {
-  requestID: '1',
-  dRKeyStoreGet: {
-    drKey: new Int8Array(1, 2, 3),
-    messageNumber: 2,
-  },
-};
-
-const dRKeyStorePut = {
-  requestID: '1',
-  dRKeyStorePut: {
-    key: new Int8Array(1, 2, 3),
-    messageNumber: 2,
-    messageKey: new Int8Array(1, 2, 3),
-  },
-};
-
-const dRKeyStoreDeleteMK = {
-  requestID: '1',
-  dRKeyStoreDeleteMK: {
-    key: new Int8Array(1, 2, 3),
-    msgNum: 2,
-  },
-};
-
-const dRKeyStoreDeleteKeys = {
-  requestID: '1',
-  dRKeyStoreDeleteKeys: {
-    key: new Int8Array(1, 2, 3),
-  },
-};
-
-const dRKeyStoreCount = {
-  requestID: '1',
-  dRKeyStoreCount: {
-    key: new Int8Array(1, 2, 3),
-  },
-};
-
-const dRKeyStoreAll = {
-  requestID: '1',
-  dRKeyStoreAll: {},
-};
 
 const showModal = {
   requestID: '1',
@@ -84,48 +38,38 @@ const sendEthereumTransaction = {
   },
 };
 
-const saveDApp = {
-  requestID: '1',
-  saveDApp: {
-    appName: '1',
-    code: '2',
-    signature: new Int8Array('test'),
-    signingPublicKey: new Int8Array('test'),
+const dAppPersisted = {
+  name: 'DAPP:PERSISTED',
+  payload: {
+    name: 'DAPP',
   },
 };
 
 const { Request } = apiProto;
 
-const prepareRequest = object => ({ upstream: Buffer.from(Request.encode(object).finish()).toString('base64') });
+const prepareClientRequest = object => ({ client: Buffer.from(Request.encode(object).finish()).toString('base64') });
+const prepareUIAPIRequest = object => ({ ui: JSON.stringify(object) });
 
 describe('Request', () => {
   test('encoding', () => {
-    expect(prepareRequest(dRKeyStoreGet)).toBeDefined();
-    expect(prepareRequest(dRKeyStorePut)).toBeDefined();
-    expect(prepareRequest(dRKeyStoreDeleteMK)).toBeDefined();
-    expect(prepareRequest(dRKeyStoreDeleteKeys)).toBeDefined();
-    expect(prepareRequest(dRKeyStoreCount)).toBeDefined();
-    expect(prepareRequest(dRKeyStoreAll)).toBeDefined();
-    expect(prepareRequest(showModal)).toBeDefined();
-    expect(prepareRequest(sendEthereumTransaction)).toBeDefined();
-    expect(prepareRequest(saveDApp)).toBeDefined();
+    expect(prepareClientRequest(showModal)).toBeDefined();
+    expect(prepareClientRequest(sendEthereumTransaction)).toBeDefined();
   });
 });
 
 
 describe('Upstream class', () => {
-  test('Handle requests', async () => {
-    expect.assertions(9);
-    const upstream = new UpstreamService(ethereumServiceMock, dbPromise, 1);
-    const result = await upstream.handleRequest(prepareRequest(dRKeyStoreGet));
-    expect(result).toBeDefined();
-    expect(await upstream.handleRequest(prepareRequest(dRKeyStorePut))).toBeDefined();
-    expect(await upstream.handleRequest(prepareRequest(dRKeyStoreDeleteMK))).toBeDefined();
-    expect(await upstream.handleRequest(prepareRequest(dRKeyStoreDeleteKeys))).toBeDefined();
-    expect(await upstream.handleRequest(prepareRequest(dRKeyStoreCount))).toBeDefined();
-    expect(await upstream.handleRequest(prepareRequest(dRKeyStoreAll))).toBeDefined();
-    expect(await upstream.handleRequest(prepareRequest(showModal))).toBeDefined();
-    expect(await upstream.handleRequest(prepareRequest(sendEthereumTransaction))).toBeDefined();
-    expect(await upstream.handleRequest(prepareRequest(saveDApp))).toBeDefined();
+  test('Handle client upstream requests', async () => {
+    expect.assertions(2);
+    const upstream = new UpstreamService(ethereumServiceMock);
+    expect(await upstream.handleRequest(prepareClientRequest(showModal))).toBeDefined();
+    expect(await upstream.handleRequest(prepareClientRequest(sendEthereumTransaction))).toBeDefined();
+  });
+  test('Handle UI API upstream requests', async () => {
+    expect.assertions(2);
+    const upstream = new UpstreamService(ethereumServiceMock);
+    const responsePromise = upstream.handleRequest(prepareUIAPIRequest(dAppPersisted));
+    expect(responsePromise).toBeInstanceOf(Promise);
+    expect(await responsePromise).toBeUndefined();
   });
 });
