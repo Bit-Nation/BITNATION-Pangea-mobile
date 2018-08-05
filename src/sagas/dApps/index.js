af32f97d700b10dff0fa3884086530d930ceac1d
Andrii Selivanov <seland2008@gmail.com>
2018-08-05 23:30:28 +0300

    [dapps] Added UI API handling sagas (not all request handled at the moment.)
    Former-commit-id: 5239171fb2750c3c3d00dcc2587aabfecf48fe6b

--- src/sagas/dApps/index.js
+++ src/sagas/dApps/index.js
@@ -4,6 +4,7 @@ import { all, takeEvery } from 'redux-saga/effects';
 import { startDAppSaga, openDApp, performDAppCallback, onDAppsListUpdated, fetchDApps } from './sagas';
 import { START_DAPP, OPEN_DAPP, PERFORM_DAPP_CALLBACK, DAPPS_LIST_UPDATED } from '../../actions/dApps';
 import { SERVICES_CREATED } from '../../actions/serviceContainer';
+import { subscribeToUIAPI } from './uiAPI';
 
 /**
  * @desc Root DApps saga.
@@ -12,6 +13,7 @@ import { SERVICES_CREATED } from '../../actions/serviceContainer';
 export default function* rootSaga(): Generator<*, *, *> {
   yield all([
     yield takeEvery(SERVICES_CREATED, fetchDApps),
+    yield takeEvery(SERVICES_CREATED, subscribeToUIAPI),
     yield takeEvery(DAPPS_LIST_UPDATED, onDAppsListUpdated),
     yield takeEvery(START_DAPP, startDAppSaga),
     yield takeEvery(OPEN_DAPP, openDApp),
