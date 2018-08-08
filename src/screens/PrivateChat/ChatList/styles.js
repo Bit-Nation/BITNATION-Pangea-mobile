e0b4b4eb882bc68f711278159e29d6ac41b0a692
Hoang Hung <hth321@gmail.com>
2018-08-08 19:19:41 +0700

    [dev]fixed issue 457
    Former-commit-id: 8b9df3538a7c72a8cda3a1695d55ab0a00d95ac9 [formerly f22fc5a79a819b7da88c1c1e8fcfce9603914acb]
    Former-commit-id: 25ba4c89eef76d0240f1aa90097c753d5744477f

--- src/screens/PrivateChat/ChatList/styles.js
+++ src/screens/PrivateChat/ChatList/styles.js
@@ -1,7 +1,7 @@
 // @flow
 
 import { MediaQueryStyleSheet } from 'react-native-responsive';
-
+import { StyleSheet, Platform } from 'react-native';
 import GlobalStyles from '../../../global/Styles';
 import Colors from '../../../global/colors';
 
@@ -70,6 +70,42 @@ const styles = MediaQueryStyleSheet.create({
   buttonArea: {
     flexDirection: 'row',
     justifyContent: 'flex-end',
+  },
+  modalMoreContainer: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    paddingLeft: 100,
+    paddingRight: 10,
+    paddingTop: Platform.OS === 'android' ? 0 : 20,
+  },
+  modalMoreContent: {
+    backgroundColor: 'white',
+    shadowColor: Colors.BitnationDarkGrayColor,
+    shadowOffset: { width: 2, height: 4 },
+    shadowOpacity: 0.8,
+    elevation: 2,
+  },
+  modalMenuItem: {
+    paddingVertical: 10,
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  modalMenuItemBorderBottom: {
+    borderBottomColor: 'rgba(0, 0, 0, 0.87)',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  modalMenuText: {
+    paddingLeft: 15,
+    fontSize: 16,
+    color: 'rgba(0, 0, 0, 0.87)',
+  },
+  iconForward: {
+    fontSize: 17,
+    color: 'rgba(0, 0, 0, 0.87)',
+    paddingTop: 5,
+    paddingLeft: 60,
+    paddingRight: 15,
   },
 });
 
