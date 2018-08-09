d953eb0eec63596e56b19127b1a29563f5de2ae4
Andrii Selivanov <seland2008@gmail.com>
2018-08-09 09:38:51 +0300

    [upstream] Added UI API implementation.

---
+++ src/sagas/upstream/index.js
@@ -0,0 +1,15 @@
+// @flow
+
+import { all, takeEvery } from 'redux-saga/effects';
+import { SERVICES_CREATED } from '../../actions/serviceContainer';
+import { subscribeToUIAPI } from './uiAPI';
+
+/**
+ * @desc Root DApps saga.
+ * @return {void}
+ */
+export default function* rootSaga(): Generator<*, *, *> {
+  yield all([
+    yield takeEvery(SERVICES_CREATED, subscribeToUIAPI),
+  ]);
+}
