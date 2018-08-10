3ba89fc96e508173f2729d43c4bc6c37efedf1f9
Andrii Selivanov <seland2008@gmail.com>
2018-08-11 00:07:43 +0300

    [dapps] Added AmountSelect component.
    Former-commit-id: 6ba626da57339d3e6a499f60f29e6cf348b2b6f1

--- src/DAppsSDK/0.0.1/components/index.js
+++ src/DAppsSDK/0.0.1/components/index.js
@@ -2,10 +2,12 @@ import Text from '../components/Text';
 import View from '../components/View';
 import TextInput from '../components/TextInput';
 import Button from '../components/Button';
+import AmountSelectController from './AmountSelect/AmountSelectController';
 
 export default {
   text: Text,
   view: View,
   textInput: TextInput,
   button: Button,
+  amountSelect: AmountSelectController,
 };
