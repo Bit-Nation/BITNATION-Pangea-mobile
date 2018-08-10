9f40438ba04cb38d7b4479fa83ee51f22c250e18
Andrii Selivanov <seland2008@gmail.com>
2018-08-11 00:08:18 +0300

    [dapps] Updated passing data to callbacks. Added wrapper to hide keyboard on tap outside.
    Former-commit-id: 471fc735f066b6c9abf670f9ed5303dfcb313dce [formerly 81bca50e9c123888f580dea46842181119212360]
    Former-commit-id: 53103be6da3ecf3b4d8176736725b1e6905a2bb6

--- src/DAppsSDK/0.0.1/components/Root.js
+++ src/DAppsSDK/0.0.1/components/Root.js
@@ -1,7 +1,7 @@
 // @flow
 
 import React, { Component } from 'react';
-import { View as ReactNativeView } from 'react-native';
+import { TouchableWithoutFeedback, View as ReactNativeView, Keyboard } from 'react-native';
 import { connect } from 'react-redux';
 
 import { type ComponentsJSON, renderJSON } from '../utils/renderer';
@@ -24,13 +24,7 @@ export type Props = {
   performDAppCallback: (appId: string, callbackID: number, args: Object) => void,
 }
 
-class Root extends Component<Props, any> {
-  constructor(props: Props) {
-    super(props);
-
-    this.state = {};
-  }
-
+class Root extends Component<Props> {
   performCallbackByID = (callbackID: number, args: Object) => {
     this.props.performDAppCallback(this.props.dAppPublicKey, callbackID, args);
   };
@@ -56,7 +50,7 @@ class Root extends Component<Props, any> {
           return;
         }
 
-        resultedProps[callbackName] = args => this.performCallbackByID(callbackID, args);
+        resultedProps[callbackName] = data => this.performCallbackByID(callbackID, data);
       });
     }
 
@@ -65,9 +59,11 @@ class Root extends Component<Props, any> {
 
   render() {
     return (
-      <ReactNativeView style={GlobalStyles.bodyContainer}>
-        {renderJSON(this.props.layout, undefined, this.generateCustomProps)}
-      </ReactNativeView>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss} accesible={false}>
+        <ReactNativeView style={GlobalStyles.bodyContainer}>
+          {renderJSON(this.props.layout, undefined, this.generateCustomProps)}
+        </ReactNativeView>
+      </TouchableWithoutFeedback>
     );
   }
 }
@@ -75,8 +71,8 @@ class Root extends Component<Props, any> {
 const mapStateToProps = () => ({});
 
 const mapDispatchToProps = dispatch => ({
-  performDAppCallback(dAppId, callbackId, state) {
-    dispatch(performDAppCallback(dAppId, callbackId, { state }));
+  performDAppCallback(dAppId, callbackId, payload) {
+    dispatch(performDAppCallback(dAppId, callbackId, { payload }));
   },
 });
 
