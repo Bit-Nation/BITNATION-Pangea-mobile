203dd7b5132e4baced78498c1bab9ffa8778e179
Andrii Selivanov <seland2008@gmail.com>
2018-08-31 01:00:07 +0300

    [settings] Liked documents list screen to the `Documents` section.
    Former-commit-id: 7aaf8bc42ba960de1f322aa2ed6bc8550a97277b

--- src/screens/screens.js
+++ src/screens/screens.js
@@ -39,6 +39,7 @@ import CreatePasscodeScreen from './Passcode/CreatePasscodeContainer';
 import ChatListScreen from './PrivateChat/ChatList';
 import PrivateChatScreen from './PrivateChat/Chat';
 import DAppModalScreen from './Dapp/ModalScreen';
+import DocumentsListScreen from './Documents/List';
 
 /**
  * @desc Registers screens for React Native Navigation.
@@ -83,4 +84,5 @@ export default function registerScreens(store: Object, Provider: Object) {
   Navigation.registerComponent(screen('CHAT_LIST_SCREEN').screen, () => ChatListScreen, store, Provider);
   Navigation.registerComponent(screen('PRIVATE_CHAT_SCREEN').screen, () => PrivateChatScreen, store, Provider);
   Navigation.registerComponent(screen('DAPP_MODAL_SCREEN').screen, () => DAppModalScreen, store, Provider);
+  Navigation.registerComponent(screen('DOCUMENTS_LIST_SCREEN').screen, () => DocumentsListScreen, store, Provider);
 }
