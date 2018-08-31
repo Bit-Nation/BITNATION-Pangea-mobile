cc7ae005b5cc3ee139119d42f8eb3a55d12a82c4
Andrii Selivanov <seland2008@gmail.com>
2018-08-31 04:23:53 +0300

    [tests] Update tests for ChatListItem.

--- __tests__/src/components/common/ChatListItem.js
+++ __tests__/src/components/common/ChatListItem.js
@@ -1,22 +1,19 @@
+// @flow
+
 import React from 'react';
 import { shallow } from 'enzyme';
 
 import ChatListItem from '../../../../src/components/common/ChatListItem';
-import AssetsImages from '../../../../src/global/AssetsImages';
 
 describe('ChatListItem tests', () => {
   describe('Rendering', () => {
     test('With text', () => {
-      const wrapper = shallow(<ChatListItem text='Test list item' />);
-      expect(wrapper).toMatchSnapshot();
-    });
-
-    test('With different texts and icon', () => {
       const wrapper = shallow(<ChatListItem
-        text='Test chat list item'
-        participants='Text'
-        itemIcon={AssetsImages.ChatUI.signal0}
-        profileImage=''
+        id='1'
+        name='Test list item'
+        lastMessage='Some message'
+        unreadMessages={false}
+        onPress={jest.fn()}
       />);
       expect(wrapper).toMatchSnapshot();
     });
@@ -26,7 +23,12 @@ describe('ChatListItem tests', () => {
     test('Press', () => {
       const mockFunc = jest.fn();
       const id = 'Test id';
-      const wrapper = shallow(<ChatListItem text='Test list item' onPress={mockFunc} id={id} />);
+      const wrapper = shallow(<ChatListItem
+        unreadMessages={false}
+        name='Test list item'
+        onPress={mockFunc}
+        id={id}
+      />);
       expect(wrapper).toMatchSnapshot();
       const render = wrapper.dive();
       const touchables = render.find('[testID="Touchable"]');
