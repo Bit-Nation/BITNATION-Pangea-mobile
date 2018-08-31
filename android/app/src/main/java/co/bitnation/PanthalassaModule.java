e1a0e6e74bcc398adb1832c92500d5f9344ed5a9
Alberto Estarrona <alberto@estarrona.me>
2018-08-30 19:34:33 -0500

    [android] Added method `PanthalassaCall` to interface

--- android/app/src/main/java/co/bitnation/PanthalassaModule.java
+++ android/app/src/main/java/co/bitnation/PanthalassaModule.java
@@ -517,6 +517,21 @@ public class PanthalassaModule extends ReactContextBaseJavaModule {
         }).start();
     }
 
+    @ReactMethod
+    public void PanthalassaCall(final ReadableMap jsonParams, final Promise promise) throws JSONException {
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    String response = Panthalassa.call(jsonParams.getString("command"),
+                                                        jsonParams.getString("payload"));
+                    promise.resolve(response);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    promise.reject("error", e.getLocalizedMessage());
+                }
+            }
+        }).start();
+    }
     //=====
 
     private void sendEvent(ReactContext reactContext,
