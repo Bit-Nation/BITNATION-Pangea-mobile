d2ba49f3bb21f7a8b017521125ddf3b56cd01f24
Miguel Alatzar <this.migbot@gmail.com>
2018-09-11 12:39:30 -0700

    [fix] Remove unused styling
    Former-commit-id: 42710212eb4d8c9d016723d1043ed90530946962

--- src/screens/PrivateChat/ContactsPicker/styles.js
+++ src/screens/PrivateChat/ContactsPicker/styles.js
@@ -17,68 +17,6 @@ const styles = MediaQueryStyleSheet.create({
   list: {
     backgroundColor: 'transparent',
     borderColor: 'transparent',
-    elevation: 0,
-  },
-  listRow: {
-    paddingVertical: 8,
-    paddingHorizontal: 10,
-    borderBottomWidth: StyleSheet.hairlineWidth,
-    borderColor: '#e0e0e0',
-  },
-  listWrapper: {
-    flexDirection: 'row',
-  },
-  listIcon: {
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  listPrimaryText: {
-    color: 'rgba(0, 0, 0, 0.87)',
-    fontSize: 14,
-    lineHeight: 21,
-  },
-  listSecondaryText: {
-    color: 'rgba(0, 0, 0, 0.54)',
-    fontSize: 14,
-    lineHeight: 21,
-  },
-
-  modalContainer: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'rgba(51, 51, 51, 0.4)',
-  },
-
-  modalContent: {
-    width: 327,
-    backgroundColor: 'white',
-    padding: 20,
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-  },
-
-  newChatModal: {
-    height: 471,
-  },
-
-  invalidKeyModal: {
-    height: 393,
-  },
-  modalTitle: {
-    fontSize: 19,
-    fontWeight: 'bold',
-  },
-
-  modalBody: {
-    marginTop: 17,
-    fontSize: 16,
-    color: 'rgba(0, 0, 0, 0.54)',
-  },
-  buttonArea: {
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
   },
 });
 
