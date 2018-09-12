c2c361640acca1e850c636fc13ba77433bc3f17c
Andrii Selivanov <seland2008@gmail.com>
2018-09-13 00:09:00 +0300

    [ios] Fixed issue with concurrent method calls.
    Former-commit-id: 55b3553fb684911ded8b1900a072e3d68a59ce9a

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ import asyncToGenerator from 'async-to-generator';
 import configureStore from './src/config/configureStore';
 import registerScreens from './src/screens/screens';
 import { startNavigation } from './src/actions/navigation';
-import { startContactsFetch } from './src/actions/contacts';
 
 // Fix for https://github.com/Bit-Nation/BITNATION-Pangea-mobile/issues/166
 // Related with https://github.com/facebook/react-native/issues/4844
@@ -15,8 +14,6 @@ babelHelpers.asyncToGenerator = asyncToGenerator;
 const store = configureStore();
 registerScreens(store, Provider);
 store.dispatch(startNavigation());
-// Workaround for https://github.com/Bit-Nation/BITNATION-Pangea-mobile/issues/518
-store.dispatch(startContactsFetch());
 
 const App = () => null;
 
