ac99a5b6ea91e2ef096982d40d21ea65b80b5193
prameetc <prameet.c@gmail.com>
2018-09-15 16:05:42 +0530

    [documents] Added Function to Upload Documents
    Former-commit-id: 5ec992213b1dbc78021f603aed02efd0185dcb41

--- src/screens/Documents/View/index.js
+++ src/screens/Documents/View/index.js
@@ -2,18 +2,18 @@
 
 import React from 'react';
 import { connect } from 'react-redux';
-import {
-  View,
-  Image,
-  Text,
-} from 'react-native';
+import { View, Image, Text } from 'react-native';
 
 import type { Navigator } from '../../../types/ReactNativeNavigation';
 import NavigatorComponent from '../../../components/common/NavigatorComponent';
 import { screen } from '../../../global/Screens';
 import styles from './styles';
 import i18n from '../../../global/i18n';
-import { startDocumentEditing, deleteDocument } from '../../../actions/documents';
+import {
+  startDocumentEditing,
+  deleteDocument,
+  uploadDocument,
+} from '../../../actions/documents';
 import type { State as DocumentsState } from '../../../reducers/documents';
 import Button from '../../../components/common/Button';
 import Colors from '../../../global/colors';
@@ -41,26 +41,38 @@ type Actions = {
    * @param id Id of document to delete.
    */
   deleteDocument: (id: number) => void,
-}
+  /**
+   * @desc Function to initiate document upload.
+   * @param id Id of document to delete.
+   */
+  uploadDocument: (id: number) => void,
+};
 
 const MORE_MENU_BUTTON = 'MORE_MENU_BUTTON';
 
-class DocumentsViewScreen extends NavigatorComponent<Props & DocumentsState & Actions, *> {
+class DocumentsViewScreen extends NavigatorComponent<
+  Props & DocumentsState & Actions,
+  *,
+> {
   constructor(props) {
     super(props);
 
     this.props.navigator.setButtons({
-      leftButtons: [{
-        id: 'cancel',
-        icon: AssetsImages.closeIcon,
-        title: i18n.t('common.cancel'),
-        buttonColor: Colors.navigationButtonColor,
-      }],
-      rightButtons: [{
-        id: MORE_MENU_BUTTON,
-        icon: AssetsImages.moreMenuIcon,
-        buttonColor: Colors.navigationButtonColor,
-      }],
+      leftButtons: [
+        {
+          id: 'cancel',
+          icon: AssetsImages.closeIcon,
+          title: i18n.t('common.cancel'),
+          buttonColor: Colors.navigationButtonColor,
+        },
+      ],
+      rightButtons: [
+        {
+          id: MORE_MENU_BUTTON,
+          icon: AssetsImages.moreMenuIcon,
+          buttonColor: Colors.navigationButtonColor,
+        },
+      ],
     });
 
     this.state = {
@@ -113,40 +125,50 @@ class DocumentsViewScreen extends NavigatorComponent<Props & DocumentsState & Ac
     });
   };
 
+  onDocumentSubmit = () => {
+    const { openedDocumentId } = this.props;
+    if (openedDocumentId == null) return;
+
+    this.props.uploadDocument(openedDocumentId);
+  };
+
   render() {
     const document = getOpenedDocument(this.props);
-    if (document == null) return (<View />);
+    if (document == null) return <View />;
 
     return (
       <View style={styles.screenContainer}>
         <View style={styles.previewContainer}>
-          <Image source={imageSource(this.content, document.mimeType)} style={styles.preview} resizeMode='contain' />
+          <Image
+            source={imageSource(this.content, document.mimeType)}
+            style={styles.preview}
+            resizeMode='contain'
+          />
         </View>
         <View style={styles.metadataContainer}>
-          <Text style={styles.headline}>
-            {document.name}
-          </Text>
-          <Text style={styles.footnote}>
-            {document.description}
-          </Text>
+          <Text style={styles.headline}>{document.name}</Text>
+          <Text style={styles.footnote}>{document.description}</Text>
           <Button
             enabled
             style={styles.actionButton}
             title={i18n.t('screens.documentView.submitdocument').toUpperCase()}
+            onPress={this.onDocumentSubmit}
             styleTitle={styles.settingsText}
           />
         </View>
         <MoreMenuModal
           visible={this.state.moreMenuVisible === true}
           onCancel={() => this.setState({ moreMenuVisible: false })}
-          options={[{
-            text: i18n.t('screens.documentView.actions.edit'),
-            onPress: this.onSelectEdit,
-          }, {
-            text: i18n.t('screens.documentView.actions.delete'),
-            onPress: this.onSelectDelete,
-          }]
-          }
+          options={[
+            {
+              text: i18n.t('screens.documentView.actions.edit'),
+              onPress: this.onSelectEdit,
+            },
+            {
+              text: i18n.t('screens.documentView.actions.delete'),
+              onPress: this.onSelectDelete,
+            },
+          ]}
         />
       </View>
     );
@@ -164,6 +186,12 @@ const mapDispatchToProps = dispatch => ({
   deleteDocument(documentId) {
     dispatch(deleteDocument(documentId));
   },
+  uploadDocument(documentId) {
+    dispatch(uploadDocument(documentId));
+  },
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(DocumentsViewScreen);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(DocumentsViewScreen);
