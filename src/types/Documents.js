460c2f2af2be3715e2efbdbe42f25bbaa72821c1
prameetc <prameet.c@gmail.com>
2018-09-17 09:44:24 +0530

    [documents] Updated Snapshots and Document Type
    Former-commit-id: 4010b10b62501d284ee16489083f8a08dc537ae4 [formerly 37997ade81baff89f3e1800ac572cded07ba8db2]
    Former-commit-id: b30ca0c25b1074d813b52f31b8513486f7d27fff

--- src/types/Documents.js
+++ src/types/Documents.js
@@ -6,8 +6,8 @@ export type Document = {
   description: string,
   tx_hash: string,
   signature: string,
-  registered: Date,
-  doc_hash: string,
+  registered?: string,
+  doc_hash?: string,
   dataId: string,
   mimeType: string,
 }
