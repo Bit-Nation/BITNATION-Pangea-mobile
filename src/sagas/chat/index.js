c6149ca5cd9da851e48d9d2fa2c2daf5778d9574
Andrii Selivanov <seland2008@gmail.com>
2018-09-29 00:27:27 +0300

    [chat] Updated saga.

--- src/sagas/chat/index.js
+++ src/sagas/chat/index.js
@@ -1,9 +1,7 @@
 import { all, takeEvery } from 'redux-saga/effects';
 
 import {
-  GET_PROFILE,
-  NEW_CHAT_SESSION,
-  OPEN_CHAT_SESSION,
+  START_NEW_CHAT,
   SEND_MESSAGE,
   FETCH_ALL_CHATS,
   LOAD_CHAT_MESSAGES,
@@ -11,12 +9,10 @@ import {
   CHANGE_UNREAD_STATUS,
 } from '../../actions/chat';
 import {
-  getProfileActionHandler,
-  createChatSession,
-  openChatSession,
-  fetchAllChats,
+  startNewChatSaga,
+  fetchAllChatsSaga,
   sendMessage,
-  loadMessages,
+  loadMessagesActionHandler,
   handlePanthalassaMessagePersisted,
   changeUnreadStatus,
 } from './sagas';
@@ -27,13 +23,60 @@ import {
  */
 export default function* rootSaga() {
   yield all([
-    takeEvery(FETCH_ALL_CHATS, fetchAllChats),
-    takeEvery(GET_PROFILE, getProfileActionHandler),
-    takeEvery(NEW_CHAT_SESSION, createChatSession),
-    takeEvery(OPEN_CHAT_SESSION, openChatSession),
+    takeEvery(FETCH_ALL_CHATS, fetchAllChatsSaga),
+    takeEvery(START_NEW_CHAT, startNewChatSaga),
     takeEvery(SEND_MESSAGE, sendMessage),
-    takeEvery(LOAD_CHAT_MESSAGES, loadMessages),
+    takeEvery(LOAD_CHAT_MESSAGES, loadMessagesActionHandler),
     takeEvery(PANTHALASSA_MESSAGE_PERSISTED, handlePanthalassaMessagePersisted),
     takeEvery(CHANGE_UNREAD_STATUS, changeUnreadStatus),
   ]);
 }
+
+const ok = [
+  {
+    id: 1,
+    members:
+      ['SuivW2LLcnq07aThdIKPapA43fHhanDivvi3ZXgLco8=',
+        '5LESXtoU0MJEu/mdXuS7qaH8GIfe9vc4Lw8quD3bNEA='],
+    accountId: '247f1386-9edb-418a-ad90-d48d9e277c87',
+    unreadMessages: false,
+    name: 'Group chat',
+    messages:
+      [{
+        _id: '1538169767157805594',
+        text: 'Hey ho',
+        createdAt: 'Sat Sep 29 2018 00:22:47 GMT+0300 (+03)',
+        user: { _id: '081c3ebd1509cd4f9681757a3a241618eafa33440c160cc70057748807888916' },
+        dAppMessage: null,
+      }],
+  },
+  {
+    id: 2,
+    members: ['4ae8af5b62cb727ab4eda4e174828f6a9038ddf1e16a70e2bef8b765780b728f'],
+    accountId: '247f1386-9edb-418a-ad90-d48d9e277c87',
+    unreadMessages: false,
+    name: null,
+    messages:
+      [{
+        _id: '1538169612730164151',
+        text: '',
+        createdAt: 'Sat Sep 29 2018 00:20:12 GMT+0300 (+03)',
+        user: { _id: '081c3ebd1509cd4f9681757a3a241618eafa33440c160cc70057748807888916' },
+        dAppMessage: null,
+      }],
+  },
+  {
+    id: 3,
+    members: ['e4b1125eda14d0c244bbf99d5ee4bba9a1fc1887def6f7382f0f2ab83ddb3440'],
+    accountId: '247f1386-9edb-418a-ad90-d48d9e277c87',
+    unreadMessages: false,
+    name: null,
+    messages:
+      [{
+        _id: '1538169612731081690',
+        text: '',
+        createdAt: 'Sat Sep 29 2018 00:20:12 GMT+0300 (+03)',
+        user: { _id: '081c3ebd1509cd4f9681757a3a241618eafa33440c160cc70057748807888916' },
+        dAppMessage: null,
+      }],
+  }];
