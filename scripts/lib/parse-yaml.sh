49b2dd5d707ad8ff9851d7e6e46feb2fc455064c
Mark Nuzz <mark@bitnation.co>
2018-12-21 23:00:28 -0700

    Cleanup unused code

--- scripts/lib/parse-yaml.sh
+++ scripts/lib/parse-yaml.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-
-# $(eval "${DIR}/check-bash-version.sh 4") #bash 4 not needed
-#if [[ $? -eq 0 ]]; then
     
 source $DIR/parse-yaml-bash.sh
 create_variables $1
