2a018e9895ba40759ce74a4654c0bfd08affc317
Mark Nuzz <mark@bitnation.co>
2018-12-30 22:26:22 -0700

    Finishing touch on logview and update readme

--- scripts/android-logview.sh
+++ scripts/android-logview.sh
@@ -15,10 +15,19 @@ YELLOW='\033[1;33m'
 CYAN='\033[0;36m'
 echo ""
 echo -e "${CYAN}Launching lnav with logs filtered on app pid:$YELLOW $APP_PID"
-sleep 1.5
-lnav -c ":load-session" \
+sleep .5
+
+LNAVLOG=$DIR/../data/logs/lnav.log
+if [ -e $LNAVLOG ]; then
+    rm $LNAVLOG
+fi
+
+lnav -d $LNAVLOG -nq -c ":load-session" \
      -c ":reset-session" \
-     -c ":filter-in (?<timestamp>\\d{2}-\\d{2}\\s+\\d{2}:\\d{2}:\\d{2}.\\d{3}) (\\s?${APP_PID}) .*" \
      -c ":save-session" \
+     "${DIR}/../data/logs/pangea-android-logcat.log"
+
+lnav -d $LNAVLOG -c ":load-session" \
+     -c ":filter-in (\\d{2}-\\d{2}\\s+\\d{2}:\\d{2}:\\d{2}.\\d{3}) (\\s?${APP_PID}) " \
      "${DIR}/../data/logs/pangea-android-logcat.log"
 
