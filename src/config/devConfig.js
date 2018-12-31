1965d84a283d432f31fb397a04482f97e08add4f
Mark Nuzz <mark@bitnation.co>
2018-12-30 22:38:24 -0700

    Fixes dev config crash (oops)

--- src/config/devConfig.js
+++ src/config/devConfig.js
@@ -11,14 +11,17 @@ function isDevBuild() {
     }
 }
 
-if (isDevBuild()) {
-    console.log('Pangea Development Build Detected.');
-    if (!fs.exists('dev.config.json')) {
-        console.error("dev.config.json not found. Disabling development config.");return false;
-    }
-    customData = require('./dev.config.json');
+(() => {
+    if (isDevBuild()) {
+        console.log('Pangea Development Build Detected.');
+        if (!fs.exists('dev.config.json')) {
+            console.error("dev.config.json not found. Disabling development config.");
+            return;
+        }
+        customData = require('./dev.config.json');
 
-    //test
-    const foo = customData.launcher.android.emulator_port;
-    const bar = 1;
-}
+        //test
+        const foo = customData.launcher.android.emulator_port;
+        const bar = 1;
+    }
+})();
