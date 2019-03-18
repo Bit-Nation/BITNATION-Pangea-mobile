837153e4a553eacaa4d832c229ccaffee097aafd
Hoang Hung <hth321@gmail.com>
2019-03-18 14:41:37 -0700

    updated text and icons govmarket
    Copied from commit 3a8a807082c60fef983ce16cdb70c827cf2f89e3

--- src/PangeaCore/UI/Services/helper.js
+++ src/PangeaCore/UI/Services/helper.js
@@ -1,11 +1,11 @@
-import AssetsImages from '../../global/AssetsImages';
+import AssetsImages from 'pangea-common-reactnative/assets/AssetsImages';
 
 export const contractData = [
   {
     uri: 'https://raw.githubusercontent.com/Bit-Nation/marriage-contract/master/MarriageContract.sol',
     description: 'Simple Marriage Contract',
-    title: 'Producer: BITNATION Americas LTD | Year 2018 | Audited: No | Cost: Standard Ethereum tx fees',
-    subTitle: 'Blockchain: Ethereum | Contact: info@bitnation.co',
+    title: 'Producer: BITNATION Americas LTD | Year 2018 | Audited: No',
+    subTitle: 'Cost: Standard Ethereum tx feesBlockchain: Ethereum | Contact: info@bitnation.co',
     imageUri: AssetsImages.govMarketItem2,
     bannerImage: AssetsImages.imgContract,
     readmore: `A very barebone marriage contract where you simply declare your intentions on the Ethereum chain, and sign it. No contract management or fund management tools included.
@@ -14,42 +14,31 @@ export const contractData = [
   {
     uri: 'https://raw.githubusercontent.com/Bit-Nation/pat-token-contract/master/src/sol/Token.sol',
     description: 'ERC-20 Standard Token Smart Contract Interface',
-    title: 'Producer: BITNATION Americas LTD',
-    subTitle: 'Fees: Standard ETH transaction fee',
-    imageUri: AssetsImages.govMarketItem1
+    title: 'Producer: BITNATION Americas LTD | Year 2017 | Audited: Yes',
+    subTitle: 'Cost: Standard Ethereum tx fees | Blockchain: Ethereum | Contact: info@bitnation.co',
+    imageUri: AssetsImages.imgERCIcon,
+    bannerImage: AssetsImages.imgERC,
+    readmore: 'The code source is standard ERC20 code. ABDK personalized it for Bitnation, and it was audited by ZK Labs afterwards. If you want to deploy the contract you must change the name and token address. You can deploy it through a wallet interface such as myetherwallet.com',
   },
-  {
-    uri: 'https://raw.githubusercontent.com/Bit-Nation/PangeaAgreementStandard/master/PangeaAgreementStandard.sol',
-    description: 'Pangea Agreement Standard',
-    title: 'Producer: BITNATION Americas LTD',
-    subTitle: 'Fees: Standard ETH transaction fee',
-  },
-  {
-    uri: 'https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-solidity/master/contracts/payment/PaymentSplitter.sol',
-    description: 'Payment Splitter',
-    title: 'Producer: OpenZeppelin',
-    subTitle: 'Fees: Standard ETH transaction fee',
-    imageUri: AssetsImages.govMarketItem3
-  },
+  // {
+  //   uri: 'https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-solidity/master/contracts/payment/PaymentSplitter.sol',
+  //   description: 'Payment Splitter',
+  //   title: 'Producer: OpenZeppelin',
+  //   subTitle: 'Fees: Standard ETH transaction fee',
+  //   imageUri: AssetsImages.govMarketItem3,
+  // },
 ];
 export const dAppData = [
   {
     uri: 'https://tse.bitnation.co',
     description: 'Public Notary DApp',
-    imageUri: AssetsImages.govMarketItem4,
+
+    imageUri: AssetsImages.imgDappNotaryIcon,
     bannerImage: AssetsImages.imgDappNotary,
-    title: 'Producer: BITNATION Americas LTD | Year 2018 | Audited: No | Cost: Standard Ethereum tx fees ',
-    subTitle: 'Blockchain: Ethereum | Contact: info@bitnation.co',
+    title: 'Producer: BITNATION Americas LTD | Year 2018 | Audited: No',
+    subTitle: 'Cost: Standard Ethereum tx fees | Blockchain: Ethereum | Contact: info@bitnation.co',
     readmore: 'With the BITNATION Public Notary DApp you can notarize important contracts, documents, certificates and more in a few seconds. Simply upload a photo of the document, or take a photo with your cell phone camera of the document you wish to notarize, and click "submit". The hash of the document will be stored on the Ethereum chain forever.\n The first version of our Public Notary was created in 2015 in partnership with the Estonia e-Residency program, on the Horizon chain. This is a simplified version, migrated onto the Ethereum chain. Planned DApp functionalities to be added in future releases includes document signing and storage. ',
   },
-  {
-    uri: 'https://refugees.bitnation.co/blockchain-emergency-id-be-id/',
-    description: 'BLOCKCHAIN EMERGENCY ID (BE-ID)',
-    title: 'Producer: BITNATION Americas LTD',
-    subTitle: 'Fees: Standard ETH transaction fee',
-    imageUri: AssetsImages.govMarketItem3
-  },
-
 ];
 export const serviceData = [
   {
@@ -57,7 +46,7 @@ export const serviceData = [
     description: 'Blockchain Institute & Technology',
     title: 'Producer: BIT BARCELONA | Year 2018 | Audited: N/A',
     subTitle: 'Cost: Depends on program',
-    imageUri: AssetsImages.govMarketItem2,
+    imageUri: AssetsImages.imgBITBARCELONAIcon,
     bannerImage: AssetsImages.imgProduct,
     readmore: `The Blockchain Institute & Technology, BLINTECH, is an academic institution that was born with the purpose of being an international benchmark for training in disruptive technologies.
     Our revolutionary study methodology based on virtual presence and practical hands-on in the main projects provide the participant with a transforming immersive experience.
@@ -71,9 +60,9 @@ export const productData = [
   {
     uri: 'https://tse.bitnation.co/passport/',
     description: 'Diplomatic Passport Programme',
-    title: 'Producer: BITNATION Americas LTD | Year 2018 | Audited: N/A | Cost: US$149 paid in XPAT',
-    subTitle: 'Blockchain: Ethereum | Contact: comms@bitnation.co',
-    imageUri: AssetsImages.govMarketItem3,
+    title: 'Producer: BITNATION Americas LTD | Year 2018 | Audited: N/A',
+    subTitle: 'Cost: US$149 paid in XPAT | Blockchain: Ethereum | Contact: comms@bitnation.co',
+    imageUri: AssetsImages.imgPassportIcon,
     bannerImage: AssetsImages.imgService,
     readmore: `The BITNATION Diplomatic Passports are Vienna Convention compliant passports (ICAO and ISO/IEC 7810 ID-3 standard and machine readable) to the public, ambassadors and core team members to allow users to travel as they work with nations and communities looking to assert sovereignty. The passports are recorded on the Ethereum blockchain and use the Bitnation Jurisdiction app’s Public Key as a unique identifier.
  
@@ -84,7 +73,6 @@ export const productData = [
     Bitnation is well placed to address this problem as we’re a politically neutral global Blockchain Jurisdiction, we don’t lay claim to any territory and hence are not affiliated with any potential geopolitical conflicts. In addition we have active Ambassadors in many countries across the world who can assist Bitnation Citizens locally.”
      
     Currently the passport is being tested in multiple jurisdictions by members of Bitnation’s core team. Anyone who wishes to join Bitnation’s diplomatic service program and obtain a passport will first be vetted through a 1-on-1 interview. The costs is US$149 payable in XPAT, Bitnation’s native ERC20 token, which can be purchased on Bancor, LAToken, and other exchanges. You cal also read the FAQ on Steemit.`,
- 
   },
 
 ];
