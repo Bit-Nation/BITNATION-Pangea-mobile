71ffe35665e63d6a4dbd8dbafad9bb93bdf0fbe4
prameetc <prameet.c@gmail.com>
2019-03-18 14:44:29 -0700

    Added Codepush
    Copied from commit 4e7abbc105b8601ca9c7ed01373155219060b01d

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-import './shim';
 import { AppRegistry } from 'react-native';
+import codePush from 'react-native-code-push';
+import './shim';
 
 import App from './App';
 
-AppRegistry.registerComponent('Pangea', () => App);
+AppRegistry.registerComponent('Pangea', () => codePush({
+  checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
+  installMode: codePush.InstallMode.ON_NEXT_RESUME,
+})(App));
